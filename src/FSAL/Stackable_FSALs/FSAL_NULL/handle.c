5b1bf71aa6e8d8169eee2791d4794ef2f0f2dc91
Vincent Saulue-Laborde <Vincent.Saulue@bull.net>
2015-03-16 10:10:16 +0100

    Nullfs : updating functions allocating/releasing handles
    Allocation functions just allocate a handle in the subfsal, and
    wrap them in a nullfs_fsal_obj_handle type.
    
    Change-Id: Iab04e7aa47c9332daa477f35eaead08251aa3a00
    Signed-off-by: Vincent Saulue-Laborde <Vincent.Saulue@bull.net>

--- src/FSAL/Stackable_FSALs/FSAL_NULL/handle.c
+++ src/FSAL/Stackable_FSALs/FSAL_NULL/handle.c
@@ -109,21 +109,123 @@ static struct nullfs_fsal_obj_handle *nullfs_alloc_handle(
 static fsal_status_t lookup(struct fsal_obj_handle *parent,
 			    const char *path, struct fsal_obj_handle **handle)
 {
-	return next_ops.obj_ops.lookup(parent, path, handle);
+	/** Parent as nullfs handle.*/
+	struct nullfs_fsal_obj_handle *null_parent;
+	null_parent = (struct nullfs_fsal_obj_handle *) parent;
+
+	/** Handle given by the subfsal. */
+	struct fsal_obj_handle *sub_handle = NULL;
+	/** The new nullfs handle. */
+	struct nullfs_fsal_obj_handle *result_handle = NULL;
+
+	*handle = NULL;
+
+	/* call to subfsal lookup with the good context. */
+	fsal_status_t status;
+	/** Current nullfs export. */
+	struct nullfs_fsal_export *export =
+		container_of(op_ctx->fsal_export, struct nullfs_fsal_export,
+			     export);
+	op_ctx->fsal_export = export->sub_export;
+	status = null_parent->sub_handle->obj_ops.lookup(
+			null_parent->sub_handle, path, &sub_handle);
+	op_ctx->fsal_export = &export->export;
+
+	if (!FSAL_IS_ERROR(status)) {
+		result_handle = nullfs_alloc_handle(export, sub_handle,
+						    parent->fs);
+		if (result_handle) {
+			*handle = &result_handle->obj_handle;
+		} else {
+			status.major = ERR_FSAL_NOMEM;
+			LogCrit(COMPONENT_FSAL, "Out of memory");
+
+			sub_handle->obj_ops.release(sub_handle);
+		}
+	}
+
+	return status;
 }
 
 static fsal_status_t create(struct fsal_obj_handle *dir_hdl,
 			    const char *name, struct attrlist *attrib,
 			    struct fsal_obj_handle **handle)
 {
-	return next_ops.obj_ops.create(dir_hdl, name, attrib, handle);
+	/** Parent directory nullfs handle. */
+	struct nullfs_fsal_obj_handle *nullfs_dir =
+		(struct nullfs_fsal_obj_handle *) dir_hdl;
+	/** Current nullfs export. */
+	struct nullfs_fsal_export *export =
+		container_of(op_ctx->fsal_export, struct nullfs_fsal_export,
+			     export);
+
+	/** Nullfs handle of the new file.*/
+	struct nullfs_fsal_obj_handle *result;
+	/** Subfsal handle of the new file.*/
+	struct fsal_obj_handle *sub_handle;
+
+	*handle = NULL;
+
+	/* creating the file with a subfsal handle. */
+	op_ctx->fsal_export = export->sub_export;
+	fsal_status_t status = nullfs_dir->sub_handle->obj_ops.create(
+		nullfs_dir->sub_handle, name, attrib, &sub_handle);
+	op_ctx->fsal_export = &export->export;
+
+	/* wraping the subfsal handle in a nullfs handle. */
+	if (!FSAL_IS_ERROR(status)) {
+		result = nullfs_alloc_handle(export, sub_handle, dir_hdl->fs);
+		if (result) {
+			*handle = &result->obj_handle;
+		} else {
+			status.major = ERR_FSAL_NOMEM;
+			LogCrit(COMPONENT_FSAL, "Out of memory");
+
+			sub_handle->obj_ops.release(sub_handle);
+		}
+	}
+
+	return status;
 }
 
 static fsal_status_t makedir(struct fsal_obj_handle *dir_hdl,
 			     const char *name, struct attrlist *attrib,
 			     struct fsal_obj_handle **handle)
 {
-	return next_ops.obj_ops.mkdir(dir_hdl, name, attrib, handle);
+	*handle = NULL;
+	/** Parent directory nullfs handle. */
+	struct nullfs_fsal_obj_handle *parent_hdl =
+		(struct nullfs_fsal_obj_handle *) dir_hdl;
+	/** Current nullfs export. */
+	struct nullfs_fsal_export *export =
+		container_of(op_ctx->fsal_export, struct nullfs_fsal_export,
+			     export);
+
+	/** Nullfs handle of the new directory.*/
+	struct nullfs_fsal_obj_handle *result;
+	/** Subfsal handle of the new directory.*/
+	struct fsal_obj_handle *sub_handle;
+
+	/* Creating the directory with a subfsal handle. */
+	op_ctx->fsal_export = export->sub_export;
+	fsal_status_t status = parent_hdl->sub_handle->obj_ops.mkdir(
+		parent_hdl->sub_handle, name, attrib, &sub_handle);
+	op_ctx->fsal_export = &export->export;
+
+	/* wraping the subfsal handle in a nullfs handle. */
+	if (!FSAL_IS_ERROR(status)) {
+		result = nullfs_alloc_handle(export, sub_handle, dir_hdl->fs);
+		if (result) {
+			*handle = &result->obj_handle;
+		} else {
+			status.major = ERR_FSAL_NOMEM;
+			LogCrit(COMPONENT_FSAL, "Out of memory");
+
+			sub_handle->obj_ops.release(sub_handle);
+		}
+	}
+
+	return status;
 }
 
 static fsal_status_t makenode(struct fsal_obj_handle *dir_hdl,
@@ -132,8 +234,42 @@ static fsal_status_t makenode(struct fsal_obj_handle *dir_hdl,
 			      struct attrlist *attrib,
 			      struct fsal_obj_handle **handle)
 {
-	return next_ops.obj_ops.mknode(dir_hdl, name, nodetype, dev,
-					attrib, handle);
+	/** Parent directory nullfs handle. */
+	struct nullfs_fsal_obj_handle *nullfs_dir =
+		(struct nullfs_fsal_obj_handle *) dir_hdl;
+	/** Current nullfs export. */
+	struct nullfs_fsal_export *export =
+		container_of(op_ctx->fsal_export, struct nullfs_fsal_export,
+			     export);
+
+	/** Nullfs handle of the new node.*/
+	struct nullfs_fsal_obj_handle *result;
+	/** Subfsal handle of the new node.*/
+	struct fsal_obj_handle *sub_handle;
+
+	*handle = NULL;
+
+	/* Creating the node with a subfsal handle. */
+	op_ctx->fsal_export = export->sub_export;
+	fsal_status_t status = nullfs_dir->sub_handle->obj_ops.mknode(
+		nullfs_dir->sub_handle, name, nodetype, dev, attrib,
+		&sub_handle);
+	op_ctx->fsal_export = &export->export;
+
+	/* wraping the subfsal handle in a nullfs handle. */
+	if (!FSAL_IS_ERROR(status)) {
+		result = nullfs_alloc_handle(export, sub_handle, dir_hdl->fs);
+		if (result) {
+			*handle = &result->obj_handle;
+		} else {
+			status.major = ERR_FSAL_NOMEM;
+			LogCrit(COMPONENT_FSAL, "Out of memory");
+
+			sub_handle->obj_ops.release(sub_handle);
+		}
+	}
+
+	return status;
 }
 
 /** makesymlink
@@ -147,8 +283,41 @@ static fsal_status_t makesymlink(struct fsal_obj_handle *dir_hdl,
 				 struct attrlist *attrib,
 				 struct fsal_obj_handle **handle)
 {
-	return next_ops.obj_ops.symlink(dir_hdl, name, link_path,
-					 attrib, handle);
+	/** Parent directory nullfs handle. */
+	struct nullfs_fsal_obj_handle *nullfs_dir =
+		(struct nullfs_fsal_obj_handle *) dir_hdl;
+	/** Current nullfs export. */
+	struct nullfs_fsal_export *export =
+		container_of(op_ctx->fsal_export, struct nullfs_fsal_export,
+			     export);
+
+	/** Nullfs handle of the new link.*/
+	struct nullfs_fsal_obj_handle *result;
+	/** Subfsal handle of the new link.*/
+	struct fsal_obj_handle *sub_handle;
+
+	*handle = NULL;
+
+	/* creating the file with a subfsal handle. */
+	op_ctx->fsal_export = export->sub_export;
+	fsal_status_t status = nullfs_dir->sub_handle->obj_ops.symlink(
+		nullfs_dir->sub_handle, name, link_path, attrib, &sub_handle);
+	op_ctx->fsal_export = &export->export;
+
+	/* wraping the subfsal handle in a nullfs handle. */
+	if (!FSAL_IS_ERROR(status)) {
+		result = nullfs_alloc_handle(export, sub_handle, dir_hdl->fs);
+		if (result) {
+			*handle = &result->obj_handle;
+		} else {
+			status.major = ERR_FSAL_NOMEM;
+			LogCrit(COMPONENT_FSAL, "Out of memory");
+
+			sub_handle->obj_ops.release(sub_handle);
+		}
+	}
+
+	return status;
 }
 
 static fsal_status_t readsymlink(struct fsal_obj_handle *obj_hdl,
@@ -413,7 +582,22 @@ static void handle_to_key(struct fsal_obj_handle *obj_hdl,
 
 static void release(struct fsal_obj_handle *obj_hdl)
 {
-	next_ops.obj_ops.release(obj_hdl);
+	struct nullfs_fsal_obj_handle *hdl =
+		container_of(obj_hdl, struct nullfs_fsal_obj_handle,
+			     obj_handle);
+
+	struct nullfs_fsal_export *export =
+		container_of(op_ctx->fsal_export, struct nullfs_fsal_export,
+			     export);
+
+	/* calling subfsal method */
+	op_ctx->fsal_export = export->sub_export;
+	hdl->sub_handle->obj_ops.release(hdl->sub_handle);
+	op_ctx->fsal_export = &export->export;
+
+	/* cleaning data allocated by nullfs */
+	fsal_obj_handle_fini(&hdl->obj_handle);
+	gsh_free(hdl);
 }
 
 void nullfs_handle_ops_init(struct fsal_obj_ops *ops)
@@ -468,8 +652,35 @@ fsal_status_t nullfs_lookup_path(struct fsal_export *exp_hdl,
 				 const char *path,
 				 struct fsal_obj_handle **handle)
 {
-	printf("Called %p\n", nullfs_lookup_path);
-	return next_ops.exp_ops.lookup_path(exp_hdl, path, handle);
+	struct nullfs_fsal_obj_handle *new_handle; /*< The new nullfs handle. */
+	/** Handle given by the subfsal. */
+	struct fsal_obj_handle *sub_handle = NULL;
+	*handle = NULL;
+
+	/* call underlying FSAL ops with underlying FSAL handle */
+	struct nullfs_fsal_export *exp;
+	exp = container_of(exp_hdl, struct nullfs_fsal_export, export);
+
+	/* call to subfsal lookup with the good context. */
+	fsal_status_t status;
+	op_ctx->fsal_export = exp->sub_export;
+	status = exp->sub_export->exp_ops.lookup_path(exp->sub_export, path,
+						      &sub_handle);
+	op_ctx->fsal_export = &exp->export;
+
+	if (!FSAL_IS_ERROR(status)) {
+		/* Note : nullfs filesystem = subfsal filesystem or NULL ? */
+		new_handle = nullfs_alloc_handle(exp, sub_handle, NULL);
+		if (new_handle) {
+			*handle = &new_handle->obj_handle;
+		} else {
+			status.major = ERR_FSAL_NOMEM;
+			LogCrit(COMPONENT_FSAL, "Out of memory");
+
+			sub_handle->obj_ops.release(sub_handle);
+		}
+	}
+	return status;
 }
 
 /* create_handle
@@ -488,6 +699,32 @@ fsal_status_t nullfs_create_handle(struct fsal_export *exp_hdl,
 				   struct gsh_buffdesc *hdl_desc,
 				   struct fsal_obj_handle **handle)
 {
-	return next_ops.exp_ops.create_handle(exp_hdl, hdl_desc,
-					       handle);
+	/** Current nullfs export. */
+	struct nullfs_fsal_export *export =
+		container_of(exp_hdl, struct nullfs_fsal_export, export);
+
+	struct nullfs_fsal_obj_handle *new_handle; /*< New nullfs handle.*/
+	struct fsal_obj_handle *sub_handle; /*< New subfsal handle.*/
+
+	/* call to subfsal lookup with the good context. */
+	fsal_status_t status;
+	op_ctx->fsal_export = export->sub_export;
+
+	status = export->sub_export->exp_ops.create_handle(export->sub_export,
+		hdl_desc, &sub_handle);
+	op_ctx->fsal_export = &export->export;
+
+	if (!FSAL_IS_ERROR(status)) {
+		/* Note : nullfs filesystem = subfsal filesystem or NULL ? */
+		new_handle = nullfs_alloc_handle(export, sub_handle, NULL);
+		if (new_handle) {
+			*handle = &new_handle->obj_handle;
+		} else {
+			status.major = ERR_FSAL_NOMEM;
+			LogCrit(COMPONENT_FSAL, "Out of memory");
+
+			sub_handle->obj_ops.release(sub_handle);
+		}
+	}
+	return status;
 }
