cbbb6b87095d994db1c0c004fe84e6c92c685687
Daniel Gryniewicz <dang@cohortfs.com>
2015-04-01 10:56:03 -0400

    Add FSAL sub_ops to FSAL_VFS
    The sub-FSALs of FSAL_VFS need to be able to perform operations as part
    of the OPs that the main FSAL does.  To enable this, add sub_ops to the
    FSAL, that can be implemented by the sub-FSALs.
    
    Initially, add sub_ops for getattr and setattr.
    
    Change-Id: Idc7c9a02683292ef6d5e6dd934562f358ed0115f
    Signed-off-by: Daniel Gryniewicz <dang@cohortfs.com>

--- src/FSAL/FSAL_VFS/handle.c
+++ src/FSAL/FSAL_VFS/handle.c
@@ -1168,8 +1168,6 @@ static fsal_status_t getattrs(struct fsal_obj_handle *obj_hdl)
 				     O_RDONLY, &fsal_error);
 	if (cfd.fd >= 0) {
 		st = posix2fsal_attributes(&stat, &obj_hdl->attributes);
-		if (cfd.close_fd)
-			close(cfd.fd);
 		if (FSAL_IS_ERROR(st)) {
 			FSAL_CLEAR_MASK(obj_hdl->attributes.mask);
 			FSAL_SET_MASK(obj_hdl->attributes.mask,
@@ -1179,6 +1177,18 @@ static fsal_status_t getattrs(struct fsal_obj_handle *obj_hdl)
 		} else {
 			obj_hdl->attributes.fsid = obj_hdl->fs->fsid;
 		}
+		if (myself->sub_ops && myself->sub_ops->getattrs) {
+			st = myself->sub_ops->getattrs(myself, cfd.fd);
+			if (FSAL_IS_ERROR(st)) {
+				FSAL_CLEAR_MASK(obj_hdl->attributes.mask);
+				FSAL_SET_MASK(obj_hdl->attributes.mask,
+						ATTR_RDATTR_ERR);
+				fsal_error = st.major;
+				retval = st.minor;
+			}
+		}
+		if (cfd.close_fd)
+			close(cfd.fd);
 	} else {
 		LogDebug(COMPONENT_FSAL, "Failed with %s, fsal_error %s",
 			 strerror(-cfd.fd),
@@ -1381,6 +1391,19 @@ static fsal_status_t setattrs(struct fsal_obj_handle *obj_hdl,
 		if (retval != 0)
 			goto fileerr;
 	}
+
+	/** SUBFSAL **/
+	if (myself->sub_ops && myself->sub_ops->setattrs) {
+		fsal_status_t st;
+
+		st = myself->sub_ops->setattrs(myself, cfd.fd, attrs);
+		if (FSAL_IS_ERROR(st)) {
+			fsal_error = st.major;
+			retval = st.minor;
+			goto hdlerr;
+		}
+	}
+
 	goto out;
 
  fileerr:
--- src/FSAL/FSAL_VFS/vfs_methods.h
+++ src/FSAL/FSAL_VFS/vfs_methods.h
@@ -88,6 +88,30 @@ fsal_status_t vfs_create_handle(struct fsal_export *exp_hdl,
 				struct gsh_buffdesc *hdl_desc,
 				struct fsal_obj_handle **handle);
 
+struct vfs_subfsal_obj_ops {
+/**
+ * @brief Gte sub-fsal attributes from an object
+ *
+ * @param[in] vfs_hdl    The VFS object to modify
+ * @param[in] fd		 Open filehandle on object
+ *
+ * @return FSAL status.
+ */
+	fsal_status_t (*getattrs)(struct vfs_fsal_obj_handle *vfs_hdl,
+				  int fd);
+/**
+ * @brief Set sub-fsal attributes on an object
+ *
+ * @param[in] vfs_hdl    The VFS object to modify
+ * @param[in] fd		 Open filehandle on object
+ * @param[in] attrib_set Attributes to set
+ *
+ * @return FSAL status.
+ */
+	fsal_status_t (*setattrs)(struct vfs_fsal_obj_handle *vfs_hdl,
+				  int fd, struct attrlist *attrib_set);
+};
+
 /*
  * VFS internal object handle
  * handle is a pointer because
@@ -106,6 +130,7 @@ struct vfs_fsal_obj_handle {
 	struct fsal_obj_handle obj_handle;
 	fsal_dev_t dev;
 	vfs_file_handle_t *handle;
+	struct vfs_subfsal_obj_ops *sub_ops;	/*< Optional subfsal ops */
 	const struct fsal_up_vector *up_ops;	/*< Upcall operations */
 	union {
 		struct {
