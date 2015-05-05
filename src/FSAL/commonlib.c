b1a7aec7d45810fa50f25596beb1e0d7bb636da1
Frank S. Filz <ffilzlnx@mindspring.com>
2015-05-05 19:15:23 -0400

    Fix posix_find_parent to differentiate when /fs1 and /fs10 are filesystems
    If we are finding the parent of /fs10/fs2, we don't want /fs1 to be the
    parent. When we match "/fs1" against "/fs10/fs2", the strncmp will match,
    so we test if the next character of the child patch is '/'. Since it's not,
    we skip /fs1 as being a possible parent of /fs10/fs2.
    
    Change-Id: I866d12f0f4ea32e2a498ed1fdd71e7974e2b249a
    Signed-off-by: Frank S. Filz <ffilzlnx@mindspring.com>

--- src/FSAL/commonlib.c
+++ src/FSAL/commonlib.c
@@ -1072,6 +1072,12 @@ static void posix_find_parent(struct fsal_filesystem *this)
 		if (strncmp(fs->path, this->path, fs->pathlen) != 0)
 			continue;
 
+		/* Differentiate between /fs1 and /fs10 for parent of
+		 * /fs10/fs2
+		 */
+		if (this->path[fs->pathlen] != '/')
+			continue;
+
 		this->parent = fs;
 		plen = fs->pathlen;
 	}
