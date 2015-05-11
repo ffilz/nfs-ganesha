3e9af698f779571cb04e8df93227953af7b648b3
Jiffin Tony Thottan <jthottan@redhat.com>
2015-05-11 13:53:22 +0000

    FSAL_GLUSTER : Conversion of nfs4 acls <-> posix acls using standard libacl api's
    This patch introduces two api's fsal_acl_2_posix_acl and
    posixacl_2_fsal_acl which will convert nfs4_acls to posix_acl
    and viceversa with help of standard libacl api's. Rightnow,
    conversion is only applicable for files in FSAL_GLUSTER
    
    BUG: 1215174
    Change-Id: I313b360fe53f1fa4facda1b35c4febb73266e329
    Signed-off-by: Jiffin Tony Thottan <jthottan@redhat.com>

--- src/FSAL/FSAL_GLUSTER/gluster_internal.h
+++ src/FSAL/FSAL_GLUSTER/gluster_internal.h
@@ -27,6 +27,7 @@
 #include "fsal.h"
 #include "fsal_types.h"
 #include "fsal_api.h"
+#include "posix_acls.h"
 #include <glusterfs/api/glfs.h>
 #include <glusterfs/api/glfs-handles.h>
 
@@ -422,6 +423,7 @@ typedef struct fsal_xstat__ {
 	int attr_valid;
 	struct stat buffstat;
 	char buffacl[GLFS_ACL_BUF_SIZE];
+	acl_t acl;
 } glusterfs_fsal_xstat_t;
 
 /* GLUSTERFS ACL */
