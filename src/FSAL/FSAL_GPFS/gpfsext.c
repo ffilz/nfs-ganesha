57e356c15103e7721685ff05a71db744a85a5e05
Malahal Naineni <malahal@us.ibm.com>
2015-06-18 17:23:37 -0500

    Teach valgrind about GPFS fsal ioctls.
    Instead of putting code here and there, organized everything into one
    function, and it is called just before the ioctl call. This should cover
    all GPFS related valgrind macro code.
    
    Change-Id: I7b0cb7ea4bfd61c6fcc83236da2e19e6688fb95a
    Signed-off-by: Malahal Naineni <malahal@us.ibm.com>

--- src/FSAL/FSAL_GPFS/gpfsext.c
+++ src/FSAL/FSAL_GPFS/gpfsext.c
@@ -13,17 +13,49 @@
  *              ESTALE  cached fs information was invalid
  *-------------------------------------------------------------------------*/
 
+#include "config.h"
+
 #include <sys/errno.h>
 #include <sys/fcntl.h>
 #include <sys/ioctl.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/vfs.h>
+#include <sys/stat.h>
+
 #include "include/gpfs_nfs.h"
 
 struct kxArgs {
 	signed long arg1;
 	signed long arg2;
 };
+
+#ifdef _VALGRIND_MEMCHECK
+static void valgrind_kganesha(struct kxArgs *args)
+{
+	int op = (int)args->arg1;
+
+	switch (op) {
+	case OPENHANDLE_STATFS_BY_FH:
+	{
+		struct statfs_arg *arg = (void *)args->arg2;
+
+		memset(arg->buf, 0, sizeof(*arg->buf));
+		break;
+	}
+	case OPENHANDLE_READ_BY_FD:
+	{
+		struct read_arg *arg = (void *)args->arg2;
+
+		memset(arg->bufP, 0, arg->length);
+		break;
+	}
+	default:
+		break;
+	}
+}
+#endif
 
 int gpfs_ganesha(int op, void *oarg)
 {
@@ -43,6 +75,9 @@ int gpfs_ganesha(int op, void *oarg)
 
 	args.arg1 = op;
 	args.arg2 = (long)oarg;
+#ifdef _VALGRIND_MEMCHECK
+	valgrind_kganesha(&args);
+#endif
 	rc = ioctl(gpfs_fd, kGanesha, &args);
 
 	return rc;
