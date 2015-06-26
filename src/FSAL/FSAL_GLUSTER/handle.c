b2ae38a2be99cfa07f9dfaa5fffa0f4d69e99bca
Jiffin Tony Thottan <jthottan@redhat.com>
2015-06-26 18:49:27 +0530

    FSAL_GLUSTER : inherit acl should only build for directories
    Change-Id: If104648f7f29927ad09a528585f5b4eae138042a
    Signed-off-by: Jiffin Tony Thottan <jthottan@redhat.com>

--- src/FSAL/FSAL_GLUSTER/handle.c
+++ src/FSAL/FSAL_GLUSTER/handle.c
@@ -681,16 +681,11 @@ static fsal_status_t getattrs(struct fsal_obj_handle *obj_hdl)
 
 	fsalattr = &objhandle->attributes;
 	stat2fsal_attributes(&buffxstat.buffstat, fsalattr);
-	switch (obj_hdl->type) {
-	case REGULAR_FILE:
-		buffxstat.is_dir = false;
-		break;
-	case DIRECTORY:
+	if (obj_hdl->type == DIRECTORY)
 		buffxstat.is_dir = true;
-		break;
-	default:
-		break;
-	}
+        else
+	        buffxstat.is_dir = false;
+
 	status = glusterfs_get_acl(glfs_export, objhandle->glhandle,
 				   &buffxstat, fsalattr);
 
