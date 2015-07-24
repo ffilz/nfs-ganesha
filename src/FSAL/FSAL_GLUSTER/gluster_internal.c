feee268c22d34e4220c2c6ac90482c9a92c16392
Jiffin Tony Thottan <jthottan@redhat.com>
2015-07-24 13:48:24 +0530

    FSAL_GLUSTER : Displaying DENY entries in nfs4_getfacl()
    In previous nfs4_acl implementation , even though DENY entries are
    handled properly in nfs4_setfacl(), it is not displayed in the
    nfs4_getfacl() api. This patch will fix the same.
    
    Change-Id: If6c08aacd0128e7b1818bb9905af17c93451909b
    Signed-off-by: Jiffin Tony Thottan <jthottan@redhat.com>

--- src/FSAL/FSAL_GLUSTER/gluster_internal.c
+++ src/FSAL/FSAL_GLUSTER/gluster_internal.c
@@ -492,7 +492,7 @@ fsal_status_t glusterfs_get_acl(struct glusterfs_export *glfs_export,
 	fsal_acl_status_t status;
 	fsal_acl_data_t acldata;
 	fsal_ace_t *pace = NULL;
-	int e_count = 0, i_count = 0;
+	int e_count = 0, i_count = 0, count;
 
 	fsalattr->acl = NULL;
 	if (NFSv4_ACL_SUPPORT && FSAL_TEST_MASK(fsalattr->mask, ATTR_ACL)) {
@@ -508,7 +508,8 @@ fsal_status_t glusterfs_get_acl(struct glusterfs_export *glfs_export,
 			i_count = ace_count(buffxstat->i_acl);
 		}
 
-		acldata.naces = e_count + i_count;
+		/* Allocating memory for both ALLOW and DENY entries */
+		acldata.naces = 2 * (e_count  + i_count);
 
 		LogDebug(COMPONENT_FSAL, "No of aces present in fsal_acl_t = %d"
 					, acldata.naces);
@@ -520,12 +521,20 @@ fsal_status_t glusterfs_get_acl(struct glusterfs_export *glfs_export,
 		acldata.aces = (fsal_ace_t *) nfs4_ace_alloc(acldata.naces);
 		pace = acldata.aces;
 
-		status = posix_acl_2_fsal_acl(buffxstat->e_acl,
+		count = posix_acl_2_fsal_acl(buffxstat->e_acl,
 				buffxstat->is_dir, false, &pace);
 		if (i_count > 0)
-			status = posix_acl_2_fsal_acl(buffxstat->i_acl,
+			count += posix_acl_2_fsal_acl(buffxstat->i_acl,
 				true, true, &pace);
-
+		/* Reallocating acldata into the required size */
+		acldata.aces = (fsal_ace_t *) gsh_realloc(acldata.aces,
+				count*sizeof(fsal_ace_t));
+		acldata.naces = count;
+		if (acldata.aces == NULL) {
+			LogCrit (COMPONENT_FSAL,
+			"failed to create a new acl list");
+			return fsalstat(ERR_FSAL_FAULT, -1);
+		}
 		fsalattr->acl = nfs4_acl_new_entry(&acldata, &status);
 		LogDebug(COMPONENT_FSAL, "fsal acl = %p, fsal_acl_status = %u",
 				fsalattr->acl, status);
