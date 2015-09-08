d16a7ff9068ce5b4da63e545d1045362be02f66f
Daniel Gryniewicz <dang@redhat.com>
2015-09-08 14:39:29 -0400

    DEBUG - add state storage to FSAL_VFS
    This allows FSAL_VFS to be used without any metadata caching
    
    Change-Id: I8e42e61105c99d5a08f042416b311d315ab9593b
    Signed-off-by: Daniel Gryniewicz <dang@redhat.com>

--- src/include/sal_functions.h
+++ src/include/sal_functions.h
@@ -93,8 +93,17 @@ void state_release_export(struct gsh_export *exp);
 
 bool state_unlock_err_ok(state_status_t status);
 
+/**
+ * @brief Initialize a state handle
+ *
+ * @param[in,out] ostate	State handle to initialize
+ * @param[in] type	Type of handle
+ * @param[in] obj	Object owning handle
+ * @return Return description
+ */
 static inline void state_hdl_init(struct state_hdl *ostate,
-				  object_file_type_t type)
+				  object_file_type_t type,
+				  struct fsal_obj_handle *obj)
 {
 	memset(ostate, 0, sizeof(*ostate));
 	pthread_rwlock_init(&ostate->state_lock, NULL);
@@ -104,6 +113,7 @@ static inline void state_hdl_init(struct state_hdl *ostate,
 		glist_init(&ostate->file.layoutrecall_list);
 		glist_init(&ostate->file.lock_list);
 		glist_init(&ostate->file.nlm_share_list);
+		ostate->file.obj = obj;
 		break;
 	case DIRECTORY:
 		glist_init(&ostate->dir.export_roots);
