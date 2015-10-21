9377ebb2b8b72b0340dbc88d782530f97655068f
Frank S. Filz <ffilzlnx@mindspring.com>
2015-10-21 16:59:43 -0700

    Put locktrace under ENABLE_LOCKTRACE
    It is useful but creates unecessary helgrind warnings.
    
    Change-Id: I6d8e01bccad95019580e37912e5c7fae5f32e366
    Signed-off-by: Frank S. Filz <ffilzlnx@mindspring.com>

--- src/cache_inode/cache_inode_lru.c
+++ src/cache_inode/cache_inode_lru.c
@@ -116,19 +116,26 @@ struct lru_q_lane {
 		struct glist_head *glist;
 		struct glist_head *glistn;
 	} iter;
+#ifdef ENABLE_LOCKTRACE
 	struct {
 		char *func;
 		uint32_t line;
 	} locktrace;
+#endif
 	 CACHE_PAD(0);
 };
 
+#ifdef ENABLE_LOCKTRACE
 #define QLOCK(qlane) \
 	do { \
 		PTHREAD_MUTEX_lock(&(qlane)->mtx); \
 		(qlane)->locktrace.func = (char *) __func__; \
 		(qlane)->locktrace.line = __LINE__; \
 	} while (0)
+#else
+#define QLOCK(qlane) \
+	PTHREAD_MUTEX_lock(&(qlane)->mtx)
+#endif
 
 #define QUNLOCK(qlane) \
 	PTHREAD_MUTEX_unlock(&(qlane)->mtx)
@@ -1361,6 +1368,8 @@ cache_inode_status_t cache_inode_lru_ref(cache_entry_t *entry, uint32_t flags)
 {
 	cache_inode_lru_t *lru = &entry->lru;
 	struct lru_q_lane *qlane = &LRU[lru->lane];
+	cache_inode_lru_t *lru;
+	struct lru_q_lane *qlane;
 	struct lru_q *q;
 
 	if ((flags & (LRU_REQ_INITIAL | LRU_REQ_STALE_OK)) == 0) {
