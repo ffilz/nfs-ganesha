8e7921247fa9d12e7229b8a59df57081b5deccdd
Swen Schillig <swen@vnet.ibm.com>
2015-10-26 13:56:05 +0100

    Prevent NULL ptr dereference after nfs4_acl_alloc()
    A call of nfs4_acl_alloc() can return NULL in case
    the triggered pool_alloc() failed.
    In this situation the dereferencing must be prevented.
    
    Change-Id: I4f8a31b0dbac9743ef43fe12e2909d74903668e3
    Signed-off-by: Swen Schillig <swen@vnet.ibm.com>

--- src/support/nfs4_acls.c
+++ src/support/nfs4_acls.c
@@ -148,7 +148,7 @@ fsal_acl_t *nfs4_acl_new_entry(fsal_acl_data_t *acldata,
 
 	/* Adding the entry in the cache */
 	acl = nfs4_acl_alloc();
-	if (pthread_rwlock_init(&(acl->lock), NULL) != 0) {
+	if (acl == NULL || pthread_rwlock_init(&(acl->lock), NULL) != 0) {
 		nfs4_acl_free(acl);
 		LogCrit(COMPONENT_NFS_V4_ACL,
 			"New ACL RW lock init returned %d (%s)", errno,
