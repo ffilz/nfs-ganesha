b8d79c10b5b5d5ac3f2cb62d5a2f12b09ecd2e69
Soumya Koduri <skoduri@redhat.com>
2016-08-02 23:32:26 +0530

    FSAL_GLUSTER: Re-add close() fop
    close()/file_close() was removed as part of extended APIs
    support for FSAL_GLUSTER. But since it is still being used
    to close global fd of the fsal_obj_handle as part of
    'fsal_close', restoring it back.
    
    Change-Id: I513b0988a3a8475f801be07b38919917d0a70c43
    Signed-off-by: Soumya Koduri <skoduri@redhat.com>

--- src/FSAL/FSAL_GLUSTER/handle.c
+++ src/FSAL/FSAL_GLUSTER/handle.c
@@ -1127,6 +1127,46 @@ static fsal_status_t file_unlink(struct fsal_obj_handle *dir_hdl,
 }
 
 /**
+ * @brief Implements GLUSTER FSAL objectoperation close
+   @todo: close2() could be used to close globalfd as well.
+ */
+
+static fsal_status_t file_close(struct fsal_obj_handle *obj_hdl)
+{
+	int rc = 0;
+	fsal_status_t status = { ERR_FSAL_NO_ERROR, 0 };
+	struct glusterfs_handle *objhandle =
+	    container_of(obj_hdl, struct glusterfs_handle, handle);
+#ifdef GLTIMING
+	struct timespec s_time, e_time;
+
+	now(&s_time);
+#endif
+
+	if (objhandle->globalfd.openflags == FSAL_O_CLOSED) {
+		LogDebug(COMPONENT_FSAL,
+			 "File already closed.");
+		return status;
+	}
+
+	rc = glfs_close(objhandle->globalfd.glfd);
+	if (rc != 0) {
+		status = gluster2fsal_error(errno);
+		LogCrit(COMPONENT_FSAL,
+			"Error : close returns with %s", strerror(errno));
+	}
+
+	objhandle->globalfd.glfd = NULL;
+	objhandle->globalfd.openflags = FSAL_O_CLOSED;
+
+#ifdef GLTIMING
+	now(&e_time);
+	latency_update(&s_time, &e_time, lat_file_close);
+#endif
+	return status;
+}
+
+/**
  * @brief Implements GLUSTER FSAL objectoperation share_op
  */
 /*
@@ -2900,6 +2940,7 @@ void handle_ops_init(struct fsal_obj_ops *ops)
 	ops->unlink = file_unlink;
 	ops->handle_digest = handle_digest;
 	ops->handle_to_key = handle_to_key;
+	ops->close = file_close;
 
 	/* fops with OpenTracking (multi-fd) enabled */
 	ops->open2 = glusterfs_open2;
