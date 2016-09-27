4e829463fe1bcdba0bb05f08f3a5ac48bedfed3c
Kaleb S. KEITHLEY <kkeithle@redhat.com>
2016-09-27 10:41:23 -0400

    FSAL_GPFS: build errors on Ubuntu xenial
    32-bit string format and used uninitialized
    
    Change-Id: Ib8621e6fb99e38cf6dd1cced7e5619cb5c7829ce
    Signed-off-by: Kaleb S. KEITHLEY <kkeithle@redhat.com>

--- src/FSAL/FSAL_GPFS/file.c
+++ src/FSAL/FSAL_GPFS/file.c
@@ -1127,9 +1127,10 @@ fsal_status_t gpfs_lock_op2(struct fsal_obj_handle *obj_hdl,
 	 */
 	if (lock_args.l_len < 0) {
 		LogCrit(COMPONENT_FSAL,
-			"The requested lock length is out of range- lock_args.l_len(%ld), request_lock_length(%"
-			PRIu64 ")",
-			lock_args.l_len, request_lock->lock_length);
+			"The requested lock length is out of range- lock_args.l_len(%"
+			PRIu64 "), request_lock_length(%" PRIu64 ")",
+			(uint64_t) lock_args.l_len,
+			(uint64_t) request_lock->lock_length);
 		return fsalstat(ERR_FSAL_BAD_RANGE, 0);
 	}
 
@@ -1497,9 +1498,12 @@ gpfs_lock_op(struct fsal_obj_handle *obj_hdl, void *owner,
 	}
 
 	LogFullDebug(COMPONENT_FSAL,
-		     "Locking: op:%d type:%d claim:%d start:%" PRIu64
-		     " length:%lu ", lock_op, request_lock->lock_type,
-		     request_lock->lock_reclaim, request_lock->lock_start,
+		     "Locking: op:%d type:%d claim:%d start:%"
+		     PRIu64 " length:%" PRIu64,
+		     lock_op,
+		     request_lock->lock_type,
+		     request_lock->lock_reclaim,
+		     request_lock->lock_start,
 		     request_lock->lock_length);
 
 	return GPFSFSAL_lock_op(op_ctx->fsal_export, obj_hdl, owner, lock_op,
