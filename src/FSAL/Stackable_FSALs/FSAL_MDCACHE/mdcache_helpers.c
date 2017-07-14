030d6ba50f918776eb8c4967051e3ed5181fe03d
Dominique Martinet <dominique.martinet@cea.fr>
2017-07-14 09:02:42 +0200

    mdcache: fix one content_lock usage
    mdc_lookup_uncached needs wrlock
    
    Signed-off-by: Dominique Martinet <dominique.martinet@cea.fr>
    
    There is complexity here because dropping the content_lock means the
    state of the chunks and dirents may have been changed.
    
    Change-Id: If2e656809d94bd5628eef26ed5529b212ec50a41
    Signed-off-by: Frank S. Filz <ffilzlnx@mindspring.com>

--- src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_helpers.c
+++ src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_helpers.c
@@ -2962,6 +2962,42 @@ again:
 				     "Lookup by key for %s failed, lookup by name now",
 				     dirent->name);
 
+			/* mdc_lookup_uncached needs write lock, dropping the
+			 * read lock means we can no longer trust the dirent or
+			 * the chunk.
+			 */
+			if (!has_write) {
+				/* We will have to re-find this dirent after we
+				 * re-acquire the lock.
+				 */
+				look_ck = dirent->ck;
+
+				PTHREAD_RWLOCK_unlock(&directory->content_lock);
+				PTHREAD_RWLOCK_wrlock(&directory->content_lock);
+				has_write = true;
+
+				/* Since we have dropped the lock, if we are
+				 * using dirent name as cookie, we can't assume
+				 * anything about the dirent cache, so we may
+				 * need to rescan (see logic above that uses
+				 * first pass. We also can no longer trust the
+				 * chunk pointer. Note that if this chunk is
+				 * actually discarded, we will read a new
+				 * chunk that may not start at the same place as
+				 * the previous chunk.
+				 */
+				first_pass = true;
+				chunk = NULL;
+
+				/* Now we need to look for this dirent again.
+				 * We haven't updated next_ck for this dirent
+				 * yet, so it is the right whence to use for a
+				 * repopulation readdir if the chunk is
+				 * discarded.
+				 */
+				goto again;
+			}
+
 			status = mdc_lookup_uncached(directory, dirent->name,
 						     &entry, NULL);
 
