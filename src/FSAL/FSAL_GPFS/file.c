200122903d116a378d86b6c8f6c70244f3aafefd
Malahal Naineni <malahal@us.ibm.com>
2017-09-12 15:57:08 +0530

    [GPFS] return first failure from gpfs_read2 and gpfs_write2
    Both functions may return failure from closing a temporary file
    descriptor instead of the actual read/write operation.
    
    Also gpfs_lock_op2() should never receive closefd as true, so it now
    logs a critical message.
    
    Change-Id: I1425d667b5476dc17d44ea7c9889af7c4fc5a1fb
    Reported-by: Sachin Punadikar <psachin@in.ibm.com>
    Signed-off-by: Malahal Naineni <malahal@us.ibm.com>

--- src/FSAL/FSAL_GPFS/file.c
+++ src/FSAL/FSAL_GPFS/file.c
@@ -759,8 +759,16 @@ gpfs_read2(struct fsal_obj_handle *obj_hdl, bool bypass, struct state_t *state,
 					read_amount, end_of_file,
 					export_fd);
 
-	if (closefd)
-		status = fsal_internal_close(my_fd, NULL, 0);
+	if (closefd) {
+		fsal_status_t status2;
+
+		status2 = fsal_internal_close(my_fd, NULL, 0);
+		if (FSAL_IS_ERROR(status2)) {
+			LogEvent(COMPONENT_FSAL,
+				 "fsal close failed, fd:%d, error: %s",
+				 my_fd, msg_fsal_err(status2.major));
+		}
+	}
 
 	if (has_lock)
 		PTHREAD_RWLOCK_unlock(&obj_hdl->obj_lock);
@@ -832,8 +840,16 @@ gpfs_write2(struct fsal_obj_handle *obj_hdl, bool bypass, struct state_t *state,
 				wrote_amount, fsal_stable, op_ctx,
 				export_fd);
 
-	if (closefd)
-		fsal_internal_close(my_fd, NULL, 0);
+	if (closefd) {
+		fsal_status_t status2;
+
+		status2 = fsal_internal_close(my_fd, NULL, 0);
+		if (FSAL_IS_ERROR(status2)) {
+			LogEvent(COMPONENT_FSAL,
+				 "fsal close failed, fd:%d, error: %s",
+				 my_fd, msg_fsal_err(status2.major));
+		}
+	}
 
 	if (has_lock)
 		PTHREAD_RWLOCK_unlock(&obj_hdl->obj_lock);
@@ -1067,15 +1083,11 @@ gpfs_lock_op2(struct fsal_obj_handle *obj_hdl, struct state_t *state,
 	status = GPFSFSAL_lock_op(export, lock_op, req_lock, conflicting_lock,
 				  &gpfs_sg_arg);
 
+	/* we shouldn't get a temporary fd for a lock request */
 	if (closefd) {
-		fsal_status_t status2;
-
-		status2 = fsal_internal_close(glock_args.lfd, NULL, 0);
-		if (FSAL_IS_ERROR(status2)) {
-			LogEvent(COMPONENT_FSAL,
-				 "fsal close failed, fd:%d, error: %s",
-				 glock_args.lfd, msg_fsal_err(status2.major));
-		}
+		LogCrit(COMPONENT_FSAL,
+			"find_fd returned closefd for a lock request");
+		(void)fsal_internal_close(glock_args.lfd, NULL, 0);
 	}
 
 	if (has_lock)
