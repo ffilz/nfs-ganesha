78a4030b6808dc33570d635cf4dd6d1978f63d2c
Soumya Koduri <skoduri@redhat.com>
2017-09-21 13:03:09 +0530

    Allow cancellation of upcall threads even if not ready
    There could be cases where in we spawn upcall thread but
    may have to destroy it before it is ready due to failures
    during export creation and initialization. Add a new boolean
    'up_cancel' to allow the same.
    
    Change-Id: I46946da83d014e8218bfe59b925c2d103b669f5b
    Signed-off-by: Soumya Koduri <skoduri@redhat.com>

--- src/FSAL_UP/fsal_up_top.c
+++ src/FSAL_UP/fsal_up_top.c
@@ -1624,11 +1624,20 @@ void up_ready_set(struct fsal_up_vector *up_ops)
 	PTHREAD_MUTEX_unlock(&up_ops->up_mutex);
 }
 
+/* This is to cancel waiting and resume Upcall threads */
+void up_ready_cancel(struct fsal_up_vector *up_ops)
+{
+	PTHREAD_MUTEX_lock(&up_ops->up_mutex);
+	up_ops->up_cancel = true;
+	pthread_cond_broadcast(&up_ops->up_cond);
+	PTHREAD_MUTEX_unlock(&up_ops->up_mutex);
+}
+
 /* Upcall threads should call this to wait for upcall readiness */
 void up_ready_wait(struct fsal_up_vector *up_ops)
 {
 	PTHREAD_MUTEX_lock(&up_ops->up_mutex);
-	while (!up_ops->up_ready)
+	while (!up_ops->up_ready && !up_ops->up_cancel)
 		pthread_cond_wait(&up_ops->up_cond, &up_ops->up_mutex);
 	PTHREAD_MUTEX_unlock(&up_ops->up_mutex);
 }
