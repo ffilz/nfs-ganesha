ecda6d70f279ff9d06142b17df14c9f441601886
Frank S. Filz <ffilzlnx@mindspring.com>
2017-12-13 21:49:01 -0800

    NFS v4.1 - use cachethis indication from client
    Change the name of use_drc to use_slot_cached_result to clarify
    what is being directed.
    
    Remove cache_used (it is a duplicate of res_cached inside
    struct COMPOUND4res_extended.
    
    Rename cached_res in compound_data_t to cached_result for
    simplicity (now cscope will find both cached_result in the slot and
    in the compound data).
    
    Note that the cached result is a small structure, the array of
    results for each compound op is referenced by pointer. The function
    nfs4_Compound_Free does a deep free only if the response is not
    cached, and thus, any place the slot cache needs to be cleaned up
    is simply accomplished by clearing res_cached and calling
    nfs4_Compound_Free.
    
    The logic changes in this patch are confined to:
    
    Protocols/NFS/nfs4_op_sequence.c which checks cachethis before
    setting up the cache, and cleaning up the slot cache if cachethis is
    false since it will no longer be referenced (and could cause
    confusion on the next request that is cached).
    
    SAL/nfs41_session_id.c which makes sure the slot cache is cleaned
    up on session destroy.
    
    Change-Id: Icf5d0fb9349b8eb2f6bb26103959b55918616226
    Signed-off-by: Frank S. Filz <ffilzlnx@mindspring.com>

--- src/SAL/nfs41_session_id.c
+++ src/SAL/nfs41_session_id.c
@@ -37,6 +37,7 @@
 #include "config.h"
 #include "nfs_core.h"
 #include "sal_functions.h"
+#include "nfs_proto_functions.h"
 
 /**
  * @brief Pool for allocating session data
@@ -254,8 +255,16 @@ int32_t dec_session_ref(nfs41_session_t *session)
 		dec_client_id_ref(session->clientid_record);
 		/* Destroy this session's mutexes and condition variable */
 
-		for (i = 0; i < session->nb_slots; i++)
-			PTHREAD_MUTEX_destroy(&session->fc_slots[i].lock);
+		for (i = 0; i < session->nb_slots; i++) {
+			nfs41_session_slot_t *slot = &session->fc_slots[i];
+
+			if (slot->cached_result.res_cached) {
+				slot->cached_result.res_cached = false;
+				nfs4_Compound_Free((nfs_res_t *)
+					&slot->cached_result);
+			}
+			PTHREAD_MUTEX_destroy(&slot->lock);
+		}
 
 		PTHREAD_COND_destroy(&session->cb_cond);
 		PTHREAD_MUTEX_destroy(&session->cb_mutex);
