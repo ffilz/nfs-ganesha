d5afce8335d2786db3366bb05e5edb158691eb2b
Pradeep <pradeepthomas@gmail.com>
2018-01-31 09:14:37 -0800

    Move dupreq2_pkginit() before nfs_Init_svc().
    Change-Id: I8fe7299c59e8fafde87b0e4ff622e40596a5b958
    Signed-off-by: Pradeep <pradeepthomas@gmail.com>

--- src/MainNFSD/nfs_init.c
+++ src/MainNFSD/nfs_init.c
@@ -722,6 +722,11 @@ static void nfs_Init(const nfs_start_info_t *p_start_info)
 	}			/*  if( nfs_param.krb5_param.active_krb5 ) */
 #endif				/* HAVE_KRB5 */
 #endif				/* _HAVE_GSSAPI */
+	/* Init duplicate request cache */
+	dupreq2_pkginit();
+	LogInfo(COMPONENT_INIT,
+		"duplicate request hash table cache successfully initialized");
+
 
 	/* RPC Initialisation - exits on failure */
 	nfs_Init_svc();
@@ -739,11 +744,6 @@ static void nfs_Init(const nfs_start_info_t *p_start_info)
 	}
 	LogInfo(COMPONENT_INIT,
 		"NFSv4 clientid cache successfully initialized");
-
-	/* Init duplicate request cache */
-	dupreq2_pkginit();
-	LogInfo(COMPONENT_INIT,
-		"duplicate request hash table cache successfully initialized");
 
 	/* Init The NFSv4 State id cache */
 	LogDebug(COMPONENT_INIT, "Now building NFSv4 State Id cache");
