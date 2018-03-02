308b45e9d605f08ecd39da5bb5dab4d065dee277
Jeff Layton <jlayton@redhat.com>
2018-03-02 13:44:59 -0500

    RADOS_KV: make rados_kv_traverse callback more generic
    We have a struct to package all the arguments, but then we pass all of
    the arguments individually. Have the rados_kv_traverse callback take a
    void pointer and then just have the callback cast it to the right sort
    of struct to get the arguments.
    
    While we're in there, also get rid of the useless pop_args_t typedef.
    
    This is mainly just a cleanup to untangle the API a bit, but it also
    allows us to potentially pass new and different sorts of callbacks to
    rados_kv_traverse.
    
    Change-Id: Ib9141a79313982577a41b831630d4166740c1a86
    Signed-off-by: Jeff Layton <jlayton@redhat.com>

--- src/SAL/recovery/recovery_rados.h
+++ src/SAL/recovery/recovery_rados.h
@@ -39,14 +39,15 @@ struct rados_kv_parameter {
 };
 extern struct rados_kv_parameter rados_kv_param;
 
-typedef void (*pop_clid_entry_t)(char *, char*, add_clid_entry_hook,
-				 add_rfh_entry_hook, bool old, bool takeover);
-typedef struct pop_args {
+/* Callback for rados_kv_traverse */
+typedef void (*traverse_callback_t)(char *, char *, void *);
+
+struct pop_args {
 	add_clid_entry_hook add_clid_entry;
 	add_rfh_entry_hook add_rfh_entry;
 	bool old;
 	bool takeover;
-} *pop_args_t;
+};
 
 int rados_kv_connect(rados_ioctx_t *io_ctx, const char *userid,
 			const char *conf, const char *pool);
@@ -54,7 +55,7 @@ void rados_kv_shutdown(void);
 int rados_kv_get(char *key, char *val, char *object);
 void rados_kv_create_key(nfs_client_id_t *clientid, char *key);
 void rados_kv_create_val(nfs_client_id_t *clientid, char *val);
-int rados_kv_traverse(pop_clid_entry_t pop_func, pop_args_t pop_args,
+int rados_kv_traverse(traverse_callback_t callback, void *args,
 			const char *object);
 void rados_kv_append_val_rdfh(char *val, char *rdfh, int rdfh_len);
 #endif	/* _RECOVERY_RADOS_H */
