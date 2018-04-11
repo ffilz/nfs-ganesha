aa205e8ba6b9545b1700af4c159e1faadbf816ee
Jeff Layton <jlayton@redhat.com>
2018-04-11 11:02:26 -0400

    FSAL_CEPH: kill off old session before the mount
    Add a test for the new cephfs reclaim infrastructure. We don't actually
    try to reclaim at this point, only ask the MDS to destroy the old
    session. For now, we just use the hostname as the uuid here.
    
    The libcephfs interface is still under debate, so this may look quite
    different in its final form.
    
    Change-Id: I70e93cbff2e2ae80989aaa9ee9c96d3031f5e919
    Signed-off-by: Jeff Layton <jlayton@redhat.com>

--- src/include/config-h.in.cmake
+++ src/include/config-h.in.cmake
@@ -63,6 +63,7 @@
 #cmakedefine USE_FSAL_CEPH_STATX 1
 #cmakedefine USE_FSAL_CEPH_LL_DELEGATION 1
 #cmakedefine USE_FSAL_CEPH_LL_SYNC_INODE 1
+#cmakedefine USE_FSAL_CEPH_RECLAIM_RESET 1
 #cmakedefine USE_FSAL_RGW_MOUNT2 1
 #cmakedefine ENABLE_LOCKTRACE 1
 #cmakedefine SANITIZE_ADDRESS 1
