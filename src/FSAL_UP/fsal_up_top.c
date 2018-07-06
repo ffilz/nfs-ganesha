ffilz/nfs-ganesha

commit 81caa34f9acafd65a8c1e36f42bcbd9f5a88fd9c

set op_ctx during deleg revokes and recalls
[ jlayton: squashed patches, uninitialized var fixes ]

Change-Id: I22ec21c368aec46f9e83012fd90f6be0d83ece0c
Signed-off-by: Soumya Koduri <skoduri@redhat.com>
Signed-off-by: Jeff Layton <jlayton@redhat.com>

src/FSAL_UP/fsal_up_top.c
@@ -1122,6 +1122,9 @@ static int32_t delegrecall_completion_func(rpc_call_t *call,
 	char str[LOG_BUFF_LEN] = "\0";
 	struct display_buffer dspbuf = {sizeof(str), str, str};
 	CB_RECALL4args *opcbrecall;
+	struct req_op_context *save_ctx = op_ctx, req_ctx = {0};
+	struct gsh_export *export = NULL;
+	bool ret = false;
 
 	LogDebug(COMPONENT_NFS_CB, "%p %s", call,
 		 (hook == RPC_CALL_COMPLETE) ? "Success" : "Failed");
@@ -1133,12 +1136,18 @@ static int32_t delegrecall_completion_func(rpc_call_t *call,
 		goto out_free_drc;
 	}
 
-	obj = get_state_obj_ref(state);
+	ret = get_state_obj_export_owner_refs(state, &obj,
+					     &export,
+					     NULL);
 
-	if (obj == NULL) {
+	if (!ret || obj == NULL) {
 		LogDebug(COMPONENT_NFS_CB, "Stale file");
 		goto out_free_drc;
 	}
+
+	op_ctx = &req_ctx;
+	op_ctx->ctx_export = export;
+	op_ctx->fsal_export = export->fsal_export;
 
 	if (isDebug(COMPONENT_NFS_CB)) {
 		char str[LOG_BUFF_LEN] = "\0";
@@ -1228,6 +1237,7 @@ out_free:
 	if (state != NULL)
 		dec_state_t_ref(state);
 
+	op_ctx = save_ctx;
 	return 0; /*Always return zero, the delegation is recalled or revoked */
 }
 
@@ -1334,6 +1344,9 @@ static void delegrevoke_check(void *ctx)
 	char str[LOG_BUFF_LEN] = "\0";
 	struct display_buffer dspbuf = {sizeof(str), str, str};
 	bool str_valid = false;
+	struct req_op_context *save_ctx = op_ctx, req_ctx = {0};
+	struct gsh_export *export = NULL;
+	bool   ret = false;
 
 	state = nfs4_State_Get_Pointer(deleg_ctx->drc_stateid.other);
 
@@ -1347,12 +1360,19 @@ static void delegrevoke_check(void *ctx)
 		str_valid = true;
 	}
 
-	obj = get_state_obj_ref(state);
+	ret = get_state_obj_export_owner_refs(state, &obj,
+					     &export,
+					     NULL);
 
-	if (obj == NULL) {
+	if (!ret || obj == NULL) {
 		LogDebug(COMPONENT_NFS_CB, "Stale file");
 		goto out;
 	}
+
+	/* op_ctx may be used by state_del_locked and others */
+	op_ctx = &req_ctx;
+	op_ctx->ctx_export = export;
+	op_ctx->fsal_export = export->fsal_export;
 
 	if (eval_deleg_revoke(state)) {
 		if (str_valid)
@@ -1391,6 +1411,8 @@ static void delegrevoke_check(void *ctx)
 
 	if (state != NULL)
 		dec_state_t_ref(state);
+
+	op_ctx = save_ctx;
 }
 
 static void delegrecall_task(void *ctx)
@@ -1398,6 +1420,9 @@ static void delegrecall_task(void *ctx)
 	struct delegrecall_context *deleg_ctx = ctx;
 	struct state_t *state;
 	struct fsal_obj_handle *obj;
+	bool   ret = false;
+	struct req_op_context *save_ctx, req_ctx = {0};
+	struct gsh_export *export = NULL;
 
 	state = nfs4_State_Get_Pointer(deleg_ctx->drc_stateid.other);
 
@@ -1405,15 +1430,25 @@ static void delegrecall_task(void *ctx)
 		LogDebug(COMPONENT_NFS_CB, "Delgation is already returned");
 		free_delegrecall_context(deleg_ctx);
 	} else {
-		obj = get_state_obj_ref(state);
+		ret = get_state_obj_export_owner_refs(state, &obj,
+						     &export,
+						     NULL);
 
-		if (obj != NULL) {
-			delegrecall_one(obj, state, deleg_ctx);
-
-		} else {
+		if (!ret || obj == NULL) {
 			LogDebug(COMPONENT_NFS_CB,
 				 "Delgation recall skipped due to stale file");
+			goto out;
 		}
+
+		/* op_ctx may be used by state_del_locked and others */
+		save_ctx = op_ctx;
+		op_ctx = &req_ctx;
+		op_ctx->ctx_export = export;
+		op_ctx->fsal_export = export->fsal_export;
+		delegrecall_one(obj, state, deleg_ctx);
+		op_ctx = save_ctx;
+
+out:
 		dec_state_t_ref(state);
 	}
 }
@@ -1456,6 +1491,7 @@ state_status_t delegrecall_impl(struct fsal_obj_handle *obj)
 	struct state_t *state;
 	state_owner_t *owner;
 	struct delegrecall_context *drc_ctx;
+	struct req_op_context *save_ctx = op_ctx, req_ctx = {0};
 
 	LogDebug(COMPONENT_FSAL_UP,
 		 "FSAL_UP_DELEG: obj %p type %u",
@@ -1501,6 +1537,11 @@ state_status_t delegrecall_impl(struct fsal_obj_handle *obj)
 			continue;
 		}
 
+		/* op_ctx may be used by state_del_locked and others */
+		op_ctx = &req_ctx;
+		op_ctx->ctx_export = drc_ctx->drc_exp;
+		op_ctx->fsal_export = drc_ctx->drc_exp->fsal_export;
+
 		drc_ctx->drc_clid = owner->so_owner.so_nfs4_owner.so_clientrec;
 		COPY_STATEID(&drc_ctx->drc_stateid, state);
 		inc_client_id_ref(drc_ctx->drc_clid);
@@ -1527,6 +1568,8 @@ state_status_t delegrecall_impl(struct fsal_obj_handle *obj)
 		delegrecall_one(obj, state, drc_ctx);
 	}
 	PTHREAD_RWLOCK_unlock(&obj->state_hdl->state_lock);
+
+	op_ctx = save_ctx;
 	return rc;
 }
 

src/MainNFSD/nfs_worker_thread.c
@@ -1593,7 +1593,11 @@ static void worker_run(struct fridgethr_context *ctx)
 		case NFS_CALL:
 			/* NFSv4 rpc call (callback) */
 			nfs_rpc_dispatch_call(&reqdata->r_u.call, 0);
-			break;
+
+			/* Callback completion functions take care of
+			 * freeing rpc call & reqdata
+			 */
+			return;
 
 #ifdef _USE_9P
 		case _9P_REQUEST:

src/SAL/nfs4_state.c
@@ -841,6 +841,9 @@ void revoke_owner_delegs(state_owner_t *client_owner)
 	state_t *state, *first;
 	struct fsal_obj_handle *obj;
 	bool so_mutex_held;
+	struct gsh_export *export = NULL;
+	struct req_op_context *save_ctx, req_ctx = {0};
+	bool ok;
 
  again:
 	first = NULL;
@@ -878,9 +881,10 @@ void revoke_owner_delegs(state_owner_t *client_owner)
 		 * even after state_deleg_revoke releases the reference it
 		 * holds.
 		 */
-		obj = get_state_obj_ref(state);
+		ok = get_state_obj_export_owner_refs(state, &obj, &export,
+						     NULL);
 
-		if (obj == NULL) {
+		if (!ok || obj == NULL) {
 			LogDebug(COMPONENT_STATE,
 				 "Stale state or file");
 			continue;
@@ -893,7 +897,15 @@ void revoke_owner_delegs(state_owner_t *client_owner)
 		 * state_del_locked which is called from deleg_revoke.
 		 */
 		PTHREAD_RWLOCK_wrlock(&obj->state_hdl->state_lock);
+
+		/* op_ctx may be used by state_del_locked and others */
+		save_ctx = op_ctx;
+		op_ctx = &req_ctx;
+		op_ctx->ctx_export = export;
+		op_ctx->fsal_export = export->fsal_export;
+
 		state_deleg_revoke(obj, state);
+		op_ctx = save_ctx;
 		PTHREAD_RWLOCK_unlock(&obj->state_hdl->state_lock);
 
 		/* Since we dropped so_mutex, we must restart the loop. */

commit e37291006bb125ccfeaa9208d0fc87d96a087ef8

FSAL_UP: fix op_ctx handling in delegrecall_task
delegrecall_task calls dec_state_t_ref at the end of it, which can do a
subcall_task call. That requires a valid op_ctx pointer, but we will
have already reset op_ctx back to its original value at that point
(which may very well be NULL).

Fix the code to set the op_ctx prior to setting getting the export and
obj refs. And then just populate the export fields once we get the
appropriate references.

Also reduce some unnecessary indentation by eliminating an else block.

Change-Id: I2718515fe12448209502ba561f5bb2c40248faa7
Reported-by: Jiffin Tony Thottan <thottanjiffin@gmail.com>
Cc: Soumya Koduri <skoduri@redhat.com>
Fixes: 81caa34f9aca (set op_ctx during deleg revokes and recalls)
Signed-off-by: Jeff Layton <jlayton@redhat.com>

src/FSAL_UP/fsal_up_top.c
@@ -1423,33 +1423,33 @@ static void delegrecall_task(void *ctx)
 	if (state == NULL) {
 		LogDebug(COMPONENT_NFS_CB, "Delgation is already returned");
 		free_delegrecall_context(deleg_ctx);
-	} else {
-		ret = get_state_obj_export_owner_refs(state, &obj,
-						     &export,
-						     NULL);
-
-		if (!ret || obj == NULL) {
-			LogDebug(COMPONENT_NFS_CB,
-				 "Delgation recall skipped due to stale file");
-			goto out;
-		}
-
-		/* op_ctx may be used by state_del_locked and others */
-		save_ctx = op_ctx;
-		op_ctx = &req_ctx;
-		op_ctx->ctx_export = export;
-		op_ctx->fsal_export = export->fsal_export;
-		delegrecall_one(obj, state, deleg_ctx);
-
-		/* Release the obj ref and export ref. */
-		obj->obj_ops.put_ref(obj);
-		put_gsh_export(export);
-
-		op_ctx = save_ctx;
-
-out:
-		dec_state_t_ref(state);
+		return;
 	}
+
+	/* op_ctx may be used by state_del_locked and others */
+	save_ctx = op_ctx;
+	op_ctx = &req_ctx;
+
+	ret = get_state_obj_export_owner_refs(state, &obj,
+					     &export,
+					     NULL);
+	if (!ret || obj == NULL) {
+		LogDebug(COMPONENT_NFS_CB,
+			 "Delgation recall skipped due to stale file");
+		goto out;
+	}
+
+	op_ctx->ctx_export = export;
+	op_ctx->fsal_export = export->fsal_export;
+
+	delegrecall_one(obj, state, deleg_ctx);
+
+	/* Release the obj ref and export ref. */
+	obj->obj_ops.put_ref(obj);
+	put_gsh_export(export);
+out:
+	dec_state_t_ref(state);
+	op_ctx = save_ctx;
 }
 
 static int schedule_delegrecall_task(struct delegrecall_context *ctx,