04b5343812289b9ed32820e4bf220b46a5dcd7f8
Kinglong Mee <kinglongmee@gmail.com>
2018-08-30 20:18:02 +0800

    FSAL_GLUSTER: copy garry from tmp2_fd when resuing open state fd
    Also fix using bad my_fd->creds.caller_glen (zero) as the gsh_memdup length.
    
    Update the gsh_memdup arguments/return as memdup in linux kernel,
    
    void *memdup(const void *src, size_t len)
    
    Change-Id: Id729d0a873a2740af9e55dc096e06397edf25b02
    Signed-off-by: Kinglong Mee <kinglongmee@gmail.com>

--- src/include/abstract_mem.h
+++ src/include/abstract_mem.h
@@ -273,8 +273,8 @@ gsh_strldup__(const char *s, size_t length, size_t *copied,
  *
  * @return Pointer to new copy of data
  */
-static inline char *
-gsh_memdup__(const char *s, const size_t l, const char *file, int line,
+static inline void *
+gsh_memdup__(const void *s, const size_t l, const char *file, int line,
 		const char *function)
 {
 	char *p = (char *) gsh_malloc__(l, file, line, function);
