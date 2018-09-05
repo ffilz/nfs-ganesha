ffilz/nfs-ganesha

commit e3a13410430f2a76c4ee5dac1805b75f92849484

rados_cluster: don't lift grace period unless everyone is enforcing
When building a new cluster from scratch, we'll generally not have
any clients at first. New nodes can end up being added to the cluster,
start up and lift the grace period before existing nodes even notice
that a new grace period has started.

Under typical operation, we'll have clients that need to reclaim, but
servers with no clients can end up shortcutting the grace period, and
cause things not to move in lockstep.

Fix this by ensuring that we don't clear the NEED_GRACE flag and lift
the grace period until all nodes have begun enforcement.

Change-Id: Ie252883a094bee9f47dc7b8ae987688500a77257
Signed-off-by: Jeff Layton <jlayton@redhat.com>

src/support/rados_grace.c
@@ -564,6 +564,7 @@ rados_grace_lift_bulk(rados_ioctx_t io_ctx, const char *oid, int nodes,
 		char			buf[sizeof(uint64_t) * 2];
 		unsigned char		more = '\0';
 		uint64_t		ver;
+		bool			enforcing;
 
 		/* read epoch blob and omap keys */
 		rop = rados_create_read_op();
@@ -603,10 +604,17 @@ rados_grace_lift_bulk(rados_ioctx_t io_ctx, const char *oid, int nodes,
 		 */
 		need = 0;
 		k = 0;
+		enforcing = true;
 		for (;;) {
 			ret = rados_omap_get_next(iter, &key, &val, &len);
 			if (!key)
 				break;
+
+			/* Don't lift if anyone is not enforcing */
+			if (!(*val & RADOS_GRACE_ENFORCING)) {
+				enforcing = false;
+				break;
+			}
 
 			if (*val & RADOS_GRACE_NEED_GRACE)
 				++need;
@@ -634,6 +642,13 @@ rados_grace_lift_bulk(rados_ioctx_t io_ctx, const char *oid, int nodes,
 		};
 		rados_omap_get_end(iter);
 		rados_release_read_op(rop);
+
+		/*
+		 * We can't lift if there are cluster members that haven't
+		 * started enforcement yet. Wait until they catch up.
+		 */
+		if (!enforcing)
+			goto out;
 
 		/* Ensure that all given nodes have a key in the omap */
 		for (i = 0; i < nodes; ++i) {
@@ -682,11 +697,11 @@ rados_grace_lift_bulk(rados_ioctx_t io_ctx, const char *oid, int nodes,
 		if (ret >= 0)
 			rados_grace_notify(io_ctx, oid);
 	} while (ret == -ERANGE);
+out:
 	if (!ret) {
 		*pcur = cur;
 		*prec = rec;
 	}
-out:
 	free(match);
 	free(vals);
 	free(lens);

commit e3cb6b6e54c1a74f1beb8f7dc41f84d59d78a6fa

rados_grace: allow lift/remove to proceed if we're not in a grace period
There's no need to deny a lift operation if we're not in a grace period
in the first place. Doing so also prevents shrinking the cluster when
we're not in a grace period.

Fixes: e3a13410430f rados_cluster: don't lift grace period unless everyone is enforcing
Change-Id: I395c3a970c0511a8628dd1c5c7e74368f76c865c
Signed-off-by: Jeff Layton <jlayton@redhat.com>

src/support/rados_grace.c
@@ -610,11 +610,9 @@ rados_grace_lift_bulk(rados_ioctx_t io_ctx, const char *oid, int nodes,
 			if (!key)
 				break;
 
-			/* Don't lift if anyone is not enforcing */
-			if (!(*val & RADOS_GRACE_ENFORCING)) {
+			/* Make note if anyone is not enforcing */
+			if (!(*val & RADOS_GRACE_ENFORCING))
 				enforcing = false;
-				break;
-			}
 
 			if (*val & RADOS_GRACE_NEED_GRACE)
 				++need;
@@ -644,10 +642,11 @@ rados_grace_lift_bulk(rados_ioctx_t io_ctx, const char *oid, int nodes,
 		rados_release_read_op(rop);
 
 		/*
-		 * We can't lift if there are cluster members that haven't
-		 * started enforcement yet. Wait until they catch up.
+		 * We can't lift if we're in a grace period and there are
+		 * cluster members that haven't started enforcement yet. Wait
+		 * until they catch up.
 		 */
-		if (!enforcing)
+		if (rec && !enforcing)
 			goto out;
 
 		/* Ensure that all given nodes have a key in the omap */