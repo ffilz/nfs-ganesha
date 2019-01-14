3344e139173dbb4ce0c51f120737a642bae83fdf
Soumya Koduri <skoduri@redhat.com>
2019-01-15 01:34:10 +0530

    Recall write delegation only if it conflicts with other clients` access
    NFS client should be able to perform some operations like setattr or
    reopen with different claim type while still holding write delegation.
    Hence the server should check and recall delegations only if there is
    conflicting access from other clients.
    
    Added a new field in state_hdl->file structure to store the client
    which has been granted write delegation. This is compared with
    op_ctx->client to check if its the same client performing operations.
    Also included a helper function to verify if the file has been write
    delegated and if yes to return the client pointer.
    
    Change-Id: Ifbe10bd3350fb9553a29b3b7ab13c6812c99b1fa
    Signed-off-by: Soumya Koduri <skoduri@redhat.com>

--- src/include/sal_functions.h
+++ src/include/sal_functions.h
@@ -905,6 +905,8 @@ state_status_t delegrecall_impl(struct fsal_obj_handle *obj);
 nfsstat4 deleg_revoke(struct fsal_obj_handle *obj, struct state_t *deleg_state);
 void state_deleg_revoke(struct fsal_obj_handle *obj, state_t *state);
 bool state_deleg_conflict(struct fsal_obj_handle *obj, bool write);
+bool is_write_delegated(struct fsal_obj_handle *obj,
+			struct gsh_client **client);
 
 /******************************************************************************
  *
