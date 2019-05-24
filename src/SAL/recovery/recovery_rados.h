2cb23e018811ce656d8095941e0eba6b2d4f3d13
Frank S. Filz <ffilzlnx@mindspring.com>
2019-05-24 15:04:57 -0700

    SAL recovery - safe string manipulation
    Also, unify to show strerror(errno), errno as %s (%d).
    
    Change-Id: I1825de84b31e7762cb78dcbd075b0321cea23846
    Signed-off-by: Frank S. Filz <ffilzlnx@mindspring.com>

--- src/SAL/recovery/recovery_rados.h
+++ src/SAL/recovery/recovery_rados.h
@@ -65,7 +65,7 @@ void rados_kv_add_clid(nfs_client_id_t *clientid);
 void rados_kv_rm_clid(nfs_client_id_t *clientid);
 void rados_kv_add_revoke_fh(nfs_client_id_t *delr_clid, nfs_fh4 *delr_handle);
 void rados_kv_create_key(nfs_client_id_t *clientid, char *key);
-void rados_kv_create_val(nfs_client_id_t *clientid, char *val);
+char *rados_kv_create_val(nfs_client_id_t *clientid, size_t *len);
 int rados_kv_traverse(pop_clid_entry_t callback, struct pop_args *args,
 			const char *object);
 void rados_kv_add_revoke_fh(nfs_client_id_t *delr_clid, nfs_fh4 *delr_handle);
