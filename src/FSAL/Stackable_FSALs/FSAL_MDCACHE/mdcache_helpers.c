e6a1b6ee748044b1438e69d543251b1bcdedca64
ashish.sangwan <ashish.sangwan@nutanix.com>
2019-05-31 12:43:17 -0700

    Free the extra ref taken by the dirent on the mdcache entry even if the content lock is held in read mode.
    Not freeing the extra ref leads to memory growth as we are not able to
    prune the mdcache entries with ref count >= 2.
    Though we expect that the ref will be released on a invalidate call,
    but invalidation of the dirents only happen when we either add/delete any
    directory entry. Otherwise the dirent stays in the cache pinning the
    correspodning mdcache entry.
    
    Signed-off-by: Ashish Sangwan <ashishsangwan2@gmail.com>
    Change-Id: I48c6e6f02705a067e3ce0fcaa2177e069ddd53f1

--- src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_helpers.c
+++ src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_helpers.c
@@ -3000,16 +3000,20 @@ again:
 
 		status.major = ERR_FSAL_NO_ERROR;
 		/* We have the content_lock for at least read. */
-		if (dirent->entry) {
-			/* Take a ref for our use */
-			entry = dirent->entry;
-			mdcache_get(entry);
-		} else {
-			/* Not cached, get actual entry using the dirent ckey */
-			status = mdcache_find_keyed_reason(&dirent->ckey,
-							   &entry,
-							   MDC_REASON_SCAN);
+		entry = dirent->entry;
+		if (entry) {
+			void *desired = NULL;
+
+			if (has_write ||
+			    atomic_cmp_and_xchg((void **)&dirent->entry,
+						(void **)&entry,
+						 &desired)) {
+				mdcache_put(entry);
+				dirent->entry = NULL;
+			}
 		}
+		status = mdcache_find_keyed_reason(&dirent->ckey,
+						   &entry, MDC_REASON_SCAN);
 
 		if (FSAL_IS_ERROR(status)) {
 			/* Failed using ckey, do full lookup. */
@@ -3101,19 +3105,6 @@ again:
 				 */
 				goto restart;
 			}
-		}
-
-		if (has_write && dirent->entry) {
-			/* If we get here, we have the write lock, have an
-			 * entry, and took a ref on it above.  The dirent also
-			 * has a ref on the entry.  Drop that ref now.  This can
-			 * only be done under the write lock.  If we don't have
-			 * the write lock, then this was not the readdir that
-			 * took the ref, and another readdir will drop the ref,
-			 * or it will be dropped when the dirent is cleaned up.
-			 * */
-			mdcache_put(dirent->entry);
-			dirent->entry = NULL;
 		}
 
 		if (reload_chunk && look_ck != 0 && dirent->ck !=
