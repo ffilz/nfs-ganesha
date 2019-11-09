7eb6b2413c3d053478e20b7d974a21ea50ffbbdd
Xi Jinyu <xijinyu@cmss.chinamobile.com>
2019-11-09 05:17:00 -0500

    Unsigned integer is positive.
    Signed-off-by: Xi Jinyu <xijinyu@cmss.chinamobile.com>
    Change-Id: Ic20a387a1d585eec524cd75d0e9ab69a2e889d61

--- src/SAL/state_deleg.c
+++ src/SAL/state_deleg.c
@@ -522,9 +522,6 @@ bool is_write_delegated(struct fsal_obj_handle *obj, nfs_client_id_t **client)
 
 	deleg_stats = &obj->state_hdl->file.fdeleg_stats;
 
-	if (deleg_stats->fds_curr_delegations < 0)
-		return false;
-
 	write_delegated = obj->state_hdl->file.write_delegated;
 	if (write_delegated && client) {
 		*client = obj->state_hdl->file.write_deleg_client;
