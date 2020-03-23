442167abcc4aaa4912e01c110a43a57adcc0af93
Frank S. Filz <ffilzlnx@mindspring.com>
2020-03-23 15:30:29 -0700

    Make fullpath and pseudopath gsh_refstr and keep a ref in op_ctx
    The pseudopath is going to be changeable in a following patch so we
    must make a stable version easily accessible. Use gsh_refstr to
    accomplish that.
    
    Also, for consistency handle fullpath the same way. It may actually
    be possible to allow changing of it also and in the meantime, code
    that picks one or the other of pseudopath and fullpath will not have
    to deal with different rules for access to those strings.
    
    Change-Id: I40270796f6508f1d2f9cc7659347ab24189a03ff
    Signed-off-by: Frank S. Filz <ffilzlnx@mindspring.com>

--- src/Protocols/NFS/nfs4_op_lookupp.c
+++ src/Protocols/NFS/nfs4_op_lookupp.c
@@ -102,7 +102,7 @@ enum nfs_req_result nfs4_op_lookupp(struct nfs_argop4 *op,
 		LogDebug(COMPONENT_EXPORT,
 			 "Handling reverse junction from Export_Id %d Pseudo %s Parent=%p",
 			 original_export->export_id,
-			 original_export->pseudopath,
+			 CTX_PSEUDOPATH(op_ctx),
 			 original_export->exp_parent_exp);
 
 		if (original_export->exp_parent_exp == NULL) {
@@ -148,7 +148,7 @@ enum nfs_req_result nfs4_op_lookupp(struct nfs_argop4 *op,
 			LogCrit(COMPONENT_EXPORT,
 				"Reverse junction from Export_Id %d Pseudo %s Parent=%p is stale",
 				original_export->export_id,
-				original_export->pseudopath,
+				CTX_PSEUDOPATH(op_ctx),
 				parent_exp);
 			res_LOOKUPP4->status = NFS4ERR_STALE;
 			return NFS_REQ_ERROR;
@@ -166,10 +166,14 @@ enum nfs_req_result nfs4_op_lookupp(struct nfs_argop4 *op,
 		/* Put our ref */
 		dir_obj->obj_ops->put_ref(dir_obj);
 
+		/* We are done with the original export's gsh_refstr */
+		ctx_put_exp_paths(op_ctx);
+
 		/* Stash parent export in opctx while still holding the lock.
 		 */
 		op_ctx->ctx_export = parent_exp;
 		op_ctx->fsal_export = op_ctx->ctx_export->fsal_export;
+		ctx_get_exp_paths(op_ctx);
 
 		/* Now we are safely transitioned to the parent export and can
 		 * release the lock.
@@ -192,7 +196,7 @@ enum nfs_req_result nfs4_op_lookupp(struct nfs_argop4 *op,
 			LogDebug(COMPONENT_EXPORT,
 				 "NFS4ERR_ACCESS Hiding Export_Id %d Pseudo %s with NFS4ERR_NOENT",
 				 parent_exp->export_id,
-				 parent_exp->pseudopath);
+				 CTX_PSEUDOPATH(op_ctx));
 			res_LOOKUPP4->status = NFS4ERR_NOENT;
 			return NFS_REQ_ERROR;
 		}
