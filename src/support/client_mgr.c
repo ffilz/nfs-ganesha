ffilz/nfs-ganesha

commit 9e806a7dc903aef22b92360ddfdf012b2cde5bf1

Rework client_mgr to use rpc_tools.c/gsh_rpc.h and related cleanup
Keep the full sockaddr_t in the gsh_client.

Instead of dynamic separate hostaddr_str buffer, since it's only 128
bytes (and maybe could be shrunk to 64) just make it part of the
structure.

Change-Id: I7e0f2dfe50d2379b8b13c784c3aee85355fe5fe9
Signed-off-by: Frank S. Filz <ffilzlnx@mindspring.com>

src/support/client_mgr.c
@@ -90,7 +90,7 @@ static struct client_by_ip client_by_ip;
  *
  * @return The computed offset.
  */
-static inline uint32_t eip_cache_offsetof(struct client_by_ip *eid, uint32_t k)
+static inline int eip_cache_offsetof(struct client_by_ip *eid, uint64_t k)
 {
 	return k % eid->cache_sz;
 }
@@ -110,10 +110,8 @@ static int client_ip_cmpf(const struct avltree_node *lhs,
 
 	lk = avltree_container_of(lhs, struct gsh_client, node_k);
 	rk = avltree_container_of(rhs, struct gsh_client, node_k);
-	if (lk->addr.len != rk->addr.len)
-		return (lk->addr.len < rk->addr.len) ? -1 : 1;
-	else
-		return memcmp(lk->addr.addr, rk->addr.addr, lk->addr.len);
+
+	return sockaddr_cmpf(&lk->cl_addrbuf, &rk->cl_addrbuf, true);
 }
 
 /**
@@ -135,61 +133,21 @@ struct gsh_client *get_gsh_client(sockaddr_t *client_ipaddr, bool lookup_only)
 	struct gsh_client *cl;
 	struct server_stats *server_st;
 	struct gsh_client v;
-	char hoststr[SOCK_NAME_MAX];
-	uint8_t *addr = NULL;
-	uint32_t ipaddr;
-	int addr_len = 0;
 	void **cache_slot;
-
-	switch (client_ipaddr->ss_family) {
-	case AF_INET:
-		addr =
-		    (uint8_t *) &((struct sockaddr_in *)client_ipaddr)->
-		    sin_addr;
-		addr_len = 4;
-		memcpy(&ipaddr,
-		       (uint8_t *) &((struct sockaddr_in *)client_ipaddr)->
-		       sin_addr, sizeof(ipaddr));
-		break;
-	case AF_INET6:
-		addr =
-		    (uint8_t *) &((struct sockaddr_in6 *)client_ipaddr)->
-		    sin6_addr;
-		addr_len = 16;
-		memcpy(&ipaddr,
-		       (uint8_t *) &((struct sockaddr_in6 *)client_ipaddr)->
-		       sin6_addr, sizeof(ipaddr));
-		break;
-#ifdef RPC_VSOCK
-	case AF_VSOCK:
-	{
-		struct sockaddr_vm *svm; /* XXX checkpatch bs */
-
-		svm = (struct sockaddr_vm *)client_ipaddr;
-		addr = (uint8_t *)&(svm->svm_cid);
-		addr_len = sizeof(svm->svm_cid);
-		ipaddr = svm->svm_cid;
-	}
-	break;
-#endif /* VSOCK */
-	default:
-		assert(0);
-	}
-	v.addr.addr = addr;
-	v.addr.len = addr_len;
+	uint64_t hash = hash_sockaddr(client_ipaddr, true);
 
 	PTHREAD_RWLOCK_rdlock(&client_by_ip.lock);
 
 	/* check cache */
 	cache_slot = (void **)
-	    &(client_by_ip.cache[eip_cache_offsetof(&client_by_ip, ipaddr)]);
+	    &(client_by_ip.cache[eip_cache_offsetof(&client_by_ip, hash)]);
 	node = (struct avltree_node *)atomic_fetch_voidptr(cache_slot);
 	if (node) {
 		if (client_ip_cmpf(&v.node_k, node) == 0) {
 			/* got it in 1 */
 			LogDebug(COMPONENT_HASHTABLE_CACHE,
 				 "client_mgr cache hit slot %d",
-				 eip_cache_offsetof(&client_by_ip, ipaddr));
+				 eip_cache_offsetof(&client_by_ip, hash));
 			cl = avltree_container_of(node, struct gsh_client,
 						  node_k);
 			goto out;
@@ -209,24 +167,21 @@ struct gsh_client *get_gsh_client(sockaddr_t *client_ipaddr, bool lookup_only)
 	}
 	PTHREAD_RWLOCK_unlock(&client_by_ip.lock);
 
-	server_st = gsh_calloc(1, (sizeof(struct server_stats) + addr_len));
+	server_st = gsh_calloc(1, sizeof(*server_st));
 
 	cl = &server_st->client;
-	memcpy(cl->addrbuf, addr, addr_len);
-	cl->addr.addr = cl->addrbuf;
-	cl->addr.len = addr_len;
-	cl->refcnt = 0;		/* we will hold a ref starting out... */
+	cl->cl_addrbuf = *client_ipaddr;
+	cl->refcnt = 0;
 
-	if (sprint_sockip(client_ipaddr, hoststr, sizeof(hoststr))) {
-		cl->hostaddr_str = gsh_strdup(hoststr);
-	} else {
-		cl->hostaddr_str = gsh_strdup("<unknown>");
+	if (!sprint_sockip(client_ipaddr, cl->hostaddr_str,
+			   sizeof(cl->hostaddr_str))) {
+		(void) strlcpy(cl->hostaddr_str, "<unknown>",
+			       sizeof(cl->hostaddr_str));
 	}
 
 	PTHREAD_RWLOCK_wrlock(&client_by_ip.lock);
 	node = avltree_insert(&cl->node_k, &client_by_ip.t);
 	if (node) {
-		gsh_free(cl->hostaddr_str);
 		gsh_free(server_st);	/* somebody beat us to it */
 		cl = avltree_container_of(node, struct gsh_client, node_k);
 	} else {
@@ -236,6 +191,7 @@ struct gsh_client *get_gsh_client(sockaddr_t *client_ipaddr, bool lookup_only)
 	}
 
  out:
+	/* we will hold a ref starting out... */
 	inc_gsh_client_refcount(cl);
 	PTHREAD_RWLOCK_unlock(&client_by_ip.lock);
 	return cl;
@@ -272,48 +228,9 @@ int remove_gsh_client(sockaddr_t *client_ipaddr)
 	struct gsh_client *cl = NULL;
 	struct server_stats *server_st;
 	struct gsh_client v;
-	uint8_t *addr = NULL;
-	uint32_t ipaddr;
-	int addr_len = 0;
 	int removed = 0;
 	void **cache_slot;
-
-	switch (client_ipaddr->ss_family) {
-	case AF_INET:
-		addr =
-		    (uint8_t *) &((struct sockaddr_in *)client_ipaddr)->
-		    sin_addr;
-		addr_len = 4;
-		memcpy(&ipaddr,
-		       (uint8_t *) &((struct sockaddr_in *)client_ipaddr)->
-		       sin_addr, sizeof(ipaddr));
-		break;
-	case AF_INET6:
-		addr =
-		    (uint8_t *) &((struct sockaddr_in6 *)client_ipaddr)->
-		    sin6_addr;
-		addr_len = 16;
-		memcpy(&ipaddr,
-		       (uint8_t *) &((struct sockaddr_in6 *)client_ipaddr)->
-		       sin6_addr, sizeof(ipaddr));
-		break;
-#ifdef RPC_VSOCK
-	case AF_VSOCK:
-	{
-		struct sockaddr_vm *svm; /* XXX checkpatch horror */
-
-		svm = (struct sockaddr_vm *)client_ipaddr;
-		addr = (uint8_t *)&(svm->svm_cid);
-		addr_len = sizeof(svm->svm_cid);
-		ipaddr = svm->svm_cid;
-	}
-	break;
-#endif /* VSOCK */
-	default:
-		assert(0);
-	}
-	v.addr.addr = addr;
-	v.addr.len = addr_len;
+	uint64_t hash = hash_sockaddr(client_ipaddr, true);
 
 	PTHREAD_RWLOCK_wrlock(&client_by_ip.lock);
 	node = avltree_lookup(&v.node_k, &client_by_ip.t);
@@ -325,7 +242,7 @@ int remove_gsh_client(sockaddr_t *client_ipaddr)
 		}
 		cache_slot = (void **)
 		    &(client_by_ip.cache[eip_cache_offsetof(
-						&client_by_ip, ipaddr)]);
+						&client_by_ip, hash)]);
 		cnode = (struct avltree_node *)atomic_fetch_voidptr(cache_slot);
 		if (node == cnode)
 			atomic_store_voidptr(cache_slot, NULL);
@@ -338,8 +255,6 @@ int remove_gsh_client(sockaddr_t *client_ipaddr)
 	if (removed == 0) {
 		server_st = container_of(cl, struct server_stats, client);
 		server_stats_free(&server_st->st);
-		if (cl->hostaddr_str != NULL)
-			gsh_free(cl->hostaddr_str);
 		gsh_free(server_st);
 	}
 	return removed;
@@ -383,7 +298,7 @@ int foreach_gsh_client(bool(*cb) (struct gsh_client *cl, void *state),
 static bool arg_ipaddr(DBusMessageIter *args, sockaddr_t *sp, char **errormsg)
 {
 	char *client_addr;
-	unsigned char addrbuf[16];
+	unsigned char cl_addrbuf[16];
 	bool success = true;
 
 	/* XXX AF_VSOCK addresses are not self-describing--and one might
@@ -398,14 +313,14 @@ static bool arg_ipaddr(DBusMessageIter *args, sockaddr_t *sp, char **errormsg)
 		*errormsg = "arg not a string";
 	} else {
 		dbus_message_iter_get_basic(args, &client_addr);
-		if (inet_pton(AF_INET, client_addr, addrbuf) == 1) {
+		if (inet_pton(AF_INET, client_addr, cl_addrbuf) == 1) {
 			sp->ss_family = AF_INET;
-			memcpy(&(((struct sockaddr_in *)sp)->sin_addr), addrbuf,
-			       4);
-		} else if (inet_pton(AF_INET6, client_addr, addrbuf) == 1) {
+			memcpy(&((struct sockaddr_in *)sp)->sin_addr,
+			       cl_addrbuf, sizeof(struct sockaddr_in));
+		} else if (inet_pton(AF_INET6, client_addr, cl_addrbuf) == 1) {
 			sp->ss_family = AF_INET6;
-			memcpy(&(((struct sockaddr_in6 *)sp)->sin6_addr),
-			       addrbuf, 16);
+			memcpy(&((struct sockaddr_in6 *)sp)->sin6_addr,
+			       cl_addrbuf, sizeof(struct sockaddr_in6));
 		} else {
 			success = false;
 			*errormsg = "can't decode client address";
@@ -510,19 +425,18 @@ static bool client_to_dbus(struct gsh_client *cl_node, void *state)
 	    (struct showclients_state *)state;
 	struct server_stats *cl;
 	char ipaddr[SOCK_NAME_MAX];
-	const char *addrp;
-	int addr_type;
 	DBusMessageIter struct_iter;
 	struct timespec last_as_ts = nfs_ServerBootTime;
 
 	cl = container_of(cl_node, struct server_stats, client);
-	addr_type = (cl_node->addr.len == 4) ? AF_INET : AF_INET6;
-	addrp =
-	    inet_ntop(addr_type, cl_node->addr.addr, ipaddr, sizeof(ipaddr));
+
+	if (!sprint_sockip(&cl_node->cl_addrbuf, ipaddr, sizeof(ipaddr)))
+		(void) strlcpy(ipaddr, "<unknown>", sizeof(ipaddr));
+
 	timespec_add_nsecs(cl_node->last_update, &last_as_ts);
 	dbus_message_iter_open_container(&iter_state->client_iter,
 					 DBUS_TYPE_STRUCT, NULL, &struct_iter);
-	dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_STRING, &addrp);
+	dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_STRING, &ipaddr);
 	server_stats_summary(&struct_iter, &cl->st);
 	dbus_append_timestamp(&struct_iter, &last_as_ts);
 	dbus_message_iter_close_container(&iter_state->client_iter,

src/FSAL/FSAL_GLUSTER/ds.c
@@ -215,8 +215,8 @@ static nfsstat4 ds_commit(struct fsal_ds_handle *const ds_pub,
 				  &op_ctx->creds->caller_gid,
 				  op_ctx->creds->caller_glen,
 				  op_ctx->creds->caller_garray,
-				  op_ctx->client->addr.addr,
-				  op_ctx->client->addr.len);
+				  socket_addr(&op_ctx->client->cl_addrbuf),
+				  socket_addr_len(&op_ctx->client->cl_addrbuf));
 
 		glfd = glfs_h_open(glfs_export->gl_fs->fs, ds->glhandle,
 				   O_RDWR);

src/FSAL/FSAL_GLUSTER/handle.c
@@ -281,8 +281,8 @@ static fsal_status_t read_dirents(struct fsal_obj_handle *dir_hdl,
 			  &op_ctx->creds->caller_gid,
 			  op_ctx->creds->caller_glen,
 			  op_ctx->creds->caller_garray,
-			  op_ctx->client->addr.addr,
-			  op_ctx->client->addr.len);
+			  socket_addr(&op_ctx->client->cl_addrbuf),
+			  socket_addr_len(&op_ctx->client->cl_addrbuf));
 
 	/** @todo : Can we use globalfd instead */
 	glfd = glfs_h_opendir(glfs_export->gl_fs->fs, objhandle->glhandle);
@@ -305,8 +305,8 @@ static fsal_status_t read_dirents(struct fsal_obj_handle *dir_hdl,
 				  &op_ctx->creds->caller_gid,
 				  op_ctx->creds->caller_glen,
 				  op_ctx->creds->caller_garray,
-				  op_ctx->client->addr.addr,
-				  op_ctx->client->addr.len);
+				  socket_addr(&op_ctx->client->cl_addrbuf),
+				  socket_addr_len(&op_ctx->client->cl_addrbuf));
 
 #ifndef USE_GLUSTER_XREADDIRPLUS
 		rc = glfs_readdir_r(glfd, &de, &pde);
@@ -427,8 +427,8 @@ static fsal_status_t read_dirents(struct fsal_obj_handle *dir_hdl,
 			  &op_ctx->creds->caller_gid,
 			  op_ctx->creds->caller_glen,
 			  op_ctx->creds->caller_garray,
-			  op_ctx->client->addr.addr,
-			  op_ctx->client->addr.len);
+			  socket_addr(&op_ctx->client->cl_addrbuf),
+			  socket_addr_len(&op_ctx->client->cl_addrbuf));
 
 	rc = glfs_closedir(glfd);
 
@@ -472,8 +472,8 @@ static fsal_status_t makedir(struct fsal_obj_handle *dir_hdl,
 			  &op_ctx->creds->caller_gid,
 			  op_ctx->creds->caller_glen,
 			  op_ctx->creds->caller_garray,
-			  op_ctx->client->addr.addr,
-			  op_ctx->client->addr.len);
+			  socket_addr(&op_ctx->client->cl_addrbuf),
+			  socket_addr_len(&op_ctx->client->cl_addrbuf));
 
 	glhandle =
 	    glfs_h_mkdir(glfs_export->gl_fs->fs, parenthandle->glhandle, name,
@@ -600,8 +600,8 @@ static fsal_status_t makenode(struct fsal_obj_handle *dir_hdl,
 			  &op_ctx->creds->caller_gid,
 			  op_ctx->creds->caller_glen,
 			  op_ctx->creds->caller_garray,
-			  op_ctx->client->addr.addr,
-			  op_ctx->client->addr.len);
+			  socket_addr(&op_ctx->client->cl_addrbuf),
+			  socket_addr_len(&op_ctx->client->cl_addrbuf));
 
 	glhandle =
 	    glfs_h_mknod(glfs_export->gl_fs->fs, parenthandle->glhandle, name,
@@ -703,8 +703,8 @@ static fsal_status_t makesymlink(struct fsal_obj_handle *dir_hdl,
 			  &op_ctx->creds->caller_gid,
 			  op_ctx->creds->caller_glen,
 			  op_ctx->creds->caller_garray,
-			  op_ctx->client->addr.addr,
-			  op_ctx->client->addr.len);
+			  socket_addr(&op_ctx->client->cl_addrbuf),
+			  socket_addr_len(&op_ctx->client->cl_addrbuf));
 
 	glhandle =
 	    glfs_h_symlink(glfs_export->gl_fs->fs, parenthandle->glhandle, name,
@@ -799,8 +799,8 @@ static fsal_status_t readsymlink(struct fsal_obj_handle *obj_hdl,
 			  &op_ctx->creds->caller_gid,
 			  op_ctx->creds->caller_glen,
 			  op_ctx->creds->caller_garray,
-			  op_ctx->client->addr.addr,
-			  op_ctx->client->addr.len);
+			  socket_addr(&op_ctx->client->cl_addrbuf),
+			  socket_addr_len(&op_ctx->client->cl_addrbuf));
 
 	rc = glfs_h_readlink(glfs_export->gl_fs->fs, objhandle->glhandle,
 			     link_content->addr, link_content->len);
@@ -982,8 +982,8 @@ static fsal_status_t linkfile(struct fsal_obj_handle *obj_hdl,
 			  &op_ctx->creds->caller_gid,
 			  op_ctx->creds->caller_glen,
 			  op_ctx->creds->caller_garray,
-			  op_ctx->client->addr.addr,
-			  op_ctx->client->addr.len);
+			  socket_addr(&op_ctx->client->cl_addrbuf),
+			  socket_addr_len(&op_ctx->client->cl_addrbuf));
 
 	rc = glfs_h_link(glfs_export->gl_fs->fs, objhandle->glhandle,
 			 dstparenthandle->glhandle, name);
@@ -1033,8 +1033,8 @@ static fsal_status_t renamefile(struct fsal_obj_handle *obj_hdl,
 			  &op_ctx->creds->caller_gid,
 			  op_ctx->creds->caller_glen,
 			  op_ctx->creds->caller_garray,
-			  op_ctx->client->addr.addr,
-			  op_ctx->client->addr.len);
+			  socket_addr(&op_ctx->client->cl_addrbuf),
+			  socket_addr_len(&op_ctx->client->cl_addrbuf));
 
 	rc = glfs_h_rename(glfs_export->gl_fs->fs, srcparenthandle->glhandle,
 			   old_name, dstparenthandle->glhandle, new_name);
@@ -1079,8 +1079,8 @@ static fsal_status_t file_unlink(struct fsal_obj_handle *dir_hdl,
 			  &op_ctx->creds->caller_gid,
 			  op_ctx->creds->caller_glen,
 			  op_ctx->creds->caller_garray,
-			  op_ctx->client->addr.addr,
-			  op_ctx->client->addr.len);
+			  socket_addr(&op_ctx->client->cl_addrbuf),
+			  socket_addr_len(&op_ctx->client->cl_addrbuf));
 
 	rc = glfs_h_unlink(glfs_export->gl_fs->fs, parenthandle->glhandle,
 			   name);
@@ -1140,8 +1140,8 @@ fsal_status_t glusterfs_open_my_fd(struct glusterfs_handle *objhandle,
 			  &op_ctx->creds->caller_gid,
 			  op_ctx->creds->caller_glen,
 			  op_ctx->creds->caller_garray,
-			  op_ctx->client->addr.addr,
-			  op_ctx->client->addr.len);
+			  socket_addr(&op_ctx->client->cl_addrbuf),
+			  socket_addr_len(&op_ctx->client->cl_addrbuf));
 
 	glfd = glfs_h_open(glfs_export->gl_fs->fs, objhandle->glhandle,
 			   posix_flags);
@@ -1175,10 +1175,11 @@ fsal_status_t glusterfs_open_my_fd(struct glusterfs_handle *objhandle,
 	}
 
 #ifdef USE_GLUSTER_DELEGATION
-	if ((op_ctx->client->addr.len) &&
-		(op_ctx->client->addr.len <= GLAPI_LEASE_ID_SIZE)) {
-		memcpy(my_fd->lease_id, op_ctx->client->addr.addr,
-		       op_ctx->client->addr.len);
+	if (socket_addr_len(&op_ctx->client->cl_addrbuf)
+						<= GLAPI_LEASE_ID_SIZE) {
+		memcpy(my_fd->lease_id,
+		       socket_addr(&op_ctx->client->cl_addrbuf),
+		       socket_addr_len(&op_ctx->client->cl_addrbuf));
 	} else
 		memset(my_fd->lease_id, 0, GLAPI_LEASE_ID_SIZE);
 #endif
@@ -1744,8 +1745,8 @@ static fsal_status_t glusterfs_open2(struct fsal_obj_handle *obj_hdl,
 			  &op_ctx->creds->caller_gid,
 			  op_ctx->creds->caller_glen,
 			  op_ctx->creds->caller_garray,
-			  op_ctx->client->addr.addr,
-			  op_ctx->client->addr.len);
+			  socket_addr(&op_ctx->client->cl_addrbuf),
+			  socket_addr_len(&op_ctx->client->cl_addrbuf));
 
 	/** @todo: glfs_h_creat doesn't honour NO_CREATE mode. Instead use
 	 *  glfs_h_open to verify if the file already exists.
@@ -1936,8 +1937,8 @@ direrr:
 				  &op_ctx->creds->caller_gid,
 				  op_ctx->creds->caller_glen,
 				  op_ctx->creds->caller_garray,
-				  op_ctx->client->addr.addr,
-				  op_ctx->client->addr.len);
+				  socket_addr(&op_ctx->client->cl_addrbuf),
+				  socket_addr_len(&op_ctx->client->cl_addrbuf));
 
 		glfs_h_unlink(glfs_export->gl_fs->fs, parenthandle->glhandle,
 			      name);
@@ -2110,8 +2111,8 @@ static void glusterfs_read2(struct fsal_obj_handle *obj_hdl,
 			  &op_ctx->creds->caller_gid,
 			  op_ctx->creds->caller_glen,
 			  op_ctx->creds->caller_garray,
-			  op_ctx->client->addr.addr,
-			  op_ctx->client->addr.len);
+			  socket_addr(&op_ctx->client->cl_addrbuf),
+			  socket_addr_len(&op_ctx->client->cl_addrbuf));
 
 	/* XXX dang switch to preadv_async once async supported */
 	nb_read = glfs_preadv(my_fd.glfd, read_arg->iov, read_arg->iov_count,
@@ -2216,8 +2217,8 @@ static void glusterfs_write2(struct fsal_obj_handle *obj_hdl,
 			  &op_ctx->creds->caller_gid,
 			  op_ctx->creds->caller_glen,
 			  op_ctx->creds->caller_garray,
-			  op_ctx->client->addr.addr,
-			  op_ctx->client->addr.len);
+			  socket_addr(&op_ctx->client->cl_addrbuf),
+			  socket_addr_len(&op_ctx->client->cl_addrbuf));
 
 	/* XXX dang switch to pwritev_async once async supported */
 	nb_written = glfs_pwritev(my_fd.glfd, write_arg->iov,
@@ -2301,8 +2302,8 @@ static fsal_status_t seek2(struct fsal_obj_handle *obj_hdl,
 			&op_ctx->creds->caller_gid,
 			op_ctx->creds->caller_glen,
 			op_ctx->creds->caller_garray,
-			op_ctx->client->addr.addr,
-			op_ctx->client->addr.len);
+			socket_addr(&op_ctx->client->cl_addrbuf),
+			socket_addr_len(&op_ctx->client->cl_addrbuf));
 
 	if (info->io_content.what == NFS4_CONTENT_DATA) {
 		what = SEEK_DATA;
@@ -2383,8 +2384,8 @@ static fsal_status_t glusterfs_commit2(struct fsal_obj_handle *obj_hdl,
 				  &op_ctx->creds->caller_gid,
 				  op_ctx->creds->caller_glen,
 				  op_ctx->creds->caller_garray,
-				  op_ctx->client->addr.addr,
-				  op_ctx->client->addr.len);
+				  socket_addr(&op_ctx->client->cl_addrbuf),
+				  socket_addr_len(&op_ctx->client->cl_addrbuf));
 #ifdef USE_GLUSTER_STAT_FETCH_API
 		retval = glfs_fsync(out_fd->glfd, NULL, NULL);
 #else
@@ -2826,8 +2827,8 @@ static fsal_status_t glusterfs_setattr2(struct fsal_obj_handle *obj_hdl,
 				  &op_ctx->creds->caller_gid,
 				  op_ctx->creds->caller_glen,
 				  op_ctx->creds->caller_garray,
-				  op_ctx->client->addr.addr,
-				  op_ctx->client->addr.len);
+				  socket_addr(&op_ctx->client->cl_addrbuf),
+				  socket_addr_len(&op_ctx->client->cl_addrbuf));
 #ifdef USE_GLUSTER_STAT_FETCH_API
 		retval = glfs_ftruncate(my_fd.glfd, attrib_set->filesize,
 					NULL, NULL);
@@ -2923,8 +2924,8 @@ static fsal_status_t glusterfs_setattr2(struct fsal_obj_handle *obj_hdl,
 			  &op_ctx->creds->caller_gid,
 			  op_ctx->creds->caller_glen,
 			  op_ctx->creds->caller_garray,
-			  op_ctx->client->addr.addr,
-			  op_ctx->client->addr.len);
+			  socket_addr(&op_ctx->client->cl_addrbuf),
+			  socket_addr_len(&op_ctx->client->cl_addrbuf));
 
 	/* If any stat changed, indicate that */
 	if (mask != 0)

src/FSAL/FSAL_GLUSTER/mds.c
@@ -357,8 +357,8 @@ static nfsstat4 pnfs_layout_commit(struct fsal_obj_handle *obj_pub,
 			  &op_ctx->creds->caller_gid,
 			  op_ctx->creds->caller_glen,
 			  op_ctx->creds->caller_garray,
-			  op_ctx->client->addr.addr,
-			  op_ctx->client->addr.len);
+			  socket_addr(&op_ctx->client->cl_addrbuf),
+			  socket_addr_len(&op_ctx->client->cl_addrbuf));
 
 	rc = glfs_h_setattrs(glfs_export->gl_fs->fs,
 			     objhandle->glhandle,

src/FSAL/FSAL_GPFS/fsal_internal.c
@@ -102,7 +102,7 @@ fsal_internal_handle2fd(int dirfd, struct gpfs_file_handle *gpfs_fh,
 	struct open_arg oarg = {0};
 	int rc;
 
-	if (op_ctx && op_ctx->client && op_ctx->client->hostaddr_str)
+	if (op_ctx && op_ctx->client)
 		oarg.cli_ip = op_ctx->client->hostaddr_str;
 
 	oarg.mountdirfd = dirfd;

src/include/client_mgr.h
@@ -48,11 +48,10 @@
 struct gsh_client {
 	struct avltree_node node_k;
 	pthread_rwlock_t lock;
-	struct gsh_buffdesc addr;
 	int64_t refcnt;
 	nsecs_elapsed_t last_update;
-	char *hostaddr_str;
-	unsigned char addrbuf[];
+	char hostaddr_str[SOCK_NAME_MAX];
+	sockaddr_t cl_addrbuf;
 };
 
 static inline int64_t inc_gsh_client_refcount(struct gsh_client *client)

commit a8c3ab84b4a7d9a60836f68594c2af4ace739235

client_mgr: fix free of static data
hostaddr_str is now part of the gsh_client struct

Reported-By: clang
Change-Id: I32e73855ef2284e9580aa2f3d2f439dcca8ad10d
Fixes: 9e806a7dc903 ("Rework client_mgr to use rpc_tools.c/gsh_rpc.h and related cleanup")
Signed-off-by: Dominique Martinet <dominique.martinet@cea.fr>

src/support/client_mgr.c
@@ -257,8 +257,6 @@ int remove_gsh_client(sockaddr_t *client_ipaddr)
 		server_st = container_of(cl, struct server_stats, client);
 		server_stats_free(&server_st->st);
 		server_stats_allops_free(&server_st->c_all);
-		if (cl->hostaddr_str != NULL)
-			gsh_free(cl->hostaddr_str);
 		gsh_free(server_st);
 	}
 	return removed;