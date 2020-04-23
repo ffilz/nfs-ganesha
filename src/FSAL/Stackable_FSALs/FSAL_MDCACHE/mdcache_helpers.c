955f7d9ad0ae692d2f33afc75a1b490ed3e20d4a
sepia-liu <liuwei_coder@163.com>
2020-04-23 17:00:54 +0800

    MDCACHE - Fix readdir reply to duplicate cookie to client
    When the last dirent lookup entry failed by ckey,
    the chunk containing the dirent is freed, then
    next_ck=chunk->reload_ck and the chunk will repopulate,
    by skipping already used dirent and look_ck dirent,
    here next_ck not change and look_ck=0 cause reply to duplicate cookie
    
    Change-Id: Id71f53afbf2506728551dfab6fbffa581c1284a0
    Signed-off-by: sepia-liu <liuwei_coder@163.com>

--- src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_helpers.c
+++ src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_helpers.c
@@ -3266,6 +3266,11 @@ again:
 			continue;
 		}
 
+		next_ck = dirent->ck;
+		LogFullDebugAlt(COMPONENT_NFS_READDIR, COMPONENT_CACHE_INODE,
+				"Setting next_ck=%"PRIx64,
+				next_ck);
+
 		if (dirent->ck == whence) {
 			/* When called with whence, the caller always wants the
 			 * next entry, skip this entry. */
@@ -3273,11 +3278,6 @@ again:
 			reload_chunk = false;
 			continue;
 		}
-
-		next_ck = dirent->ck;
-		LogFullDebugAlt(COMPONENT_NFS_READDIR, COMPONENT_CACHE_INODE,
-				"Setting next_ck=%"PRIx64,
-				next_ck);
 
 		/* Ensure the attribute cache is valid.  The simplest way to do
 		 * this is to call getattrs().  We need a copy anyway, to ensure
