b30460add91d6cdcb6d1a3d43a49c529eb312c9a
Luo Muyao <luo.muyao@zte.com.cn>
2020-05-14 19:53:27 +0800

    Add support for NFSv3 ACLs.
    This adds functions for encoding and decoding POSIX ACLs for the NFSACL protocol extension, and the GETACL and SETACL RPCs.
    The actual access decisions are performed using the ACCESS RPC which is part of NFSv3 proper, and is independent of acls.
    The GETACL and SETACL RPCs are mainly used by tools like getfacl and setfacl.In addition, for files created inside directories that have a default acl,SETACL is used at file create time to implement the POSIX ACL file create semantics.
    
    Change-Id: I5b73caf733c54003370abe81200f9ac5cc8b6abc
    Signed-off-by: Luo Muyao <luo.muyao@zte.com.cn>
    Signed-off-by: Zhu Shangzhong <zhu.shangzhong@zte.com.cn>

--- src/MainNFSD/nfs_worker_thread.c
+++ src/MainNFSD/nfs_worker_thread.c
@@ -660,6 +660,34 @@ const nfs_function_desc_t rquota2_func_desc[] = {
 				       .dispatch_behaviour = NEEDS_CRED}
 };
 
+const nfs_function_desc_t nfsacl_func_desc[] = {
+	[0] = {
+	       .service_function = nfsacl_Null,
+	       .free_function = nfsacl_Null_Free,
+	       .xdr_decode_func = (xdrproc_t) xdr_void,
+	       .xdr_encode_func = (xdrproc_t) xdr_void,
+	       .funcname = "NFSACL_NULL",
+	       .dispatch_behaviour = NOTHING_SPECIAL},
+	[NFSACLPROC_GETACL] = {
+				 .service_function = nfsacl_getacl,
+				 .free_function = nfsacl_getacl_Free,
+				 .xdr_decode_func =
+				 (xdrproc_t) xdr_getaclargs,
+				 .xdr_encode_func =
+				 (xdrproc_t) xdr_getaclres,
+				 .funcname = "NFSACL_GETACL",
+				 .dispatch_behaviour = NEEDS_CRED},
+	[NFSACLPROC_SETACL] = {
+				 .service_function = nfsacl_setacl,
+				 .free_function = nfsacl_setacl_Free,
+				 .xdr_decode_func =
+				 (xdrproc_t) xdr_setaclargs,
+				 .xdr_encode_func =
+				 (xdrproc_t) xdr_setaclres,
+				 .funcname = "NFSACL_SETACL",
+				 .dispatch_behaviour = NEEDS_CRED}
+};
+
 void auth_failure(nfs_request_t *reqdata, enum auth_stat auth_rc)
 {
 	svcerr_auth(&reqdata->svc, auth_rc);
@@ -1083,7 +1111,8 @@ static enum xprt_stat nfs_rpc_process_request(nfs_request_t *reqdata)
 	    || reqdata->svc.rq_msg.cb_proc == NFSPROC_NULL)
 		goto null_op;
 	/* Get the export entry */
-	if (reqdata->svc.rq_msg.cb_prog == NFS_program[P_NFS]) {
+	if (reqdata->svc.rq_msg.cb_prog == NFS_program[P_NFS] ||
+		reqdata->svc.rq_msg.cb_prog == NFS_program[P_NFSACL]) {
 		/* The NFSv3 functions' arguments always begin with the file
 		 * handle (but not the NULL function).  This hook is used to
 		 * get the fhandle with the arguments and so determine the
@@ -1230,7 +1259,6 @@ static enum xprt_stat nfs_rpc_process_request(nfs_request_t *reqdata)
 	} else if (reqdata->svc.rq_msg.cb_prog == NFS_program[P_MNT]) {
 		progname = "MNT";
 	}
-
 	/* Only do access check if we have an export. */
 	if (op_ctx->ctx_export != NULL) {
 		/* We ONLY get here for NFS v3 or NLM requests with a handle */
@@ -1582,6 +1610,16 @@ enum xprt_stat nfs_rpc_valid_NFS(struct svc_req *req)
 
 	reqdata->funcdesc = &invalid_funcdesc;
 
+	if (req->rq_msg.cb_prog == NFS_program[P_NFSACL]) {
+		if (req->rq_msg.cb_vers == NFSACL_V3 && CORE_OPTION_NFSV3) {
+			if (req->rq_msg.cb_proc <= NFSACLPROC_SETACL) {
+				reqdata->funcdesc =
+					&nfsacl_func_desc[req->rq_msg.cb_proc];
+				return nfs_rpc_process_request(reqdata);
+			}
+		}
+	}
+
 	if (req->rq_msg.cb_prog != NFS_program[P_NFS]) {
 		return nfs_rpc_noprog(reqdata);
 	}
@@ -1619,6 +1657,15 @@ enum xprt_stat nfs_rpc_valid_NFS(struct svc_req *req)
 #else
 	lo_vers = NFS_V4;
 #endif
+//	if(req->rq_msg.cb_prog == NFS_program[P_NFSACL]) {
+//		if (req->rq_msg.cb_vers == NFSACL_V3) {
+//			if (req->rq_msg.cb_proc <= NFSACLPROC_SETACL) {
+//				reqdata->funcdesc =
+//					&nfsacl_func_desc[req->rq_msg.cb_proc];
+//				return nfs_rpc_process_request(reqdata);
+//			}
+//		}
+//	}
 
 	return nfs_rpc_novers(reqdata, lo_vers, hi_vers);
 }
@@ -1712,5 +1759,30 @@ enum xprt_stat nfs_rpc_valid_RQUOTA(struct svc_req *req)
 		}
 		return nfs_rpc_novers(reqdata, RQUOTAVERS, EXT_RQUOTAVERS);
 	}
+	return nfs_rpc_noprog(reqdata);
+}
+
+enum xprt_stat nfs_rpc_valid_NFSACL(struct svc_req *req)
+{
+	nfs_request_t *reqdata =
+			container_of(req, struct nfs_request, svc);
+
+	reqdata->funcdesc = &invalid_funcdesc;
+
+	if (req->rq_msg.cb_prog == NFS_program[P_NFSACL]) {
+		LogFullDebug(COMPONENT_DISPATCH,
+			     "ERROR: i am in nfs_rpc_valid_NFSAC1");
+		if (req->rq_msg.cb_vers == NFSACL_V3) {
+			if (req->rq_msg.cb_proc <= NFSACLPROC_SETACL) {
+				reqdata->funcdesc =
+					&nfsacl_func_desc[req->rq_msg.cb_proc];
+				return nfs_rpc_process_request(reqdata);
+			}
+			return nfs_rpc_noproc(reqdata);
+		}
+		return nfs_rpc_novers(reqdata, NFSACL_V3, NFSACL_V3);
+	}
+	LogFullDebug(COMPONENT_DISPATCH,
+			     "ERROR: i am in nfs_rpc_valid_NFSAC2");
 	return nfs_rpc_noprog(reqdata);
 }
