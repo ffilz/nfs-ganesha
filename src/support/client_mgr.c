98fb1136aa2358cf0e23af4ed28ef15f647da787
Xi Jinyu <xijinyu@cmss.chinamobile.com>
2020-06-04 06:20:16 -0400

    Add ganesha_stats iomon.
    Monitoring the real-time bandwidth of each export instance.
    
    Signed-off-by: Xi Jinyu <xijinyu@cmss.chinamobile.com>
    Change-Id: I9e6da1f5e872979a6198d0e68006815732c21608

--- src/support/client_mgr.c
+++ src/support/client_mgr.c
@@ -934,6 +934,55 @@ static struct gsh_dbus_method cltmgr_show_v42_layouts = {
 };
 
 /**
+ * DBUS method to report NFS I/O real-time statistics
+ *
+ */
+
+static bool get_nfsmon_stats_io(DBusMessageIter *args,
+				DBusMessage *reply,
+				DBusError *error)
+{
+	struct gsh_client *client = NULL;
+	struct server_stats *server_st = NULL;
+	bool success = true;
+	char *errormsg = "OK";
+	DBusMessageIter iter;
+
+	dbus_message_iter_init_append(reply, &iter);
+	if (!nfs_param.core_param.enable_NFSSTATS)
+		errormsg = "NFS stat counting disabled";
+	client = lookup_client(args, &errormsg);
+	if (client == NULL) {
+		success = false;
+		if (errormsg == NULL)
+			errormsg = "Client IP address not found";
+	} else {
+		server_st = container_of(client, struct server_stats, client);
+		if (server_st == NULL) {
+			success = false;
+			errormsg = "Client does not have any NFS activity";
+		}
+	}
+	gsh_dbus_status_reply(&iter, success, errormsg);
+	if (success)
+		server_dbus_nfsmon_server_iostats(server_st, &iter);
+
+	if (client != NULL)
+		put_gsh_client(client);
+	return true;
+}
+
+static struct gsh_dbus_method cltmgr_show_nfsmon_io = {
+	.name = "GetNFSIOMon",
+	.method = get_nfsmon_stats_io,
+	.args = {IPADDR_ARG,
+		 STATUS_REPLY,
+		 TIMESTAMP_REPLY,
+		 IOSTATS_REPLY,
+		 END_ARG_LIST}
+};
+
+/**
  * DBUS method to report NFSv4 delegation statistics
  */
 static bool get_stats_delegations(DBusMessageIter *args,
@@ -1134,6 +1183,7 @@ static struct gsh_dbus_method *cltmgr_stats_methods[] = {
 	&cltmgr_show_v41_layouts,
 	&cltmgr_show_v42_io,
 	&cltmgr_show_v42_layouts,
+	&cltmgr_show_nfsmon_io,
 	&cltmgr_show_delegations,
 	&cltmgr_client_io_ops,
 	&cltmgr_client_all_ops,
