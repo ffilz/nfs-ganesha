350119e3f51c72f5f96c6fc4f35fabab8de7035a
Dominique Martinet <dominique.martinet@cea.fr>
2020-07-17 16:31:47 +0200

    move run dir to /run
    /run has been preferred over /var/run for a while.
    The main motivation for this change is that /run is not dependent on SYSSTATEDIR,
    and under some distributions (e.g. nixos) led to improbable run dir.
    
    Also, for systemd distributions, /run/ganesha has been created by systemd for a while
    (via the RunDirectory= setting), so embrace the dynamic directory creation for all
    service files
    
    Change-Id: I83b3871d5dd1984534487e6e237dbe8ae0eefe00
    Signed-off-by: Dominique Martinet <dominique.martinet@cea.fr>

--- src/include/config-h.in.cmake
+++ src/include/config-h.in.cmake
@@ -74,12 +74,12 @@
 #define NFS_GANESHA 1
 
 #define GANESHA_CONFIG_PATH "@SYSCONFDIR@/ganesha/ganesha.conf"
-#define GANESHA_PIDFILE_PATH "@SYSSTATEDIR@/run/ganesha.pid"
+#define GANESHA_PIDFILE_PATH "/run/ganesha.pid"
 #define NFS_V4_RECOV_ROOT "@SYSSTATEDIR@/lib/nfs/ganesha"
 /**
  * @brief Default value for krb5_param.ccache_dir
  */
-#define DEFAULT_NFS_CCACHE_DIR "@SYSSTATEDIR@/run/ganesha"
+#define DEFAULT_NFS_CCACHE_DIR "/run/ganesha"
 
 /* We're LGPL'd */
 #define _LGPL_SOURCE 1
