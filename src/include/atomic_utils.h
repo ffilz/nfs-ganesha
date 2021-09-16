4f7ffe16758613ac776b09efc5b77f450ea528fb
Frank S. Filz <ffilzlnx@mindspring.com>
2021-09-16 16:49:15 -0700

    Add atomic_add_unless and PTHREAD_MUTEX_decrement_XXX_t_and_lock
    Change-Id: I70dd0ab1296b27d87e5abe45e08a3b591a2fcee5
    Signed-off-by: Frank S. Filz <ffilzlnx@mindspring.com>

---
+++ src/include/atomic_utils.h
@@ -0,0 +1,104 @@
+/*
+ * vim:noexpandtab:shiftwidth=8:tabstop=8:
+ */
+
+/**
+ * @file common_utils.h
+ * @brief Common tools for printing, parsing, ....
+ */
+
+#ifndef ATOMIC_UTILS_H
+#define ATOMIC_UTILS_H
+
+#include "common_utils.h"
+#include "abstract_atomic.h"
+
+/**
+ * @brief Decrement an int64_t refcounter and take mutex if zero.
+ *
+ * @param[in,out] _mtx The mutex to acquire
+ */
+
+#if defined(GCC_ATOMIC_FUNCTIONS) | defined(GCC_SYNC_FUNCTIONS)
+static inline
+bool PTHREAD_MUTEX_decrement_int64_t_and_lock(int64_t *var,
+					      pthread_mutex_t *lock)
+{
+	if (atomic_add_unless_int64_t(var, -1, 1))
+		return false;
+
+	PTHREAD_MUTEX_lock(lock);
+	if (atomic_add_int64_t(var, -1) == 0)
+		return true;
+	PTHREAD_MUTEX_unlock(lock);
+	return false;
+}
+#endif
+
+/**
+ * @brief Decrement an uint64_t refcounter and take mutex if zero.
+ *
+ * @param[in,out] _mtx The mutex to acquire
+ */
+
+#if defined(GCC_ATOMIC_FUNCTIONS) | defined(GCC_SYNC_FUNCTIONS)
+static inline
+bool PTHREAD_MUTEX_decrement_uint64_t_and_lock(uint64_t *var,
+					       pthread_mutex_t *lock)
+{
+	if (atomic_add_unless_uint64_t(var, -1, 1))
+		return false;
+
+	PTHREAD_MUTEX_lock(lock);
+	if (atomic_add_uint64_t(var, -1) == 0)
+		return true;
+	PTHREAD_MUTEX_unlock(lock);
+	return false;
+}
+#endif
+
+/**
+ * @brief Decrement an int32_t refcounter and take mutex if zero.
+ *
+ * @param[in,out] _mtx The mutex to acquire
+ */
+
+#if defined(GCC_ATOMIC_FUNCTIONS) | defined(GCC_SYNC_FUNCTIONS)
+static inline
+bool PTHREAD_MUTEX_decrement_int32_t_and_lock(int32_t *var,
+					      pthread_mutex_t *lock)
+{
+	if (atomic_add_unless_int32_t(var, -1, 1))
+		return false;
+
+	PTHREAD_MUTEX_lock(lock);
+	if (atomic_add_int32_t(var, -1) == 0)
+		return true;
+	PTHREAD_MUTEX_unlock(lock);
+	return false;
+}
+#endif
+
+/**
+ * @brief Decrement an uint32_t refcounter and take mutex if zero.
+ *
+ * @param[in,out] _mtx The mutex to acquire
+ */
+
+#if defined(GCC_ATOMIC_FUNCTIONS) | defined(GCC_SYNC_FUNCTIONS)
+static inline
+bool PTHREAD_MUTEX_decrement_uint32_t_and_lock(uint32_t *var,
+					       pthread_mutex_t *lock)
+{
+	if (atomic_add_unless_uint32_t(var, -1, 1))
+		return false;
+
+	PTHREAD_MUTEX_lock(lock);
+	if (atomic_add_uint32_t(var, -1) == 0)
+		return true;
+	PTHREAD_MUTEX_unlock(lock);
+	return false;
+}
+#endif
+
+#endif				/* !ATOMIC_UTILS_H */
