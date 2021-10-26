e37ffe66a0774040310a62fa0511a0e0b5ae95fc
Frank S. Filz <ffilzlnx@mindspring.com>
2021-10-26 13:30:31 -0700

    NLM: Use IPv4 address instead of encapsulated for caller_name
    If nsm_use_caller_name is false, and the client is an IPv4
    address encapsulated in IPv6, we want to use the IPv4 address
    not the IPv6 encapsulated address for caller_name.
    
    Change-Id: I0e0ac0a0836bbdcd8aa22fda7253f4bad6abab4e
    Signed-off-by: Frank S. Filz <ffilzlnx@mindspring.com>

--- src/SAL/nlm_owner.c
+++ src/SAL/nlm_owner.c
@@ -852,13 +852,19 @@ state_nsm_client_t *get_nsm_client(care_t care,  char *caller_name)
 	hash_error_t rc;
 	struct gsh_buffdesc buffkey;
 	struct gsh_buffdesc buffval;
+	char hostaddr_str[SOCK_NAME_MAX];
 
 	if (caller_name == NULL)
 		return NULL;
 
 	memset(&key, 0, sizeof(key));
 
-	if (nfs_param.core_param.nsm_use_caller_name) {
+	if (nfs_param.core_param.nsm_use_caller_name ||
+	    op_ctx->client == NULL) {
+		/* If nsm_use_caller_name is false but op_ctx->client is NULL
+		 * we are being called for SM_NOTIFY. caller name is supposed to
+		 * be an IP address.
+		 */
 		key.ssc_nlm_caller_name_len = strlen(caller_name);
 
 		if (key.ssc_nlm_caller_name_len > LM_MAXSTRLEN)
@@ -869,7 +875,34 @@ state_nsm_client_t *get_nsm_client(care_t care,  char *caller_name)
 			     "Using caller_name %s",
 			     caller_name);
 	} else {
-		key.ssc_nlm_caller_name = op_ctx->client->hostaddr_str;
+		sockaddr_t alt_host;
+		sockaddr_t *host = NULL;
+
+		if (isFullDebug(COMPONENT_STATE)) {
+			char str[LOG_BUFF_LEN] = "\0";
+			struct display_buffer db = {sizeof(str), str, str};
+
+			display_sockaddr(&db, op_ctx->caller_addr);
+			LogFullDebug(COMPONENT_NLM,
+				     "Using address %s as caller name",
+				     str);
+		}
+
+		/* Fixup any encapsulated IPv4 addresses */
+		host = convert_ipv6_to_ipv4(op_ctx->caller_addr, &alt_host);
+
+		/* Generate caller name from fixed up address */
+		if (!sprint_sockip(host, hostaddr_str, sizeof(hostaddr_str))) {
+			LogCrit(COMPONENT_STATE,
+				"Could not generate caller name");
+			return NULL;
+		}
+
+		LogFullDebug(COMPONENT_STATE,
+			     "Using caller address %s",
+			     hostaddr_str);
+
+		key.ssc_nlm_caller_name = hostaddr_str;
 		key.ssc_nlm_caller_name_len = strlen(key.ssc_nlm_caller_name);
 		key.ssc_client = op_ctx->client;
 	}
