5b9fc57f4c022bd02e567699b71e92db9f3ea53f
liyinshu <liyinshu@bytedance.com>
2022-08-16 15:42:02 +0800

    Check whether request is a real replay to warn kernel's bug
    If client use kernel 4.14.81 in https://kernel.org/, sometimes
    different requests share the same slot and sequenceid, ganesha
    thinks the latter request is a replay of former request, so
    return the response of the former request as the reply of the
    latter request. It can cause client hangs forever. For example,
    OP_SEQUENCE comes first, and then comes OP_GETATTR which share
    the same slot and sequenceid with the former OP_SEQUENCE, and
    ganesha reply NFS4ERR_RETRY_UNCACHED_REP. Then nfs-client will
    retry send the OP_GETATTR with the same slot and sequenceid,
    and ganesha still reply NFS4ERR_RETRY_UNCACHED_REP, ..., this
    will last forever. This bug, i.e. different requests share the
    same slot and sequenceid, disappers in kernel 5.4.143, fixed by
    some previous version.
    
    Change-Id: I9108d3bbe5310c3eaf0c1d600668a18b9dabbf76
    Signed-off-by: liyinshu <liyinshu@bytedance.com>

--- src/SAL/nfs41_session_id.c
+++ src/SAL/nfs41_session_id.c
@@ -36,6 +36,7 @@
  */
 
 #include "config.h"
+#include "nfs_convert.h"
 #include "nfs_core.h"
 #include "nfs_proto_functions.h"
 #include "sal_functions.h"
@@ -109,6 +110,39 @@ int display_session(struct display_buffer *dspbuf, nfs41_session_t *session)
 
 	if (b_left > 0)
 		b_left = display_session_id(dspbuf, session->session_id);
+
+	if (b_left > 0)
+		b_left = display_cat(dspbuf, "}");
+
+	return b_left;
+}
+
+/**
+ * @brief Display a compound request's operations
+ *
+ * @param[in]  dspbuf      The key to display
+ * @param[in]  opcodes     The array of opcode belongs to one compound
+ * @param[in]  opcodes_len The number of opcode belongs to one compound
+ *
+ * @return Length of output string.
+ */
+
+int display_nfs4_operations(struct display_buffer *dspbuf, nfs_opnum4 *opcodes,
+	uint32_t opcode_num)
+{
+	uint32_t i = 0;
+	const char *opname = NULL;
+
+	int b_left = display_cat(dspbuf, "nfs4 operations {");
+
+	while (b_left > 0 && i < opcode_num) {
+		if (i > 0)
+			b_left = display_cat(dspbuf, ",");
+
+		opname = nfsop4_to_str(opcodes[i]);
+		b_left = display_cat(dspbuf, opname);
+		i++;
+	}
 
 	if (b_left > 0)
 		b_left = display_cat(dspbuf, "}");
