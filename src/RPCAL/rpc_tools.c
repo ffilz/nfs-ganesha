0b453743a57828624453ae133e1f1694e7e828d8
Gaurav Gangalwar <gaurav.gangalwar@nutanix.com>
2022-09-13 11:59:58 +0000

    NSM: Handle little endian check in is_loopback.
    We were hitting mismatch if we copy sockaddr from
    network to host order on x86.
    
    Signed-off-by: Gaurav Gangalwar <gaurav.gangalwar@nutanix.com>
    Change-Id: Ic38f5b4c00be573c85187e7544f0b8e3e9f85c5c

--- src/RPCAL/rpc_tools.c
+++ src/RPCAL/rpc_tools.c
@@ -449,7 +449,8 @@ bool is_loopback(sockaddr_t *addr)
 	if (addr->ss_family == AF_INET) {
 		struct sockaddr_in *inaddr = (struct sockaddr_in *)addr;
 
-		return (inaddr->sin_addr.s_addr & 0xFF000000) == 0x7F000000;
+		return (((inaddr->sin_addr.s_addr & 0xFF000000) == 0x7F000000) ||
+			((inaddr->sin_addr.s_addr & 0xFF) == 0x7F));
 	} else if (addr->ss_family != AF_INET6) {
 		return false;
 	}
