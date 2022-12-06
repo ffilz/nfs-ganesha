c386f43c65bdfd0a38ec8007e599a8986d19af12
Lior Suliman <liorsu@google.com>
2022-12-06 15:13:35 +0200

    Turn on more compilation warnings and resolve existing ones
    Signed-off-by: Lior Suliman <liorsu@gmail.com>
    Change-Id: I05413e76442077161e752da7ba57756a6da7a33e

--- src/FSAL/fsal_helper.c
+++ src/FSAL/fsal_helper.c
@@ -1977,7 +1977,8 @@ again:
 	PTHREAD_MUTEX_lock(data->fsa_mutex);
 
 	while (!data->done) {
-		int rc = pthread_cond_wait(data->fsa_cond, data->fsa_mutex);
+		int __attribute__((unused)) rc =
+			pthread_cond_wait(data->fsa_cond, data->fsa_mutex);
 
 		assert(rc == 0);
 	}
@@ -2000,7 +2001,8 @@ again:
 	PTHREAD_MUTEX_lock(data->fsa_mutex);
 
 	while (!data->done) {
-		int rc = pthread_cond_wait(data->fsa_cond, data->fsa_mutex);
+		int __attribute__((unused)) rc =
+			pthread_cond_wait(data->fsa_cond, data->fsa_mutex);
 
 		assert(rc == 0);
 	}
