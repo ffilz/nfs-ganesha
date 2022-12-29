yogendra-t/nfs-ganesha-1

Malloc trim support
Glibc is known to cause greedy allocations with ganesha daemon, this
feature dynamically calls malloc_trim() to release memory back to the
system based on certain RSS (Resident Set Size) values. This feature
can be enabled by having "enable_trim = 1" in NFS_Core_Param{} block.

Also, it can be enabled/disabled by running:
ganesha_mgr trim enable OR
ganesha_mgr trim disable.

Its status can be queried with: ganesha_mgr trim status

ganesha_mgr trim call makes a one time malloc_trim(0) as well.

Change-Id: Ic947d98cfbe69faf4785d862047738c240b005f9
Signed-off-by: Malahal Naineni <malahal@us.ibm.com>

src/include/gsh_config.h
@@ -362,6 +362,7 @@ typedef struct nfs_core_param {
 	char *ganesha_modules_loc;
 	/* Frequency of dbus health heartbeat in ms. Set to 0 to disable */
 	uint32_t heartbeat_freq;
+	bool enable_trim; /* Enable malloc trim */
 } nfs_core_parameter_t;
 
 /** @} */

src/support/nfs_read_conf.c
@@ -170,6 +170,8 @@ static struct config_item core_params[] = {
 		       nfs_core_param, ganesha_modules_loc),
 	CONF_ITEM_UI32("heartbeat_freq", 0, 5000, 1000,
 		       nfs_core_param, heartbeat_freq),
+	CONF_ITEM_BOOL("enable_trim", false,
+		       nfs_core_param, enable_trim),
 	CONFIG_EOL
 };
 

ffilz/nfs-ganesha

(cherry picked from commit 24c26c728137c73be2e848f24e6f8fb4be42e84d)

src/include/gsh_config.h
@@ -449,6 +449,7 @@ typedef struct nfs_core_param {
 		nfs3_complete_read.
 		Defaults to true and settable by Getattrs_In_Complete_Read. */
 	bool getattrs_in_complete_read;
+	bool malloc_trim; /* Enable malloc trim */
 } nfs_core_parameter_t;
 
 /** @} */

src/support/nfs_read_conf.c
@@ -270,6 +270,8 @@ static struct config_item core_params[] = {
 		       nfs_core_param, readdir_max_count),
 	CONF_ITEM_BOOL("Getattrs_In_Complete_Read", true,
 			   nfs_core_param, getattrs_in_complete_read),
+	CONF_ITEM_BOOL("Enable_malloc_trim", false,
+		       nfs_core_param, malloc_trim),
 	CONFIG_EOL
 };
 