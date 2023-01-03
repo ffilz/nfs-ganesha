ffilz/nfs-ganesha

commit 5f06bd1c40b89c726ccb0f0284caace66d12a1d0

Fix crash bcz slot->cached_result is null for replay request
Reproduce crash as follows:
thread1: receive a replay request, and found slot->cached_result not
         null, assign slot to data->slot at nfs4_op_sequence.c:131,
         and unlock the slot->lock.
thread2: receive a normal request, got slot->lock and
         release_slot(slot), so slot->cached_result was set null.
thread1: in complete_op, assign *status with
         data->slot->cached_result->status,
         but data->slot->cached_result is null, so crash happens.

To fix crash, move the code which need slot->cached_result from
complete_op to nfs4_op_sequence, so thread1 can visit cached_result
with the guard of slot->lock. Besides, we need to save cached_result's
status in compound_data, so complete_op can get it.

Change-Id: I92f472edc773bf357b35da1a1805e2d59c95a286
Signed-off-by: liyinshu <liyinshu@bytedance.com>

src/Protocols/NFS/nfs4_op_sequence.c
@@ -129,8 +129,23 @@ enum nfs_req_result nfs4_op_sequence(struct nfs_argop4 *op,
 				 * Take a reference to the slot cached response.
 				 */
 				data->slot = slot;
+
+				/* Free the reply allocated originally */
+				release_nfs4_res_compound(
+					data->res->res_compound4_extended);
+
 				refcnt = atomic_inc_int32_t(
 					&slot->cached_result->res_refcnt);
+
+				/* Copy the reply from the cache (the reference
+				 * is already taken by SEQUENCE.
+				 */
+				data->res->res_compound4_extended =
+					data->slot->cached_result;
+
+				data->cached_result_status =
+					((COMPOUND4res *) data->slot->
+						cached_result)->status;
 
 				LogFullDebugAlt(COMPONENT_SESSIONS,
 						COMPONENT_CLIENTID,

src/Protocols/NFS/nfs4_Compound.c
@@ -680,19 +680,10 @@ enum nfs_req_result complete_op(compound_data_t *data, nfsstat4 *status,
 		 * return from the compound.
 		 */
 
-		/* Free the reply allocated originally */
-		release_nfs4_res_compound(data->res->res_compound4_extended);
-
-		/* Copy the reply from the cache (the reference is already
-		 * taken by SEQUENCE.
-		 */
-		data->res->res_compound4_extended = data->slot->cached_result;
-
-		*status = ((COMPOUND4res *) data->slot->cached_result)->status;
+		*status = data->cached_result_status;
 
 		LogFullDebug(COMPONENT_SESSIONS,
-			     "Use session replay cache %p result %s",
-			     data->slot->cached_result,
+			     "Use session replay cache result %s",
 			     nfsstat4_to_str(*status));
 
 		/* Will exit the for loop since result is not NFS_REQ_OK */

src/include/nfs_proto_data.h
@@ -356,6 +356,8 @@ struct compound_data {
 						   reserved, if any */
 	struct nfs41_session_slot__ *slot;	/*< NFv41: pointer to the
 							session's slot */
+	nfsstat4 cached_result_status; /* <NFv41: save the
+	    slot->cached_result->status for reply request */
 	bool sa_cachethis;	/*< True if cachethis was specified in
 				    SEQUENCE op. */
 	nfs_opnum4 opcode;	/*< Current NFS4 OP */

commit 5f37c72f09a5cf0f80ddde18db947be2ef121757

Fix crash for used after free when replay request
This caused by commit 5f06bd1c40b89c726ccb0f0284caace66d12a1d0, which
first release_nfs4_res_compound(data->res->res_compound4_extended), then
assign res_SEQUENCE4->sr_status with NFS4_OK. Because res_SEQUENCE4 is in
data->res->res_compound4_extended, so heap used after free happens.
Actually we did not need to assign res_SEQUENCE4->sr_status with NFS4_OK
for replay request, because it will comes from slot->cached_result. Even
before commit 5f06bd1c40b89c726ccb0f0284caace66d12a1d0, the res_SEQUENCE4
also will be freed in complete_op by release_nfs4_res_compound, so we did
not need to assign the value of res_SEQUENCE4.

Change-Id: I9e3f4823e23718bf421c5237cc78a878b6f3014e
Signed-off-by: liyinshu <liyinshu@bytedance.com>

src/Protocols/NFS/nfs4_op_sequence.c
@@ -243,7 +243,6 @@ enum nfs_req_result nfs4_op_sequence(struct nfs_argop4 *op,
 				PTHREAD_MUTEX_unlock(&slot->lock);
 
 				dec_session_ref(session);
-				res_SEQUENCE4->sr_status = NFS4_OK;
 				return NFS_REQ_REPLAY;
 			} else {
 				/* Illegal replay */