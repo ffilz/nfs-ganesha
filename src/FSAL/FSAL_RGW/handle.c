8382de6366937a37f392ef90e179f62306dce0e0
Matt Benjamin <mbenjamin@redhat.com>
2023-04-22 21:15:39 -0400

    rgw: require rgw_file version 1.2.1 (rgw_readdir2 returns int)
    Required for build against Reef+.
    
    Change-Id: I5637517edd4ac141e3068b79f9ee76c85f56bdcf
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/FSAL/FSAL_RGW/handle.c
+++ src/FSAL/FSAL_RGW/handle.c
@@ -131,7 +131,7 @@ struct rgw_cb_arg {
 	attrmask_t attrmask;
 };
 
-static bool rgw_cb(const char *name, void *arg, uint64_t offset,
+static int rgw_cb(const char *name, void *arg, uint64_t offset,
 		struct stat *st, uint32_t st_mask, uint32_t flags)
 {
 	struct rgw_cb_arg *rgw_cb_arg = arg;
