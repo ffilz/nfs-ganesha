572d465d3673bd9274159c22687bf3b999eb0586
Rojin George <itsmerojin@gmail.com>
2023-09-25 14:04:12 +0530

    Keep expired client until reaching threshold of expired clients
    This merge request updates Ganesha reaper logic to avoid
    expiring client post lease period, unless the number of
    unresponsive clients go over a limit. Ganesha keeps track
    of all expired clients in LRU fashion and picks the oldest
    expired client when the number of clients exceeds the max
    limit. This allows Ganesha to retain the open & lock state
    and there by helping certain client workloads like MLPerf
    to run smoothly, even after a network partition.
    
    Change-Id: I4972fba60cc1add02935a47edaaab0bc904d4936
    Signed-off-by: Rojin George <itsmerojin@gmail.com>

--- src/Protocols/NFS/nfs4_op_lock.c
+++ src/Protocols/NFS/nfs4_op_lock.c
@@ -651,7 +651,7 @@ enum nfs_req_result nfs4_op_lock(struct nfs_argop4 *op,
 		res_LOCK4->status = NFS4ERR_BAD_STATEID;
 		goto out2;
 	}
-
+RETRY_ACQUIRE_LOCK:
 	/* Now we have a lock owner and a stateid.  Go ahead and push
 	 * lock into SAL (and FSAL). */
 	state_status = state_lock(obj,
@@ -667,7 +667,26 @@ enum nfs_req_result nfs4_op_lock(struct nfs_argop4 *op,
 		LogDebug(COMPONENT_NFS_V4_LOCK, "LOCK failed with status %s",
 			 state_err_str(state_status));
 
-		if (state_status == STATE_LOCK_CONFLICT) {
+		if (state_status == STATE_LOCK_CONFLICT && conflict_owner) {
+			nfs_client_id_t *client_id =
+			conflict_owner->so_owner.so_nfs4_owner.so_clientrec;
+			
+			if (client_id->marked_for_delayed_cleanup) {
+				/* Release any lock owner reference
+					* passed back from SAL
+					*/
+				dec_state_owner_ref(conflict_owner);
+
+				/* Release the state lock, to clean */
+				if (st_lock_held)
+					STATELOCK_unlock(obj);
+				reap_expired_client_list(client_id);
+				/* Acquire back the state lock */
+				if (st_lock_held)
+					STATELOCK_lock(obj);
+
+				goto RETRY_ACQUIRE_LOCK;
+			}
 			/* A conflicting lock from a different lock_owner,
 			 * returns NFS4ERR_DENIED, but check that the
 			 * response will fit, if not, return response error.
