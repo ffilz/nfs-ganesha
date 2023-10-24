ffilz/nfs-ganesha

commit 6e18be9236b952fd1dbba80b4ef6819e3f2f9a2d

FSAL cap retries for stat while resolving POSIX filesystems
Also, don't spin on stat, insert a small delay (default 100 msec)
between retries. Count and delay are configurable.

Change-Id: I6159a677e0524a7a4cd733f8afacf0206bc36f7c
Signed-off-by: Frank S. Filz <ffilzlnx@mindspring.com>

src/FSAL/localfs.c
@@ -1014,18 +1014,54 @@ int resolve_posix_filesystem(const char *path,
 {
 	int retval = EAGAIN;
 	struct stat statbuf;
+	uint32_t retries_left = nfs_param.core_param.resolve_fs_retries;
 
-	while (retval == EAGAIN) {
+	while (retval == EAGAIN && retries_left > 0) {
+		struct timespec how_long = {
+			/* 1M micros per second */
+			.tv_sec  = nfs_param.core_param.resolve_fs_delay / 1000,
+			/* Remainder => nanoseconds */
+			.tv_nsec = (nfs_param.core_param.resolve_fs_delay %
+				    1000) * 1000000
+		};
+
 		/* Need to retry stat on path until we don't get EAGAIN in case
 		 * autofs needed to mount the file system.
 		 */
 		retval = stat(path, &statbuf);
+
 		if (retval != 0) {
 			retval = errno;
 			LogDebug(COMPONENT_FSAL,
 				 "stat returned %s (%d) while resolving export path %s %s",
 				 strerror(retval), retval, path,
 				 retval == EAGAIN ? "(may retry)" : "(failed)");
+		}
+
+		retries_left--;
+
+		if (retries_left == 0) {
+			LogCrit(COMPONENT_FSAL,
+				"Stat on filesystem %s failed, no retries remaining.",
+				path);
+			break;
+		}
+
+		if (nanosleep(&how_long, NULL) != 0) {
+			/*
+			 * Let interrupts wake us up and not care. Anything else
+			 * should be fatal.
+			 */
+
+			if (errno != EINTR) {
+				retval = errno;
+				LogCrit(COMPONENT_FSAL,
+					"nanosleep failed. Asked for %"PRIu32
+					" msec. Errno %d (%s)",
+					nfs_param.core_param.resolve_fs_delay,
+					retval, strerror(retval));
+				return retval;
+			}
 		}
 	}
 

src/support/nfs_read_conf.c
@@ -299,6 +299,10 @@ static struct config_item core_params[] = {
 		       nfs_core_param, heartbeat_freq),
 	CONF_ITEM_BOOL("fsid_device", false,
 		       nfs_core_param, fsid_device),
+	CONF_ITEM_UI32("resolve_fs_retries", 1, 10, 1000,
+		       nfs_core_param, resolve_fs_retries),
+	CONF_ITEM_UI32("resolve_fs_delay", 1, 100, 1000,
+		       nfs_core_param, resolve_fs_delay),
 	CONF_ITEM_BOOL("mount_path_pseudo", false,
 		       nfs_core_param, mount_path_pseudo),
 	CONF_ITEM_ENUM_BITS("Enable_UDP", UDP_LISTENER_ALL, UDP_LISTENER_MASK,

src/include/gsh_config.h
@@ -428,6 +428,12 @@ typedef struct nfs_core_param {
 	uint32_t heartbeat_freq;
 	/** Whether to use device major/minor for fsid. Defaults to false. */
 	bool fsid_device;
+	/** How many times to attempt retry of stat while resolving POSIX
+	 *  filesystems */
+	uint32_t resolve_fs_retries;
+	/** Delay (in mili-seconds) between stat calls when trying to resolve
+	 *  POSIX filesystems */
+	uint32_t resolve_fs_delay;
 	/** Whether to use Pseudo (true) or Path (false) for NFS v3 and 9P
 	    mounts. */
 	bool mount_path_pseudo;

commit 0d64cb77b690bb7df331169b6dba9f92eeb2659c

FSAL: Fix some issues with resolving POSIX filesystems
Commit 6e18be9236b952fd1dbba80b4ef6819e3f2f9a2d accidentally
introduced a 1 second delay per export.

If stat() returns 0 then the initial loop iteration continues, causing
at least one call to nanosleep().  Instead, the loop should break on
success.

Why 1 second?  Arguments to CONF_ITEM_UI32() should look like:

  CONF_ITEM_UI32(_name_, _min_, _max_, _def_, _struct_, _mem_)

That is the 3rd argument should be the maximum and the 4th should be
the default.  These currently appear to be in the wrong order, since
the default is not within the range.  This is actually lucky because
with a timeout of 100ms we probably wouldn't have noticed the first
problem.  ;-)

Signed-off-by: Martin Schwenke <mschwenke@ddn.com>
Change-Id: If5e0ca60485a2e5f13d03fa5e2a3050ef6697eab

src/FSAL/localfs.c
@@ -1031,13 +1031,15 @@ int resolve_posix_filesystem(const char *path,
 		 */
 		retval = stat(path, &statbuf);
 
-		if (retval != 0) {
-			retval = errno;
-			LogDebug(COMPONENT_FSAL,
-				 "stat returned %s (%d) while resolving export path %s %s",
-				 strerror(retval), retval, path,
-				 retval == EAGAIN ? "(may retry)" : "(failed)");
+		if (retval == 0) {
+			break;
 		}
+
+		retval = errno;
+		LogDebug(COMPONENT_FSAL,
+			 "stat returned %s (%d) while resolving export path %s %s",
+			 strerror(retval), retval, path,
+			 retval == EAGAIN ? "(may retry)" : "(failed)");
 
 		retries_left--;
 

src/support/nfs_read_conf.c
@@ -301,9 +301,9 @@ static struct config_item core_params[] = {
 		       nfs_core_param, heartbeat_freq),
 	CONF_ITEM_BOOL("fsid_device", false,
 		       nfs_core_param, fsid_device),
-	CONF_ITEM_UI32("resolve_fs_retries", 1, 10, 1000,
+	CONF_ITEM_UI32("resolve_fs_retries", 1, 1000, 10,
 		       nfs_core_param, resolve_fs_retries),
-	CONF_ITEM_UI32("resolve_fs_delay", 1, 100, 1000,
+	CONF_ITEM_UI32("resolve_fs_delay", 1, 1000, 100,
 		       nfs_core_param, resolve_fs_delay),
 	CONF_ITEM_BOOL("mount_path_pseudo", false,
 		       nfs_core_param, mount_path_pseudo),