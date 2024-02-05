7a5baba77857ce686b47a875b959e92072df24c7
Shahar Hochma <shaharhoch@google.com>
2024-02-05 13:36:17 +0000

    Change link, unlink and rename operations to return attomic pre/post attributes
    This is done in order to allow returning atomic pre and post attrs if
    the underlying FSAL supports it.
    
    Change-Id: Iffcaeb83a866cab15ae54527835b713a044d0bc8
    Signed-off-by: Shahar Hochma <shaharhoch@google.com>

--- src/FSAL/FSAL_CEPH/handle.c
+++ src/FSAL/FSAL_CEPH/handle.c
@@ -764,7 +764,9 @@ out_err:
 
 static fsal_status_t ceph_fsal_link(struct fsal_obj_handle *handle_pub,
 			       struct fsal_obj_handle *destdir_pub,
-			       const char *name)
+			       const char *name,
+			       struct fsal_attrlist *destdir_pre_attrs_out,
+			       struct fsal_attrlist *destdir_post_attrs_out)
 {
 	/* Generic status return */
 	int rc = 0;
@@ -796,6 +798,14 @@ static fsal_status_t ceph_fsal_link(struct fsal_obj_handle *handle_pub,
  * @param[in] old_name   Original name
  * @param[in] newdir_pub Destination directory
  * @param[in] new_name   New name
+ * @param[in,out] olddir_pre_attrs_out Optional attributes for olddir dir
+ *					before the operation. Should be atomic.
+ * @param[in,out] olddir_post_attrs_out Optional attributes for olddir dir
+ *					after the operation. Should be atomic.
+ * @param[in,out] newdir_pre_attrs_out Optional attributes for newdir dir
+ *					before the operation. Should be atomic.
+ * @param[in,out] newdir_post_attrs_out Optional attributes for newdir dir
+ *					after the operation. Should be atomic.
  *
  * @return FSAL status.
  */
@@ -804,7 +814,11 @@ static fsal_status_t ceph_fsal_rename(struct fsal_obj_handle *obj_hdl,
 				 struct fsal_obj_handle *olddir_pub,
 				 const char *old_name,
 				 struct fsal_obj_handle *newdir_pub,
-				 const char *new_name)
+				 const char *new_name,
+				 struct fsal_attrlist *olddir_pre_attrs_out,
+				 struct fsal_attrlist *olddir_post_attrs_out,
+				 struct fsal_attrlist *newdir_pre_attrs_out,
+				 struct fsal_attrlist *newdir_post_attrs_out)
 {
 	/* Generic status return */
 	int rc = 0;
@@ -842,13 +856,20 @@ static fsal_status_t ceph_fsal_rename(struct fsal_obj_handle *obj_hdl,
  *
  * @param[in] dir_pub Parent directory
  * @param[in] name    Name to remove
+ * @param[in] obj_hdl The object being removed
+ * @param[in,out] parent_pre_attrs_out Optional attributes for parent dir before
+ *				       the operation. Should be atomic.
+ * @param[in,out] parent_post_attrs_out Optional attributes for parent dir
+ *					after the operation. Should be atomic.
  *
  * @return FSAL status.
  */
 
 static fsal_status_t ceph_fsal_unlink(struct fsal_obj_handle *dir_pub,
-				      struct fsal_obj_handle *obj_pub,
-				      const char *name)
+	struct fsal_obj_handle *obj_pub,
+	const char *name,
+	struct fsal_attrlist *parent_pre_attrs_out,
+	struct fsal_attrlist *parent_post_attrs_out)
 {
 	/* Generic status return */
 	int rc = 0;
