905a1f3453fe8cb3efb56a7a30ffd5aeabb53d74
Rojin George <itsmerojin@gmail.com>
2024-07-22 09:43:55 +0530

    Skip Delegation Recall Async task, if the client_id goes stale
    Change-Id: Ib3849f8346226ea3bdde92bd994f7455fc1024d6
    Signed-off-by: Rojin George <itsmerojin@gmail.com>

--- src/FSAL_UP/fsal_up_top.c
+++ src/FSAL_UP/fsal_up_top.c
@@ -1518,6 +1518,16 @@ state_status_t delegrecall_impl(struct fsal_obj_handle *obj)
 		}
 
 		client_id = owner->so_owner.so_nfs4_owner.so_clientrec;
+		if (!client_id ||
+			!atomic_fetch_int32_t(&client_id->cid_refcount)) {
+			/* Let's release the owner ref &*/
+			dec_state_owner_ref(owner);
+			LogDebug(COMPONENT_FSAL_UP,
+				 "Client id within owner has gone stale, no need to recall delegation");
+			gsh_free(drc_ctx);
+			*deleg_state = DELEG_GRANTED;
+			continue;
+		}
 		inc_client_id_ref(client_id);
 		dec_state_owner_ref(owner);
 
