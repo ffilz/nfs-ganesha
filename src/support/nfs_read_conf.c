20cc4d7d2e4d43d75703dab1614b5ac00cbb0a7b
Rojin George <itsmerojin@gmail.com>
2024-09-11 10:17:45 +0000

    State FD Limit for NFSv4 LOCK and NLM Lock/Share
    Change-Id: Ie5f5e4a6de826e6384ff9569637e2b9318edc4f7
    Signed-off-by: Rojin George <itsmerojin@gmail.com>

--- src/support/nfs_read_conf.c
+++ src/support/nfs_read_conf.c
@@ -369,6 +369,8 @@ static struct config_item core_params[] = {
 		       enable_connection_manager),
 	CONF_ITEM_UI32("Connection_Manager_Timeout_sec", 0, UINT32_MAX, 2 * 60,
 		       nfs_core_param, connection_manager_timeout_sec),
+	CONF_ITEM_UI32("Max_Locks_Allowed_Percent", 0, 50, 45,
+		       nfs_core_param, nfs_max_locks_allowed_pct),
 	CONFIG_EOL
 };
 
