a29c31fe276b1532b4c3d511542b9d10ef34d1b4
Deeraj.Patil <Deeraj.Patil@ibm.com>
2025-02-19 01:16:04 -0500

    QOS Implementation.
    1. IO bandwidth control (Data, Bytes Per Second).
    2. IOPS (IO Operation Per Second).
    2. Quota (N/W data Limit over a larger time).
    
    For managing the QOS at export level and client level,
    same structure gets used: struct Qos_Class .
    
    Bandwidth Control:
    Two types of approaches are there for bandwidth control: SYNC and ASYNC.
    Sync Control : Per-Export, Per-Client. (macro in code currently disabled)
    ASYNC Control : Per-Export, Per-Client, PerExport-PerClient Combined.
    Default ASYNC macro is enabled in code (!BW_SYNC_ENABLE).
    Default ASYNC type is PerExport-PerClient.
    
    IOPS Control:
    Only ASYNC IOPS Control is supported.
    Can be enabled at : Per-Export, Per-Client, PerExport-PerClient.
    Default type is PerExport-PerClient(Combined Only).
    
    Quota Control:
    i.e (Network Data Consumtion limit over a period of larger time).
    Per-Export level, Per-Client level, or can PerExport-PerClient level.
    Default : Quota is currently disabled.
    
    Can indvidually enable BW control, IOPS Control, Quota.
    Limit values for Bandwidth and Quota can be controlled indvidually for
    Write and Read or can be combined together.
    For IOPS only Combined accounting supported (can be extended in future).
    
    For enabling QOS, enable_qos params should be true in global config.
    Default value for enable_qos in global config is false.
    
    For Controlling ratelimiting values dynamically :
            1.DBUS interface (Currently only for BW control).
            2.Dynamic config update at export level(Using SIGHUP).
    
    QOS CompileTime enable and disablement,
    using ENABLE_QOS flag while doing CMAKE.
    
    Implemented by naresh.chillarege@ibm.com and deeraj.patil@ibm.com
    
    Change-Id: I36f064ef91ba3ba2ef6fd0fcb3d490a8ff114757
    Signed-off-by: Deeraj.Patil <Deeraj.Patil@ibm.com>

--- src/MainNFSD/nfs_admin_thread.c
+++ src/MainNFSD/nfs_admin_thread.c
@@ -60,6 +60,7 @@
 #endif
 #include "conf_url.h"
 #include "nfs_rpc_callback.h"
+#include "nfs_qos.h"
 
 /**
  * @brief Mutex protecting shutdown flag.
@@ -851,6 +852,11 @@ static void do_shutdown(void)
 	LogEvent(COMPONENT_MAIN, "Stopping delayed executor.");
 	delayed_shutdown();
 	LogEvent(COMPONENT_MAIN, "Delayed executor stopped.");
+
+#ifdef ENABLE_QOS
+	/* QOS shutdown */
+	shutdown_qos();
+#endif
 
 	LogEvent(COMPONENT_MAIN, "Stopping state asynchronous request thread");
 	rc = state_async_shutdown();
