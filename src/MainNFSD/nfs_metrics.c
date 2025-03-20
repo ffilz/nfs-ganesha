f08e8b4637b0c6585bf267158ce0b7bb9c073046
Frank S. Filz <ffilzlnx@mindspring.com>
2025-03-20 16:42:38 -0700

    Added mntpath label for export_id metrics.
    Change-Id: Ib377a633ca42d6761e724b04883a95dc823c1d11
    Signed-off-by: Sreedhar.Agraharam <sragraha@redhat.com>

--- src/MainNFSD/nfs_metrics.c
+++ src/MainNFSD/nfs_metrics.c
@@ -333,28 +333,30 @@ void nfs_metrics__rpcs_in_flight(int64_t value)
 #ifdef _USE_NFS3
 void nfs_metrics__nfs3_request(uint32_t proc, nsecs_elapsed_t request_time,
 			       nfsstat3 nfs_status, export_id_t export_id,
-			       const char *client_ip)
+			       const char *mntpath, const char *client_ip)
 {
 	const char *const version = "nfs3";
 	const char *const operation = nfsproc3_to_str(proc);
 	const char *const status_label = nfsstat3_to_str(nfs_status);
 
 	dynamic_metrics__observe_nfs_request(operation, request_time, version,
-					     status_label, export_id,
+					     status_label, export_id, mntpath,
 					     client_ip);
 }
 #endif
 
-void nfs_metrics__nfs4_request(uint32_t op, nsecs_elapsed_t request_time,
-			       nfsstat4 status, export_id_t export_id,
-			       const char *client_ip)
+void nfs_metrics__nfs4_request(const uint32_t op,
+			       const nsecs_elapsed_t request_time,
+			       const nfsstat4 status,
+			       const export_id_t export_id,
+			       const char *mntpath, const char *client_ip)
 {
 	const char *const version = "nfs4";
 	const char *const operation = nfsop4_to_str(op);
 	const char *const status_label = nfsstat4_to_str(status);
 
 	dynamic_metrics__observe_nfs_request(operation, request_time, version,
-					     status_label, export_id,
+					     status_label, export_id, mntpath,
 					     client_ip);
 }
 
