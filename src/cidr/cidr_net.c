771bda842f13bc839a1f69bf68a3597251da6ad9
s-athani <suhas.athani@ibm.com>
2025-04-22 02:02:15 -0400

    Coverity 502077 Overflowed array index write: In cidr_net_supernet: Write to array at integer-overflowed index. Fix integer underflow risk by validating CIDR prefix length.
    Change-Id: Icd31e18d4f830f3b6ba4816fc5a39761df21b239
    Signed-off-by: s-athani <suhas.athani@ibm.com>

--- src/cidr/cidr_net.c
+++ src/cidr/cidr_net.c
@@ -45,8 +45,13 @@ CIDR *cidr_net_supernet(const CIDR *addr)
 	int pflen;
 	CIDR *toret;
 
-	/* If it's already a /0 in its protocol, return nothing */
 	pflen = cidr_get_pflen(addr);
+
+	/* If prefix length value returned is -1 */
+	if (pflen < 0)
+		return (NULL);
+
+	/* If it's already a /0 in its protocol, return nothing */
 	if (pflen == 0) {
 		errno = 0;
 		return (NULL);
