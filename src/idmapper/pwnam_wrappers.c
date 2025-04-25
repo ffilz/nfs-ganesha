c5c17b0a579f6f2ea6ce817adb0d4993e2b8b065
Lior Suliman <liorsu@gmail.com>
2025-04-25 17:20:05 +0000

    NFS-Ganesha: Add pwnam wrappers module
    This commit adds wrappers for the `pwnam` functions.
    This will allow to add an alternative implementations
    to these functions.
    
    Change-Id: I3dbeed086e8b88fab1fffc09f833cdd9eea18300
    Signed-off-by: Lior Suliman <liorsu@gmail.com>

---
+++ src/idmapper/pwnam_wrappers.c
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+/*
+ * Copyright CEA/DAM/DIF  (2008)
+ * contributeur : Lior Suliman   liorsu@google.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ * ---------------------------------------
+ */
+
+/**
+ * @addtogroup pwnam_wrappers
+ * @{
+ */
+
+/**
+ * @file pwnam_wrappers.c
+ * @brief pwnam wrappers
+ */
+
+#include "third_party/nfs_ganesha/filestore/src/include/pwnam_wrappers.h"
+
+int pwnam_wrappers__getgrouplist(const char *user, gid_t group, gid_t *groups,
+				 int *ngroups)
+{
+	return getgrouplist(user, group, groups, ngroups);
+}
+
+int pwnam_wrappers__getpwnam_r(const char *name, struct passwd *pwd, char *buf,
+			       size_t buflen, struct passwd **result)
+{
+	return getpwnam_r(name, pwd, buf, buflen, result);
+}
+
+int pwnam_wrappers__getpwuid_r(uid_t uid, struct passwd *pwd, char *buf,
+			       size_t buflen, struct passwd **result)
+{
+	return getpwuid_r(uid, pwd, buf, buflen, result);
+}
+
+int pwnam_wrappers__getgrnam_r(const char *name, struct group *grp, char *buf,
+			       size_t buflen, struct group **result)
+{
+	return getgrnam_r(name, grp, buf, buflen, result);
+}
+
+int pwnam_wrappers__getgrgid_r(gid_t gid, struct group *grp, char *buf,
+			       size_t buflen, struct group **result)
+{
+	return getgrgid_r(gid, grp, buf, buflen, result);
+}
