df6a2e5ac9e9a3c466a3625affbd135b46edb06a
Gaurav Gangalwar <gaurav.gangalwar@gmail.com>
2025-08-05 07:30:45 -0400

    Enable callback channel setup for RDMA.
    Tirpc PR https://github.com/nfs-ganesha/ntirpc/pull/353
    
    Change-Id: Ib0bc0fa844a7fe58d49ced1fb95e3911c3def167
    Signed-off-by: Gaurav Gangalwar <gaurav.gangalwar@gmail.com>

--- src/MainNFSD/nfs_rpc_callback.c
+++ src/MainNFSD/nfs_rpc_callback.c
@@ -797,18 +797,18 @@ int nfs_rpc_create_chan_v41(SVCXPRT *xprt, nfs41_session_t *session,
 	assert(xprt);
 
 	if (svc_get_xprt_type(xprt) == XPRT_RDMA) {
-		LogWarn(COMPONENT_NFS_CB,
-			"refusing to create back channel over RDMA for now");
-		code = EINVAL;
-		goto out;
+		chan->clnt = clnt_rdma_ncreatef(xprt, session->cb_program,
+						NFS_CB, CLNT_CREATE_FLAG_NONE,
+						false);
+	} else {
+		/*
+		 * connect an RPC client
+		 * Use version 1 per errata ID 2291 for RFC 5661
+		 */
+		chan->clnt = clnt_vc_ncreate_svc(xprt, session->cb_program,
+						 NFS_CB /* Errata ID: 2291 */,
+						 CLNT_CREATE_FLAG_NONE);
 	}
-
-	/* connect an RPC client
-	 * Use version 1 per errata ID 2291 for RFC 5661
-	 */
-	chan->clnt = clnt_vc_ncreate_svc(xprt, session->cb_program,
-					 NFS_CB /* Errata ID: 2291 */,
-					 CLNT_CREATE_FLAG_NONE);
 
 	if (CLNT_FAILURE(chan->clnt)) {
 		err = rpc_sperror(&chan->clnt->cl_error, "failed");
