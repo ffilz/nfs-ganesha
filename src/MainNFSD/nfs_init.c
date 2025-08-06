48478683121f81ed339f09b2d8fb5266b0460fca
Deeraj.Patil <Deeraj.Patil@ibm.com>
2025-08-06 17:57:38 -0400

    NFS Ganesha - TLS support.
       TLS entertained via AUTH_TLS and non-AUTH_TLS.
       KTLS and non KTLS based send and recv.
       export option introduced : XprtSec= none/mtls/tls
       Client mount supported matrix is updated in man page.
       Can control export specific TLS/MTLS required or not.
    
       Libntirpc changes :
       backend TLS lib : Openssl, GnuTLS.
       Compile time option for enabling : USE_TLS, USE_OPENSSL, USE_GNUTLS.
    
    Change-Id: I97f93258bc72e57ce5f0683be3b78a31915befaa
    Signed-off-by: Deeraj.Patil <Deeraj.Patil@ibm.com>

--- src/MainNFSD/nfs_init.c
+++ src/MainNFSD/nfs_init.c
@@ -746,6 +746,21 @@ int nfs_set_param_from_conf(config_file_t parse_tree,
 	}
 #endif
 
+#ifdef USE_TLS
+	/* TLS global parameters - validation handled by tls_config_commit() */
+	(void)load_config_from_parse(parse_tree, &tls_core, &tls_config, true,
+				     err_type);
+	if (!config_error_is_harmless(err_type)) {
+		LogCrit(COMPONENT_INIT,
+			"Error while parsing TLS configuration");
+		return -1;
+	}
+
+	/* Initialize TLS if enabled */
+	if (tls_config.enabled == true)
+		tls_init(&tls_config);
+#endif
+
 	/* Directory Services specific configuration */
 	(void)load_config_from_parse(parse_tree, &directory_services_param,
 				     ds_param, true, err_type);
@@ -1367,6 +1382,9 @@ void nfs_start(nfs_start_info_t *p_start_info)
 	/* Regular exit */
 	LogEvent(COMPONENT_MAIN, "NFS EXIT: regular exit");
 
+#ifdef USE_TLS
+	tls_cleanup();
+#endif
 	nfs_init_cleanup();
 
 	Cleanup();
