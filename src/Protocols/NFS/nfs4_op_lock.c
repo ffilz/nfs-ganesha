ffilz/nfs-ganesha

commit 1cfef0bde73e0dca0a347df7e0cad9d31ea57599

NFSv4.0: fix crash in release_cb_slot() due to uninitialized mutex.
In NFSv4.0, nfsv4_granted_callback() construct of RPC done by getting the chan
and basic required entities and does the RPC. But cleanup of the same is tried
w.r.t 4.1 construct. This led to a crash from accessing an uninitialized mutex.

Ensure that nfs41_release_single() is only invoked for NFSv4.1, not for NFSv4.0.

Change-Id: Id20ef65a92c2b3d1c921141ca81a6dcd67c84a2e
Signed-off-by: Deeraj.Patil <Deeraj.Patil@ibm.com>

src/Protocols/NFS/nfs4_op_lock.c
@@ -66,7 +66,8 @@ static const char *lock_tag = "LOCK";
 
 static void notify_granted_completion(rpc_call_t *call)
 {
-	nfs41_release_single(call);
+	if (call->chan->source.clientid->cid_minorversion != 0)
+		nfs41_release_single(call);
 }
 
 static state_status_t nfsv4_granted_callback(struct fsal_obj_handle *obj,

commit 457b770e57a027eb8ce3a2c2c7490520f77a9e03

Fix notify_granted_completion for NFSv4.1
This fixes an issue introduced in:
https://github.com/nfs-ganesha/nfs-ganesha/commit/1cfef0bde73e0dca0a347df7e0cad9d31ea57599

The patch changed it so up channel slot release is only called for NFSv4.1, but it used the clientid under the channel.
In NFSv4.1 the clientid is not initialized in the channel. The channel keeps a union between the clientid (for NFSv4.0) and the session (for nfsv4.1).

So, in NFSv4.1, the check looked at garbage, which means that in some cases the slot was never released.

We change the check to look at the channel type, which is always initialized.

Change-Id: I9565b148cfdd6a8d1e663e7d12f0d2705294899b
Signed-off-by: Shahar Hochma <shaharhoch@google.com>

src/Protocols/NFS/nfs4_op_lock.c
@@ -66,7 +66,7 @@ static const char *lock_tag = "LOCK";
 
 static void notify_granted_completion(rpc_call_t *call)
 {
-	if (call->chan->source.clientid->cid_minorversion != 0)
+	if (call->chan->type == RPC_CHAN_V41)
 		nfs41_release_single(call);
 }
 