a81a52b21d1efaed608ee43315b3aeac57e01f85
Naresh Chillarege <naresh.chillarege@ibm.com>
2025-09-14 23:55:28 -0400

    Cluster QoS module implementation
    The Cluster QoS feature does bandwidth rate-limiting and
    iops rate-limiting for across all nodes of cluster.
    
    It makes sure that aggregate bandwidth and iops of all
    nodes is with in the configured value.
    
    Change-Id: I985895068b54ae9404b5e9eb3cb1b69b8560849c
    Signed-off-by: Naresh Chillarege <naresh.chillarege@ibm.com>

--- src/support/nfs_read_conf.c
+++ src/support/nfs_read_conf.c
@@ -56,6 +56,7 @@
 #include "config_parsing.h"
 #include "pwnam_wrappers.h"
 #include "nfs_qos.h"
+#include "nfs_cluster_qos.h"
 
 /**
  * @brief Core configuration parameters
@@ -266,11 +267,53 @@ void remove_self_cluster_members(void)
 	freeifaddrs(ifap);
 }
 
+void populate_cqos_hosts(void)
+{
+	struct glist_head *glist;
+	struct base_client_entry *client;
+	struct cqos_ceph_nodes *cli;
+	char *ip_string;
+
+	if (glist_empty(&nfs_param.core_param.cluster_members))
+		return;
+
+	glist_for_each(glist, &nfs_param.core_param.cluster_members) {
+	      client = glist_entry(glist, struct base_client_entry, cle_list);
+
+		cli = gsh_calloc(1, sizeof(struct cqos_ceph_nodes));
+		glist_init(&cli->node_list);
+		cli->fd = -1;
+		cli->is_my_ip = false;
+		cli->clnt = NULL;
+
+                cli->node_addr = client->cidr->ip_addr;
+
+		ip_string = gsh_calloc(SOCK_NAME_MAX, sizeof(char));
+
+		struct display_buffer dspbuf = { SOCK_NAME_MAX * sizeof(char),
+						ip_string, ip_string };
+
+		display_sockip(&dspbuf, &cli->node_addr);
+		if (is_self_address(ip_string) == true)
+                        cli->is_my_ip = true;
+
+		gsh_free(ip_string);
+
+		glist_add_tail(&cqos_hosts, &cli->node_list);
+	}
+}
+
 static int core_commit(void *node, void *link_mem, void *self_struct,
 		       struct config_error_type *err_type)
 {
 	LogDebug(COMPONENT_CONFIG, "NFS_CORE_PARAM commit");
 
+	/*
+	 * Cluster QoS global structures needs to be initialized
+	 * with cluster_members details
+	 */
+	populate_cqos_hosts();
+	
 	remove_self_cluster_members();
 
 	Log_ClientList_Level(COMPONENT_CONFIG, NIV_INFO, "Cluster_Members",
@@ -313,6 +356,10 @@ static struct config_item core_params[] = {
 	CONF_ITEM_UI16("Rquota_Port", 0, UINT16_MAX, RQUOTA_PORT,
 		       nfs_core_param, port[P_RQUOTA]),
 #endif
+#ifdef ENABLE_CLUSTER_QOS
+	CONF_ITEM_UI16("Cqos_Port", 0, UINT16_MAX, CQOS_PORT,
+		       nfs_core_param, port[P_CQOS]),
+#endif
 #ifdef _USE_NFS_RDMA
 	CONF_ITEM_UI16("NFS_RDMA_Port", 0, UINT16_MAX, NFS_RDMA_PORT,
 		       nfs_core_param, port[P_NFS_RDMA]),
@@ -337,6 +384,10 @@ static struct config_item core_params[] = {
 #ifdef _USE_RQUOTA
 	CONF_ITEM_UI32("Rquota_Program", 1, INT32_MAX, RQUOTAPROG,
 		       nfs_core_param, program[P_RQUOTA]),
+#endif
+#ifdef ENABLE_CLUSTER_QOS
+	CONF_ITEM_UI32("Cqos_Program", 1, INT32_MAX, CQOSPROG,
+		       nfs_core_param, program[P_CQOS]),
 #endif
 #ifdef USE_NFSACL3
 	CONF_ITEM_UI32("NFSACL_Program", 1, INT32_MAX, NFSACLPROG,
@@ -524,6 +575,12 @@ static struct config_item_list qos_types_supported[] = {
 static struct config_item qos_global_params[] = {
 	CONF_ITEM_BOOL("enable_qos", false, qos_block_config, enable_qos),
 
+#if ENABLE_CLUSTER_QOS
+	CONF_ITEM_BOOL("enable_cluster_qos", true, qos_block_config,
+			enable_cluster_qos),
+	CONF_ITEM_UI32("cqos_msg_interval", CQOS_MIN_MSGTIME, CQOS_MAX_MSGTIME,
+		       CQOS_DEF_MSGTIME, qos_block_config, cqos_msg_interval),
+#endif
 	CONF_ITEM_BOOL("enable_tokens", false, qos_block_config, enable_tokens),
 	CONF_ITEM_BOOL("enable_bw_control", false, qos_block_config,
 		       enable_bw_control),
@@ -622,6 +679,7 @@ struct config_block qos_core = {
 };
 
 #endif
+
 /**
  * @brief Kerberos/GSSAPI parameters
  */
