1a899cd72977c062caae2340bf9a1be4b4091729
Sreedhar Agraharam <sragraha@redhat.com>
2026-03-17 02:20:35 -0400

    Enhance header files to support both C and C++ compilation
    This change standardizes the header interface, making it
    robust for use of both C and C++ source files, which is
    critical for prometheus .cc files usability.
    
    Change-Id: Id8355941660d40654dc3a7d2f5293d2570996f8f
    Signed-off-by: Sreedhar Agraharam <sragraha@redhat.com>

--- src/include/nfsv41.h
+++ src/include/nfsv41.h
@@ -8168,9 +8168,7 @@ static inline bool xdr_nfs_opnum4(XDR *xdrs, nfs_opnum4 *objp)
 
 static inline bool xdr_nfs_argop4(XDR *xdrs, nfs_argop4 *objp)
 {
-	struct nfs_request_lookahead slhd = { .flags = 0,
-					      .read = 0,
-					      .write = 0 };
+	struct nfs_request_lookahead slhd = { 0, 0, 0 };
 	struct nfs_request_lookahead *lkhd =
 		xdrs->x_public ? (struct nfs_request_lookahead *)xdrs->x_public
 			       : &slhd;
@@ -9786,6 +9784,6 @@ static inline void copy_into_utf8string(utf8string *dest, const char *src,
 
 #ifdef __cplusplus
 }
-#endif
+#endif /* extern "C" */
 
 #endif /* !_NFSV41_H_RPCGEN */
