7e2506d98b8f35d6a483cb500753fa3159769a69
Ofir Vainshtein <ofirvins@google.com>
2026-03-22 15:32:11 +0000

    Add a rolling log index to Ganesha logs.
    Introduces a new log format option, LOG_INDEX, which adds a monotonically increasing index to each log line. The index is a 5-digit number that wraps around at 100000. This helps in tracking the order of log messages. The super_ut configuration is updated to enable this new option.
    
    note because index is taken before write and not under exclusive lock with it. logs are not guaranteed to appear in order in the file
    
    Change-Id: Ieb251326238810d9c3251b36a0d40c2d4844d52d
    Signed-off-by: Ofir Vainshtein <ofirvins@google.com>

--- src/doc/man/ganesha-log-config.rst
+++ src/doc/man/ganesha-log-config.rst
@@ -283,6 +283,10 @@ time_format(enum,default ganesha)
 
 **CLIENT_REQ_XID(bool, default false)**
 
+**LOG_INDEX(bool, default false)**
+
+**LOG_INDEX_WRAP_AROUND(uint32_t, default 100000)**
+
 See also
 ==============================
 :doc:`ganesha-config <ganesha-config>`\(8)
--- src/log/log_functions.c
+++ src/log/log_functions.c
@@ -178,6 +178,8 @@ struct logfields {
 	bool disp_level;
 	bool disp_op_id;
 	bool disp_client_req_xid;
+	bool disp_log_index;
+	uint32_t log_index_wrap_around;
 	enum timedate_formats_t datefmt;
 	enum timedate_formats_t timefmt;
 	char *user_date_fmt;
@@ -200,6 +202,8 @@ static struct logfields default_logfields = { .disp_epoch = true,
 					      .disp_funct = true,
 					      .disp_comp = true,
 					      .disp_level = true,
+					      .disp_log_index = false,
+					      .log_index_wrap_around = 100000,
 					      .datefmt = TD_GANESHA,
 					      .timefmt = TD_GANESHA };
 
@@ -1498,6 +1502,14 @@ static int display_log_component(struct display_buffer *dsp_log,
 				op_ctx->nfs_reqdata->svc.rq_msg.rm_xid);
 	}
 
+	if (b_left > 0 && logfields->disp_log_index) {
+		static int64_t log_index;
+		const int64_t current_index =
+			atomic_relaxed_inc_int64_t(&log_index) %
+			logfields->log_index_wrap_around;
+		b_left = display_printf(dsp_log, " #%05u :", current_index);
+	}
+
 	/* If we overflowed the buffer with the header, just skip it. */
 	if (b_left == 0) {
 		display_reset_buffer(dsp_log);
@@ -2095,6 +2107,9 @@ static struct config_item format_options[] = {
 	CONF_ITEM_BOOL("LEVEL", true, logfields, disp_level),
 	CONF_ITEM_BOOL("OP_ID", false, logfields, disp_op_id),
 	CONF_ITEM_BOOL("CLIENT_REQ_XID", false, logfields, disp_client_req_xid),
+	CONF_ITEM_BOOL("LOG_INDEX", false, logfields, disp_log_index),
+	CONF_ITEM_UI32("LOG_INDEX_WRAP_AROUND", 1000, UINT32_MAX, 100000,
+		       logfields, log_index_wrap_around),
 	CONFIG_EOL
 };
 
