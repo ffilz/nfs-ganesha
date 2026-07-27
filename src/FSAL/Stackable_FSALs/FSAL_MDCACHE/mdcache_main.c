85d82f663f409c5a3a2b7f37833ea856c37604a1
VidyaThumukunta <vidya.thumukunta@ibm.com>
2026-07-27 03:46:55 -0400

    Grpc: Add remaining export statistics APIs
    Implement gRPC equivalent APIs for the remaining export statistics D-Bus methods:
    
    - GetFULLV3Stats
    - GetFULLV4Stats
    - GetFastOPS
    - GetNFSv41Layouts
    - GetNFSv42Layouts
    - Get9pIO
    - Get9pOpStats
    - EnableStats
    - DisableStats
    - ResetStats
    - StatusStats
    - GetAuthStats
    - GetExportDetails
    - ShowFDUsage
    - ShowMDCache
    
    The implementation preserves D-Bus behavior while introducing common
    helper functions to reduce duplicated logic across protocol-specific
    statistics handlers.
    
    Change-Id: I428fa1321a33983e11876cc5d5cb9d1522dc12f3
    Signed-off-by: VidyaThumukunta <vidya.thumukunta@ibm.com>

--- src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_main.c
+++ src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_main.c
@@ -48,6 +48,7 @@
 #include "FSAL/fsal_commonlib.h"
 #include "mdcache_hash.h"
 #include "mdcache_lru.h"
+#include "server_stats_grpc.h"
 
 pool_t *mdcache_entry_pool;
 
@@ -476,4 +477,42 @@ void mdcache_utilization(DBusMessageIter *iter)
 	dbus_message_iter_close_container(iter, &struct_iter);
 }
 #endif /* USE_DBUS */
+
+#ifdef USE_GRPC
+/**
+ * @brief Retrieve MDCACHE statistics and LRU utilization metrics.
+ */
+bool grpc_show_mdcache_stats(struct grpc_mdcache_stats *cache_out,
+			     struct grpc_lru_utilization *lru_out,
+			     struct timespec *time_out, bool *success,
+			     char *errmsg, size_t errmsg_len)
+{
+	*success = true;
+	strlcpy(errmsg, "OK", errmsg_len);
+
+	now(time_out);
+
+	/* MDCACHE statistics */
+	cache_out->cache_requests = cache_st.inode_req;
+	cache_out->cache_hits = cache_st.inode_hit;
+	cache_out->cache_misses = cache_st.inode_miss;
+	cache_out->cache_conflicts = cache_st.inode_conf;
+	cache_out->cache_adds = cache_st.inode_added;
+	cache_out->cache_mapping = cache_st.inode_mapping;
+
+	/* LRU utilization */
+	lru_out->open_fds = atomic_fetch_int32_t(&fsal_fd_global_counter);
+
+	lru_out->entries_used = atomic_fetch_uint64_t(&lru_state.entries_used);
+
+	lru_out->chunks_used = atomic_fetch_uint64_t(&lru_state.chunks_used);
+
+	lru_out->fd_state = atomic_fetch_uint32_t(&fd_lru_state.fd_state);
+
+	lru_out->fd_limit =
+		atomic_fetch_uint32_t(&fd_lru_state.fds_system_imposed);
+
+	return true;
+}
+#endif /* USE_GRPC */
 /** @} */
