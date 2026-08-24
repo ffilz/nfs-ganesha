ffilz/nfs-ganesha

commit 7871a4657bf4a436acb3169f0606e4912079ec5a

Add 'all' option to ganesha_stats for client statistics
This commit adds support for querying statistics for all connected NFS
clients in a single command, eliminating the need to query each client
individually.

Features:
- client_io_ops all: Show I/O operations for all clients
- client_all_ops all: Show all NFS operations for all clients
- Enhanced output with client count and numbered display
- JSON format support for both commands
- Backward compatible with existing IP-based queries

Usage:
          ganesha_stats client_io_ops all
          ganesha_stats client_all_ops all
          ganesha_stats json client_io_ops all
          ganesha_stats json client_all_ops all

Change-Id: I4091cba21e22aa7f1d9902dfdc5302764bc08bfe
Signed-off-by: Prabhu Murugesan <prabhu.murugesan1@ibm.com>

src/include/server_stats_private.h
@@ -235,11 +235,13 @@ struct auth_stats {
 	{ .name = "read", .type = "(tttttt)", .direction = "out" }, \
 		{ .name = "write", .type = "(tttttt)", .direction = "out" }
 
+#define CEIOSTATS_TYPE   "(ttdt)(ttdt)(ttd)"   /* read, write, other */
 #define CEIOSTATS_REPLY                                                    \
 	{ .name = "read", .type = "(ttdt)", .direction = "out" },          \
 		{ .name = "write", .type = "(ttdt)", .direction = "out" }, \
 		{ .name = "other", .type = "(ttd)", .direction = "out" }
 
+#define CELOSTATS_TYPE   "(ttt)"               /* layout */
 #define CELOSTATS_REPLY \
 	{ .name = "layout", .type = "(ttt)", .direction = "out" }
 
@@ -264,18 +266,78 @@ struct auth_stats {
 		CEIOSTATS_REPLY, CELOSTATS_REPLY
 #endif
 
+/* Type signature for array of client I/O stats (for GetAllClientIOops) */
 #ifdef _USE_NFS3
+#define CE_STATS_TYPE                            \
+	"b" CEIOSTATS_TYPE                       /* clnt_v3  */ \
+	"b" CEIOSTATS_TYPE                       /* clnt_v40 */ \
+	"b" CEIOSTATS_TYPE CELOSTATS_TYPE        /* clnt_v41 */ \
+	"b" CEIOSTATS_TYPE CELOSTATS_TYPE        /* clnt_v42 */
+#else
+#define CE_STATS_TYPE                            \
+	CEIOSTATS_TYPE                           /* unconditional */ \
+	"b" CEIOSTATS_TYPE                       /* clnt_v40 */ \
+	"b" CEIOSTATS_TYPE CELOSTATS_TYPE        /* clnt_v41 */ \
+	"b" CEIOSTATS_TYPE CELOSTATS_TYPE        /* clnt_v42 */
+#endif
+
+/* Array reply macro for all clients stats - wraps CE_STATS_REPLY in
+ * array format */
+#define CE_STATS_ARRAY_REPLY                          \
+	{ .name = "clients",                          \
+	  .type = "a(s(tt)" CE_STATS_TYPE ")",        \
+	  .direction = "out" }
+
+/* Type signature for array of client all_ops stats (for GetAllClientAllops) */
+#ifdef _USE_NFS3
+#ifdef _USE_NLM
+#define CE_ALL_OPS_TYPE                          \
+	"b" CLNT_V3NLM_OPS_TYPE                  /* clnt_v3  */ \
+	"b" CLNT_V3NLM_OPS_TYPE                  /* clnt_nlm */ \
+	"b" CLNT_V4_OPS_TYPE                     /* clnt_v4  */ \
+	"b" CLNT_CMP_OPS_TYPE                    /* clnt_cmp */
+#else
+#define CE_ALL_OPS_TYPE                          \
+	"b" CLNT_V3NLM_OPS_TYPE                  /* clnt_v3  */ \
+	"b" CLNT_V4_OPS_TYPE                     /* clnt_v4  */ \
+	"b" CLNT_CMP_OPS_TYPE                    /* clnt_cmp */
+#endif
+#else
+#ifdef _USE_NLM
+#define CE_ALL_OPS_TYPE                          \
+	"b" CLNT_V3NLM_OPS_TYPE                  /* clnt_nlm */ \
+	"b" CLNT_V4_OPS_TYPE                     /* clnt_v4  */ \
+	"b" CLNT_CMP_OPS_TYPE                    /* clnt_cmp */
+#else
+#define CE_ALL_OPS_TYPE                          \
+	"b" CLNT_V4_OPS_TYPE                     /* clnt_v4  */ \
+	"b" CLNT_CMP_OPS_TYPE                    /* clnt_cmp */
+#endif
+#endif
+
+/* Array reply macro for all clients all_ops stats */
+#define CE_ALL_OPS_ARRAY_REPLY                        \
+	{ .name = "clients",                          \
+	  .type = "a(s(tt)" CE_ALL_OPS_TYPE ")",      \
+	  .direction = "out" }
+
+#ifdef _USE_NFS3
+#define CLNT_V3NLM_OPS_TYPE "a(sttt)"
 #define CLNT_V3NLM_OPS_REPLY              \
 	{ .name = "clnt_v3nlm_ops_stats", \
-	  .type = "a(sttt)",              \
+	  .type = CLNT_V3NLM_OPS_TYPE,    \
 	  .direction = "out" }
 #endif
 
+#define CLNT_V4_OPS_TYPE "a(stt)"
 #define CLNT_V4_OPS_REPLY \
-	{ .name = "clnt_v4_ops_stats", .type = "a(stt)", .direction = "out" }
+	{ .name = "clnt_v4_ops_stats", .type = CLNT_V4_OPS_TYPE, \
+	  .direction = "out" }
 
+#define CLNT_CMP_OPS_TYPE "ttt"
 #define CLNT_CMP_OPS_REPLY \
-	{ .name = "clnt_cmp_ops_stats", .type = "ttt", .direction = "out" }
+	{ .name = "clnt_cmp_ops_stats", .type = CLNT_CMP_OPS_TYPE, \
+	  .direction = "out" }
 
 #define TRANSPORT_REPLY                                                    \
 	{ .name = "rx_bytes", .type = "(t)", .direction = "out" },         \
@@ -396,6 +458,12 @@ void server_dbus_client_io_ops(DBusMessageIter *iter,
 			       struct gsh_client *client);
 void server_dbus_client_all_ops(DBusMessageIter *iter,
 				struct gsh_client *client);
+
+/* Array-safe version that always writes complete structure */
+void server_dbus_client_io_ops_for_array(DBusMessageIter *iter,
+					 struct gsh_client *client);
+void server_dbus_client_all_ops_for_array(DBusMessageIter *iter,
+					  struct gsh_client *client);
 void server_dbus_export_details(DBusMessageIter *iter,
 				struct gsh_export *g_export);
 #ifdef _USE_NFS3

src/support/server_stats.c
@@ -1844,8 +1844,8 @@ static void server_dbus_iostats(struct xfer_op *iop, DBusMessageIter *iter)
  * @param for_export  [IN] boolean indicating whether it is for an export
  **/
 
-static void server_dbus_cexop_stats(struct xfer_op *iop, DBusMessageIter *iter,
-				    bool for_export)
+void server_dbus_cexop_stats(struct xfer_op *iop, DBusMessageIter *iter,
+			     bool for_export)
 {
 	DBusMessageIter struct_iter;
 	double res = 0.0;
@@ -1856,13 +1856,11 @@ static void server_dbus_cexop_stats(struct xfer_op *iop, DBusMessageIter *iter,
 				       &iop->cmd.total);
 	dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_UINT64,
 				       &iop->cmd.errors);
-	if (for_export) {
-		if (iop->cmd.total)
-			res = (double)(iop->cmd.latency.latency * 0.000001) /
-			      (iop->cmd.total);
-		dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_DOUBLE,
-					       &res);
-	}
+	/* Always write latency field - calculate if for_export, else 0.0 */
+	if (for_export && iop->cmd.total)
+		res = (double)(iop->cmd.latency.latency * 0.000001) /
+		      (iop->cmd.total);
+	dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_DOUBLE, &res);
 	dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_UINT64,
 				       &iop->transferred);
 	dbus_message_iter_close_container(iter, &struct_iter);
@@ -1881,8 +1879,8 @@ static void server_dbus_cexop_stats(struct xfer_op *iop, DBusMessageIter *iter,
  * @param iter  [IN] iterator in reply stream to fill
  * @param for_export  [IN] boolean indicating whether it is for an export
  **/
-static void server_dbus_ceop_stats(struct proto_op *op, DBusMessageIter *iter,
-				   bool for_export)
+void server_dbus_ceop_stats(struct proto_op *op, DBusMessageIter *iter,
+			    bool for_export)
 {
 	DBusMessageIter struct_iter;
 	double res = 0.0;
@@ -1893,13 +1891,10 @@ static void server_dbus_ceop_stats(struct proto_op *op, DBusMessageIter *iter,
 				       &op->total);
 	dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_UINT64,
 				       &op->errors);
-	if (for_export) {
-		if (op->total)
-			res = (double)(op->latency.latency * 0.000001) /
-			      op->total;
-		dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_DOUBLE,
-					       &res);
-	}
+	/* Always write latency field - calculate if for_export, else 0.0 */
+	if (for_export && op->total)
+		res = (double)(op->latency.latency * 0.000001) / op->total;
+	dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_DOUBLE, &res);
 	dbus_message_iter_close_container(iter, &struct_iter);
 }
 
@@ -1916,8 +1911,8 @@ static void server_dbus_ceop_stats(struct proto_op *op, DBusMessageIter *iter,
  * @param iter  [IN] iterator in reply stream to fill
  * @param for_export  [IN] boolean indicating whether it is for an export
  **/
-static void server_dbus_celo_stats(struct nfsv41_stats *sp,
-				   DBusMessageIter *iter, bool for_export)
+void server_dbus_celo_stats(struct nfsv41_stats *sp, DBusMessageIter *iter,
+			    bool for_export)
 {
 	DBusMessageIter struct_iter;
 	uint64_t total, errors, delays;
@@ -1932,9 +1927,8 @@ static void server_dbus_celo_stats(struct nfsv41_stats *sp,
 					 &struct_iter);
 	dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_UINT64, &total);
 	dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_UINT64, &errors);
-	if (for_export)
-		dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_UINT64,
-					       &delays);
+	/* Always write delays field - type signature is (ttt) */
+	dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_UINT64, &delays);
 	dbus_message_iter_close_container(iter, &struct_iter);
 }
 
@@ -2012,6 +2006,248 @@ void server_dbus_client_io_ops(DBusMessageIter *iter, struct gsh_client *client)
 		server_dbus_ceop_stats(&st->nfsv42->compounds, iter, false);
 		server_dbus_celo_stats(st->nfsv42, iter, false);
 	}
+}
+
+/**
+ * @brief Write client I/O stats for array element (always writes all fields)
+ *
+ * This is similar to server_dbus_client_io_ops but ensures all fields are
+ * written even when stats are not available (for DBus array consistency).
+ *
+ * @param iter [IN] DBus iterator
+ * @param client [IN] Client pointer
+ */
+void server_dbus_client_io_ops_for_array(DBusMessageIter *iter,
+					 struct gsh_client *client)
+{
+	struct server_stats *svr = NULL;
+	struct gsh_stats *st;
+	dbus_bool_t stats_available;
+	struct xfer_op zero_xfer = { 0 };
+	struct proto_op zero_proto = { 0 };
+	struct nfsv41_stats zero_v41 = { 0 };
+
+	svr = container_of(client, struct server_stats, client);
+	st = &svr->st;
+
+	gsh_dbus_append_timestamp(iter, &client->last_update);
+
+#ifdef _USE_NFS3
+	stats_available = st->nfsv3 != 0;
+	dbus_message_iter_append_basic(iter, DBUS_TYPE_BOOLEAN,
+				       &stats_available);
+	if (st->nfsv3) {
+		server_dbus_cexop_stats(&st->nfsv3->read, iter, false);
+		server_dbus_cexop_stats(&st->nfsv3->write, iter, false);
+		server_dbus_ceop_stats(&st->nfsv3->cmds, iter, false);
+	} else {
+		server_dbus_cexop_stats(&zero_xfer, iter, false);
+		server_dbus_cexop_stats(&zero_xfer, iter, false);
+		server_dbus_ceop_stats(&zero_proto, iter, false);
+	}
+#endif
+
+	stats_available = st->nfsv40 != 0;
+	dbus_message_iter_append_basic(iter, DBUS_TYPE_BOOLEAN,
+				       &stats_available);
+	if (st->nfsv40) {
+		server_dbus_cexop_stats(&st->nfsv40->read, iter, false);
+		server_dbus_cexop_stats(&st->nfsv40->write, iter, false);
+		server_dbus_ceop_stats(&st->nfsv40->compounds, iter, false);
+	} else {
+		server_dbus_cexop_stats(&zero_xfer, iter, false);
+		server_dbus_cexop_stats(&zero_xfer, iter, false);
+		server_dbus_ceop_stats(&zero_proto, iter, false);
+	}
+
+	stats_available = st->nfsv41 != 0;
+	dbus_message_iter_append_basic(iter, DBUS_TYPE_BOOLEAN,
+				       &stats_available);
+	if (st->nfsv41) {
+		server_dbus_cexop_stats(&st->nfsv41->read, iter, false);
+		server_dbus_cexop_stats(&st->nfsv41->write, iter, false);
+		server_dbus_ceop_stats(&st->nfsv41->compounds, iter, false);
+		server_dbus_celo_stats(st->nfsv41, iter, false);
+	} else {
+		server_dbus_cexop_stats(&zero_xfer, iter, false);
+		server_dbus_cexop_stats(&zero_xfer, iter, false);
+		server_dbus_ceop_stats(&zero_proto, iter, false);
+		server_dbus_celo_stats(&zero_v41, iter, false);
+	}
+
+	stats_available = st->nfsv42 != 0;
+	dbus_message_iter_append_basic(iter, DBUS_TYPE_BOOLEAN,
+				       &stats_available);
+	if (st->nfsv42) {
+		server_dbus_cexop_stats(&st->nfsv42->read, iter, false);
+		server_dbus_cexop_stats(&st->nfsv42->write, iter, false);
+		server_dbus_ceop_stats(&st->nfsv42->compounds, iter, false);
+		server_dbus_celo_stats(st->nfsv42, iter, false);
+	} else {
+		server_dbus_cexop_stats(&zero_xfer, iter, false);
+		server_dbus_cexop_stats(&zero_xfer, iter, false);
+		server_dbus_ceop_stats(&zero_proto, iter, false);
+		server_dbus_celo_stats(&zero_v41, iter, false);
+	}
+}
+
+/**
+ * @brief Write client all_ops stats for array element
+ * (always writes all fields)
+ *
+ * This is similar to server_dbus_client_all_ops but ensures all fields are
+ * written even when stats are not available (for DBus array consistency).
+ * Empty arrays are written when stats unavailable.
+ *
+ * @param iter [IN] DBus iterator
+ * @param client [IN] Client pointer
+ */
+void server_dbus_client_all_ops_for_array(DBusMessageIter *iter,
+					  struct gsh_client *client)
+{
+	struct server_stats *svr = NULL;
+	struct gsh_clnt_allops_stats *c_all;
+	dbus_bool_t stats_available;
+	int i;
+	DBusMessageIter array_iter;
+	struct gsh_stats *st;
+	uint64_t tot_cmp = 0, err_cmp = 0, ops_in_cmp = 0;
+	char *op_name;
+
+	svr = container_of(client, struct server_stats, client);
+	c_all = &svr->c_all;
+	st = &svr->st;
+
+	gsh_dbus_append_timestamp(iter, &client->last_update);
+
+#ifdef _USE_NFS3
+	/* Stats of NFSv3 ops */
+	stats_available = c_all->nfsv3 != 0;
+	dbus_message_iter_append_basic(iter, DBUS_TYPE_BOOLEAN,
+				       &stats_available);
+	/* Always write array container, even if empty */
+	dbus_message_iter_open_container(iter, DBUS_TYPE_ARRAY, "(sttt)",
+					 &array_iter);
+	if (c_all->nfsv3) {
+		DBusMessageIter struct_iter;
+
+		for (i = 0; i < NFS_V3_NB_COMMAND; i++) {
+			if (c_all->nfsv3->cmds[i].total) {
+				dbus_message_iter_open_container(
+					&array_iter, DBUS_TYPE_STRUCT, NULL,
+					&struct_iter);
+				op_name = (char *)nfsproc3_to_str(i);
+				dbus_message_iter_append_basic(&struct_iter,
+							       DBUS_TYPE_STRING,
+							       &op_name);
+				dbus_message_iter_append_basic(
+					&struct_iter, DBUS_TYPE_UINT64,
+					&c_all->nfsv3->cmds[i].total);
+				dbus_message_iter_append_basic(
+					&struct_iter, DBUS_TYPE_UINT64,
+					&c_all->nfsv3->cmds[i].errors);
+				dbus_message_iter_append_basic(
+					&struct_iter, DBUS_TYPE_UINT64,
+					&c_all->nfsv3->cmds[i].dups);
+				dbus_message_iter_close_container(&array_iter,
+								  &struct_iter);
+			}
+		}
+	}
+	dbus_message_iter_close_container(iter, &array_iter);
+#endif
+
+#ifdef _USE_NLM
+	/* Stats of NLMv4 ops */
+	stats_available = c_all->nlm4 != 0;
+	dbus_message_iter_append_basic(iter, DBUS_TYPE_BOOLEAN,
+				       &stats_available);
+	/* Always write array container, even if empty */
+	dbus_message_iter_open_container(iter, DBUS_TYPE_ARRAY, "(sttt)",
+					 &array_iter);
+	if (c_all->nlm4) {
+		DBusMessageIter struct_iter;
+
+		for (i = 0; i < NLM_V4_NB_OPERATION; i++) {
+			if (c_all->nlm4->cmds[i].total) {
+				dbus_message_iter_open_container(
+					&array_iter, DBUS_TYPE_STRUCT, NULL,
+					&struct_iter);
+				dbus_message_iter_append_basic(&struct_iter,
+							       DBUS_TYPE_STRING,
+							       &optnlm[i].name);
+				dbus_message_iter_append_basic(
+					&struct_iter, DBUS_TYPE_UINT64,
+					&c_all->nlm4->cmds[i].total);
+				dbus_message_iter_append_basic(
+					&struct_iter, DBUS_TYPE_UINT64,
+					&c_all->nlm4->cmds[i].errors);
+				dbus_message_iter_append_basic(
+					&struct_iter, DBUS_TYPE_UINT64,
+					&c_all->nlm4->cmds[i].dups);
+				dbus_message_iter_close_container(&array_iter,
+								  &struct_iter);
+			}
+		}
+	}
+	dbus_message_iter_close_container(iter, &array_iter);
+#endif
+
+	/* Stats of NFSv4 ops */
+	stats_available = c_all->nfsv4 != 0;
+	dbus_message_iter_append_basic(iter, DBUS_TYPE_BOOLEAN,
+				       &stats_available);
+	/* Always write array container, even if empty */
+	dbus_message_iter_open_container(iter, DBUS_TYPE_ARRAY, "(stt)",
+					 &array_iter);
+	if (c_all->nfsv4) {
+		DBusMessageIter struct_iter;
+
+		for (i = 0; i < NFS4_OP_LAST_ONE; i++) {
+			if (c_all->nfsv4->cmds[i].total) {
+				dbus_message_iter_open_container(
+					&array_iter, DBUS_TYPE_STRUCT, NULL,
+					&struct_iter);
+				op_name = (char *)nfsop4_to_str(i);
+				dbus_message_iter_append_basic(&struct_iter,
+							       DBUS_TYPE_STRING,
+							       &op_name);
+				dbus_message_iter_append_basic(
+					&struct_iter, DBUS_TYPE_UINT64,
+					&c_all->nfsv4->cmds[i].total);
+				dbus_message_iter_append_basic(
+					&struct_iter, DBUS_TYPE_UINT64,
+					&c_all->nfsv4->cmds[i].errors);
+				dbus_message_iter_close_container(&array_iter,
+								  &struct_iter);
+			}
+		}
+	}
+	dbus_message_iter_close_container(iter, &array_iter);
+
+	/* Gather info abt compound ops */
+	if (st->nfsv40) {
+		tot_cmp += st->nfsv40->compounds.total;
+		err_cmp += st->nfsv40->compounds.errors;
+		ops_in_cmp += st->nfsv40->ops_per_compound;
+	}
+	if (st->nfsv41) {
+		tot_cmp += st->nfsv41->compounds.total;
+		err_cmp += st->nfsv41->compounds.errors;
+		ops_in_cmp += st->nfsv41->ops_per_compound;
+	}
+	if (st->nfsv42) {
+		tot_cmp += st->nfsv42->compounds.total;
+		err_cmp += st->nfsv42->compounds.errors;
+		ops_in_cmp += st->nfsv42->ops_per_compound;
+	}
+	stats_available = tot_cmp != 0;
+	dbus_message_iter_append_basic(iter, DBUS_TYPE_BOOLEAN,
+				       &stats_available);
+	/* Always write compound ops values directly (not in a struct) */
+	dbus_message_iter_append_basic(iter, DBUS_TYPE_UINT64, &tot_cmp);
+	dbus_message_iter_append_basic(iter, DBUS_TYPE_UINT64, &err_cmp);
+	dbus_message_iter_append_basic(iter, DBUS_TYPE_UINT64, &ops_in_cmp);
 }
 
 void server_dbus_client_all_ops(DBusMessageIter *iter,

src/scripts/ganeshactl/ganesha_stats.py
@@ -47,8 +47,8 @@ To display stat counters use:
               inode | iov3 [export id] | iov4 [export id] | iov41 [export id] | iov42 [export id] |
               iomon [export id] | export | total [export id] | fast | pnfs [export id] |
               fsal <fsal name> | v3_full | v4_full | auth |
-              client_io_ops <ip address> | export_details <export id> |
-              client_all_ops <ip address>]
+              client_io_ops <ip address | all> | export_details <export id> |
+              client_all_ops <ip address | all>]
 
 To display stat counters in json format use:
   {progname} json <command>
@@ -96,10 +96,15 @@ commands = (
 if command not in commands:
     print("\nError: Option '%s' is not correct." % command)
     print_usage_exit(1)
-# requires an IP address
-elif command in ('deleg', 'client_io_ops', 'client_all_ops'):
+# requires an IP address or 'all'
+elif command == 'deleg':
     if not len(opts) == 1:
         print("\nError: Option '%s' must be followed by an ip address." % command)
+        print_usage_exit(1)
+    command_arg = opts[0]
+elif command in ('client_io_ops', 'client_all_ops'):
+    if not len(opts) == 1:
+        print("\nError: Option '%s' must be followed by an ip address or 'all'." % command)
         print_usage_exit(1)
     command_arg = opts[0]
 # requires an export id
@@ -158,9 +163,15 @@ try:
     elif command == "deleg":
         result = cl_interface.deleg_stats(command_arg)
     elif command == "client_io_ops":
-        result = cl_interface.client_io_ops_stats(command_arg)
+        if command_arg == 'all':
+            result = cl_interface.all_clients_io_ops_stats()
+        else:
+            result = cl_interface.client_io_ops_stats(command_arg)
     elif command == "client_all_ops":
-        result = cl_interface.client_all_ops_stats(command_arg)
+        if command_arg == 'all':
+            result = cl_interface.all_clients_all_ops_stats()
+        else:
+            result = cl_interface.client_all_ops_stats(command_arg)
     elif command == "iov3":
         result = exp_interface.v3io_stats(command_arg)
     elif command == "iov4":

commit ee9e15616e7104c56cf8c2600a0e00ff830854dc

Fix DBus parsing issue: revert conditional latency field writing
The commit 7871a4657 introduced a parsing mismatch where latency field
was always written to DBus output, causing the Python parser to
incorrectly interpret latency values as transferred bytes.

This maintains backward compatibility with existing Python parser and
ensures correct field mapping for client statistics.

Change-Id: Iad1863691dfe5e7471ddc4644a5b25961238332f
Fixes: Field misalignment in client I/O statistics output
Signed-off-by: Prabhu Murugesan <Prabhu.Murugesan1@ibm.com>

src/include/server_stats_private.h
@@ -238,15 +238,15 @@ struct auth_stats {
 	{ .name = "read", .type = "(tttttt)", .direction = "out" }, \
 		{ .name = "write", .type = "(tttttt)", .direction = "out" }
 
-#define CEIOSTATS_TYPE   "(ttdt)(ttdt)(ttd)"   /* read, write, other */
+#define CEIOSTATS_TYPE   "(ttt)(ttt)(tt)"   /* read, write, other */
 #define CEIOSTATS_REPLY                                                    \
-	{ .name = "read", .type = "(ttdt)", .direction = "out" },          \
-		{ .name = "write", .type = "(ttdt)", .direction = "out" }, \
-		{ .name = "other", .type = "(ttd)", .direction = "out" }
+	{ .name = "read", .type = "(ttt)", .direction = "out" },          \
+		{ .name = "write", .type = "(ttt)", .direction = "out" }, \
+		{ .name = "other", .type = "(tt)", .direction = "out" }
 
-#define CELOSTATS_TYPE   "(ttt)"               /* layout */
+#define CELOSTATS_TYPE   "(tt)"               /* layout */
 #define CELOSTATS_REPLY \
-	{ .name = "layout", .type = "(ttt)", .direction = "out" }
+	{ .name = "layout", .type = "(tt)", .direction = "out" }
 
 #ifdef _USE_NFS3
 #define CE_STATS_REPLY                                                   \

src/support/server_stats.c
@@ -1873,11 +1873,14 @@ void server_dbus_cexop_stats(struct xfer_op *iop, DBusMessageIter *iter,
 				       &iop->cmd.total);
 	dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_UINT64,
 				       &iop->cmd.errors);
-	/* Always write latency field - calculate if for_export, else 0.0 */
-	if (for_export && iop->cmd.total)
-		res = (double)(iop->cmd.latency.latency * 0.000001) /
-		      (iop->cmd.total);
-	dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_DOUBLE, &res);
+	/* Only write latency field for exports */
+	if (for_export) {
+		if (iop->cmd.total)
+			res = (double)(iop->cmd.latency.latency * 0.000001) /
+			      (iop->cmd.total);
+		dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_DOUBLE,
+					       &res);
+	}
 	dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_UINT64,
 				       &iop->transferred);
 	dbus_message_iter_close_container(iter, &struct_iter);
@@ -1908,10 +1911,14 @@ void server_dbus_ceop_stats(struct proto_op *op, DBusMessageIter *iter,
 				       &op->total);
 	dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_UINT64,
 				       &op->errors);
-	/* Always write latency field - calculate if for_export, else 0.0 */
-	if (for_export && op->total)
-		res = (double)(op->latency.latency * 0.000001) / op->total;
-	dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_DOUBLE, &res);
+	/* Only write latency field for exports */
+	if (for_export) {
+		if (op->total)
+			res = (double)(op->latency.latency * 0.000001) /
+			      op->total;
+		dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_DOUBLE,
+					       &res);
+	}
 	dbus_message_iter_close_container(iter, &struct_iter);
 }
 
@@ -1944,8 +1951,10 @@ void server_dbus_celo_stats(struct nfsv41_stats *sp, DBusMessageIter *iter,
 					 &struct_iter);
 	dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_UINT64, &total);
 	dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_UINT64, &errors);
-	/* Always write delays field - type signature is (ttt) */
-	dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_UINT64, &delays);
+	/* Only write delays field for exports */
+	if (for_export)
+		dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_UINT64,
+					       &delays);
 	dbus_message_iter_close_container(iter, &struct_iter);
 }
 