e34fe49ccab8869d3cd2fc2048c427d9b9546694
Frank S. Filz <ffilzlnx@mindspring.com>
2026-08-27 09:37:01 -0700

    TEMP
    Change-Id: I659645c9a36ad5d7b49df6050f05217644797dbc
    Signed-off-by: Frank S. Filz <ffilzlnx@mindspring.com>

--- src/support/cluster_members.c
+++ src/support/cluster_members.c
@@ -349,3 +349,34 @@ bool grpc_show_cluster_members(char ***ips_out, size_t *count_out,
 	return ok;
 }
 #endif /* USE_GRPC */
+
+#ifdef ENABLE_CLUSTER_QOS
+void populate_cqos_hosts(void)
+{
+	struct glist_head *glist;
+	struct base_client_entry *client;
+	struct cqos_ceph_nodes *cli;
+
+	if (glist_empty(&nfs_param.core_param.cluster_members))
+		return;
+
+	glist_for_each(glist, &nfs_param.core_param.cluster_members) {
+		client = glist_entry(glist, struct base_client_entry, cle_list);
+
+		cli = gsh_calloc(1, sizeof(struct cqos_ceph_nodes),
+				 MEM_COMP_QOS);
+		glist_init(&cli->node_list);
+		cli->fd = -1;
+		cli->clnt = NULL;
+
+		cli->node_addr = client->cidr->ip_addr;
+
+		glist_add_tail(&cqos_hosts, &cli->node_list);
+	}
+}
+
+void cqos_hosts_release(struct glist_head *list, mem_components_t comp)
+{
+	//
+}
+#endif
