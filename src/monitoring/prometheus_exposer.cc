ce18c7410c76f9723c6f096ca600cf11c8bafa07
Sreedhar Agraharam <sragraha@redhat.com>
2026-09-04 11:15:10 -0400

    Revert "Monitoring: Add scrape-time NFSv4 export and ops metrics update support"
    This reverts commit 1eb59ba89f84de089f8beb1286a869a1af89ae4f.
    
    Change-Id: I4f70821b93be4a2d64e7d597b46767162a1a7d6b
    Signed-off-by: Sreedhar Agraharam <sragraha@redhat.com>

--- src/monitoring/prometheus_exposer.cc
+++ src/monitoring/prometheus_exposer.cc
@@ -37,7 +37,6 @@
 
 extern "C" {
 #include "fsal.h"
-#include "nfs_core.h"
 #include "gsh_config.h"
 #include "log.h"
 }
@@ -52,8 +51,9 @@ extern "C" {
 #include <sys/time.h>
 #include "prometheus_exposer.h"
 #include "dynamic_metrics.h"
+#include "nfs_core.h"
 #include "nfs_metrics.h"
-#include "server_stats.h"
+
 #ifdef USE_MONITORING
 
 #define PERROR(MESSAGE)                                                    \
@@ -341,14 +341,6 @@ static inline int64_t get_elapsed_ms(uint64_t start_time_ns)
 	return (now_mono_ns() - start_time_ns) / 1000000LL;
 }
 
-static void update_all_metrics(void)
-{
-	update_rpc_metrics();
-	//update_client_metrics(); // Needs to be implemented
-	update_ops_metrics(); // Implemented in patchset 2
-	return;
-}
-
 void *PrometheusExposer::server_thread(void *arg)
 {
 	PrometheusExposer *const exposer =
@@ -365,7 +357,6 @@ void *PrometheusExposer::server_thread(void *arg)
 				PERROR("Failed to accept connection");
 			continue;
 		}
-		update_all_metrics();
 		const uint64_t start_time = now_mono_ns();
 		recv(client_fd, buffer, sizeof(buffer), 0);
 
@@ -411,18 +402,12 @@ void *PrometheusExposer::server_thread(void *arg)
 
 extern "C" {
 
-void register_all_metrics()
-{
-	register_rpcs_metrics();
-}
-
 void prometheus_exposer__start(const sockaddr_t *addr, uint16_t port,
 			       prometheus_registry_handle_t registry_handle)
 {
 	static bool initialized = false;
 	if (initialized)
 		return;
-	register_all_metrics();
 	prometheus::Registry *registry_ptr =
 		static_cast<prometheus::Registry *>(registry_handle.registry);
 	static PrometheusExposer exposer(*registry_ptr);
